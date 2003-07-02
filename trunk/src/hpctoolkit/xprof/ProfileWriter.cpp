db7d49d553ed4a61ae5040c9a4802839977a5221
eraxxon <eraxxon@b1b131fe-c5db-9c05-3d80-cf3d96147e2e>
2003-07-02 18:22:21 +0000

    'nativeName' and 'displayName' should be output as strings so any invalid characters are appropriately escaped.

--- trunk/src/hpctoolkit/xprof/ProfileWriter.cpp
+++ trunk/src/hpctoolkit/xprof/ProfileWriter.cpp
@@ -147,9 +147,9 @@ ProfileWriter::WriteProfile(std::ostream& os, DerivedProfile* profData,
     for (uint i = 0; it.IsValid(); ++it, ++i) {
       DerivedProfileMetric* m = it.Current();
       os << "<METRIC shortName"; WriteAttrNum(os, i);
-      os << " nativeName";       WriteAttrNum(os, m->GetNativeName());
+      os << " nativeName";       WriteAttrStr(os, m->GetNativeName());
       os << " period";           WriteAttrNum(os, m->GetPeriod());
-      os << " displayName";      WriteAttrNum(os, m->GetName());
+      os << " displayName";      WriteAttrStr(os, m->GetName());
       // os << " count_total";   WriteAttrNum(os, m->GetTotalCount());
       os << "/>\n";
     }
