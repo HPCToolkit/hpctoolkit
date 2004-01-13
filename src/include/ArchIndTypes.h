8db8d74a87ba9b6629e1c8bbf0936ea8a7366460
eraxxon <eraxxon@b1b131fe-c5db-9c05-3d80-cf3d96147e2e>
2004-01-13 21:02:09 +0000

    In order to fix compilation errors and warnings with GCC 3.3.2:   - Add INTx_C() macros for writing 64-bit integer literals in a     platform independent way. (The C++ standard assumes integer     literals are at most of 'long' type, causing truncation on 32-bit     machines.)   - Add PRIab() macros for writing printf() format specifiers for     64-bit values in a platform independent way.  (On 32-bit machines,     the format string includes 'll' whereas on 64-bit machines it is     usually 'l'.)
    Other improvements:
      - The file can be included by C++ or C code.
      - Platfrom detection tests for definitions of system macros
        (#if defined(x)) instead of their values (#if x).
      - Define platform, architecture and os macros for later use.
      - use <inttypes.h> to define specifically-sized types (e.g. int32_t)
        and remove manual definitions of the same.

--- src/include/ArchIndTypes.h
+++ src/include/ArchIndTypes.h
@@ -52,57 +52,102 @@
 
 //************************* System Include Files ****************************
 
-#ifdef NO_STD_CHEADERS
-# include <limits.h>
+// The following provides a portable definition of basic types
+#include <inttypes.h> /* commonly available, unlike <stdint.h> */
+
+#if defined(__cplusplus)
+# ifdef NO_STD_CHEADERS
+#  include <limits.h>
+# else
+#  include <climits>
+# endif
 #else
-# include <climits>
+# include <limits.h>
 #endif
 
 //*************************** User Include Files ****************************
 
 //*************************** Forward Declarations ***************************
 
-#if ((__digital__ || __alpha) && __unix)
-# define ALPHA_64 /* __arch64__ is not necessarily portable */  
-#elif (__sgi && __mips && __unix)
+//****************************************************************************
+
+// Verify that we are using a known platform and define platform and
+// architecture macros.
+// (Note: These are in alphabetical order by platform name)
+
+#if ((defined(__alpha) || defined(__digital__)) && defined(__unix__))
+# define PLATFORM_ALPHA_OSF1
+# define ARCH_ALPHA64
+# define ARCH_64
+# define OS_OSF1
+#elif (defined(__i386) && defined(__linux))
+# define PLATFORM_I686_LINUX
+# define ARCH_IA32
+# define ARCH_32
+# define OS_LINUX
+#elif (defined(__ia64) && defined(__linux))
+# define PLATFORM_IA64_LINUX
+# define ARCH_IA64
+# define ARCH_64
+# define OS_LINUX
+#elif (defined(__mips) && defined(__sgi) && defined(__unix))
+# define PLATFORM_MIPS_IRIX64
+# define OS_IRIX64
 # if ((_MIPS_ISA == _MIPS_ISA_MIPS1) || (_MIPS_ISA == _MIPS_ISA_MIPS2))
-#  define MIPS_32
+#  define ARCH_MIPS32
+#  define ARCH_32
 # elif ((_MIPS_ISA == _MIPS_ISA_MIPS3) || (_MIPS_ISA == _MIPS_ISA_MIPS4))
-#  define MIPS_64
+#  define ARCH_MIPS64
+#  define ARCH_64
 # else
-#  error "ArchIndTypes - MIPS: does not know of your system"
+#  error "ArchIndTypes.h: Unknown MIPS platform."
 # endif
-#elif (__sun && __sparc && __unix)
-# if (__sparcv9)
-#  define SPARC_64
-#  error "ArchIndTypes - SPARC: 64-bit arch has not been tested" 
+#elif (defined(__ppc__) && defined(__MACH__))
+# define PLATFORM_POWERPC_DARWIN
+# define ARCH_POWERPC32
+# define ARCH_32
+# define OS_MACHDARWIN
+#elif (defined(__sparc) && defined(__sun) && defined(__unix))
+# define PLATFORM_SPARC_SUNOS
+# define OS_SUNOS
+# if (defined(__sparcv9))
+#  define ARCH_SPARC64
+#  define ARCH_64
+#  error "ArchIndTypes.h: Sparc64 architecture has not been tested."
 # else // (__sparcv8)
-#  define SPARC_32
+#  define ARCH_SPARC32
+#  define ARCH_32
 # endif
-#elif (__linux && __i386)
-# define INTEL_IA32
-#elif (__linux && __ia64)
-# define INTEL_IA64
-#elif (defined(__MACH__) && defined(__ppc__))
-# define DARWIN_POWERPC
 #else
-# error "ArchIndTypes: does not know of your system" 
+# error "ArchIndTypes.h does not know of your platform!"
 #endif
 
 //****************************************************************************
 
-// 64 and 32 bit compilation types (along with appropriate limits):
-// - pointer-sized int/unit (psint, psuint)
-// - standard-sized int/uint (sint, suint) 
+// Special types (along with appropriate limits):
+// - pointer-sized integer: psint, psuint
+// - standard-sized integer: sint, suint
 //   Both are always guaranteed to have the same number of bits as a
 //   pointer; the latter are designed for use as a type that is better
 //   thought of as an integer (without the implementation/pointer
 //   baggage, but where a few pointer casts are permissable).
-// - 32-bit integer (int32, uint32)
-//   For use as types where a certain size is needed independent of
-//   the implementation.
 
-#if ((__digital__ || __alpha) && __unix)
+#if (defined(ARCH_IA32) || defined(ARCH_MIPS32) || defined(ARCH_POWERPC32) \
+     || defined(ARCH_SPARC32))
+
+  // int/long/pointer: 32 bits
+  typedef int           psint;  // pointer-sized int (32-bit)
+  typedef unsigned int  psuint; // pointer-sized unsigned int
+# define PSINT_MIN  (INT_MIN)
+# define PSINT_MAX  (INT_MAX)
+# define PSUINT_MAX (UINT_MAX)
+
+# if (defined(ARCH_SPARC32))
+#  define ARCH_USE_LONG_LONG 1 /* FIXME */
+# endif
+
+#elif (defined(ARCH_ALPHA64) || defined(ARCH_IA32) || defined(ARCH_MIPS64) \
+       || defined(ARCH_SPARC64))
 
   // long/pointer: 64 bits; int: 32 bits
   typedef long          psint;  // pointer-sized int (64-bit)
@@ -110,132 +155,11 @@
 # define PSINT_MIN  (LONG_MIN)
 # define PSINT_MAX  (LONG_MAX)
 # define PSUINT_MAX (ULONG_MAX)
-
-  typedef int           int32;
-  typedef unsigned int  uint32;
-# define INT32_MIN  (INT_MIN)
-# define INT32_MAX  (INT_MAX)
-# define UINT32_MAX (UINT_MAX)
 
 # define ARCH_INT_DIFFERS_FROM_SINT 1
-  typedef long          int64;
-  typedef unsigned long uint64;
-#define INT64_MIN  (LONG_MIN)
-#define INT64_MAX  (LONG_MAX)
-#define UINT64_MAX (ULONG_MAX)
-
-#elif (__sgi && __mips && __unix)
-# if (_MIPS_SZLONG == 32)
-
-   // int/long/pointer: 32 bits
-   typedef int           psint;  // pointer-sized int (32-bit)
-   typedef unsigned int  psuint; // pointer-sized unsigned int
-#  define PSINT_MIN  (INT_MIN)
-#  define PSINT_MAX  (INT_MAX)
-#  define PSUINT_MAX (UINT_MAX)
-
-   typedef int           int32;
-   typedef unsigned int  uint32;
-#  define INT32_MIN  (INT_MIN)
-#  define INT32_MAX  (INT_MAX)
-#  define UINT32_MAX (UINT_MAX)
-
-# elif (_MIPS_SZLONG == 64)
-
-   // long/pointer: 64 bits; int: 32 bits
-   typedef long          psint;  // pointer-sized int (64-bit)
-   typedef unsigned long psuint; // pointer-sized unsigned int
-#  define PSINT_MIN  (LONG_MIN)
-#  define PSINT_MAX  (LONG_MAX)
-#  define PSUINT_MAX (ULONG_MAX)
-
-   typedef int           int32;
-   typedef unsigned int  uint32;
-#  define INT32_MIN  (INT_MIN)
-#  define INT32_MAX  (INT_MAX)
-#  define UINT32_MAX (UINT_MAX)
-
-#  define ARCH_INT_DIFFERS_FROM_SINT 1
-   typedef long          int64;
-   typedef unsigned long uint64;
-#  define INT64_MIN  (LONG_MIN)
-#  define INT64_MAX  (LONG_MAX)
-#  define UINT64_MAX (ULONG_MAX)
-
-# else
-#  error "ArchIndTypes: Need a new type definition!"
-# endif // __sgi
-#elif (__sun && __sparc && __unix)
-# if (__sparcv9)
-
-   // long/pointer: 64 bits; int: 32 bits (_LP64)
-   typedef long          psint;  // pointer-sized int (64-bit)
-   typedef unsigned long psuint; // pointer-sized unsigned int
-#  define PSINT_MIN  (LONG_MIN)
-#  define PSINT_MAX  (LONG_MAX)
-#  define PSUINT_MAX (ULONG_MAX)
-
-   typedef int           int32;
-   typedef unsigned int  uint32;
-#  define INT32_MIN  (INT_MIN)
-#  define INT32_MAX  (INT_MAX)
-#  define UINT32_MAX (UINT_MAX)
-
-#  error "ArchIndTypes: 64-bit sparc architecture has not been tested!"
-#  define ARCH_INT_DIFFERS_FROM_SINT 1
-   typedef long          int64;
-   typedef unsigned long uint64;
-#  define INT64_MIN  (LONG_MIN)
-#  define INT64_MAX  (LONG_MAX)
-#  define UINT64_MAX (ULONG_MAX)
-
-# else //  (__sparcv8)
-
-   // int/long/pointer: 32 bits (_ILP32)
-   typedef long          psint;  // pointer-sized int (32-bit)
-   typedef unsigned long psuint; // pointer-sized unsigned int
-#  define PSINT_MIN  (LONG_MIN)
-#  define PSINT_MAX  (LONG_MAX)
-#  define PSUINT_MAX (ULONG_MAX)
-
-   typedef int           int32;
-   typedef unsigned int  uint32;
-   // INT32_MIN, INT32_MAX, UINT32_MAX are already defined
-#  define ARCH_USE_LONG_LONG 1
-
-# endif // __sun
-#elif ((__i386 && __linux) || defined(DARWIN_POWERPC))
-
-  // int/long/pointer: 32 bits (_ILP32)
-  typedef long          psint;  // pointer-sized int (32-bit)
-  typedef unsigned long psuint; // pointer-sized unsigned int
-# define PSINT_MIN  (LONG_MIN)
-# define PSINT_MAX  (LONG_MAX)
-# define PSUINT_MAX (ULONG_MAX)
-
-  typedef int           int32;
-  typedef unsigned int  uint32;
-# define INT32_MIN  (INT_MIN)
-# define INT32_MAX  (INT_MAX)
-# define UINT32_MAX (UINT_MAX)
-
-#elif (__ia64 && __linux)
-
-  // long/pointer: 64 bits; int: 32 bits
-  typedef long          psint;  // pointer-sized int (64-bit)
-  typedef unsigned long psuint; // pointer-sized unsigned int
-# define PSINT_MIN  (LONG_MIN)
-# define PSINT_MAX  (LONG_MAX)
-# define PSUINT_MAX (ULONG_MAX)
-
-  typedef int           int32;
-  typedef unsigned int  uint32;
-# define INT32_MIN  (INT_MIN)
-# define INT32_MAX  (INT_MAX)
-# define UINT32_MAX (UINT_MAX)
 
 #else
-# error "ArchIndTypes: Need a new type definition!"
+# error "ArchIndTypes.h: Internal error defining special types."
 #endif 
 
 
@@ -245,10 +169,74 @@ typedef psuint suint;  // standard sized unsigned int
 #define SINT_MAX  (PSINT_MAX)
 #define SUINT_MAX (PSUINT_MAX)
 
+
+// Special types for convenience 
+//   - Analagous to historical ushort, ulong
+
+#if defined(__cplusplus) 
+  // This can still cause duplicate definition conflicts (with system
+  // headers) in some C code.  Eventually we will be able to rely on
+  // C99 conformance.
+  typedef unsigned short int ushort;
+  typedef unsigned long  int ulong;
+#endif
+
 //****************************************************************************
 
-typedef unsigned short ushort;
-typedef unsigned long  ulong;
+// Macros for integer literal expressions
+// (Analagous to INTx_C() on Solaris/Linux, provided by inttypes.h)
+
+#ifndef INT8_C
+# define INT8_C(x)   x
+#endif
+#ifndef UINT8_C
+# define UINT8_C(x)  x ## U
+#endif
+#ifndef INT16_C
+# define INT16_C(x)  x
+#endif
+#ifndef UINT16_C
+# define UINT16_C(x) x ## U
+#endif
+
+#if defined(ARCH_32)
+# ifndef INT32_C /* use as a synecdoche for INT32_C and INT64_C */
+#  define INT32_C(x)  x ## L
+#  define UINT32_C(x) x ## UL
+#  define INT64_C(x)  x ## LL
+#  define UINT64_C(x) x ## ULL
+# endif
+#elif defined(ARCH_64)
+# ifndef INT32_C /* use as a synecdoche for xINT32_C and xINT64_C */
+#  define INT32_C(x)  x
+#  define UINT32_C(x) x
+#  define INT64_C(x)  x ## L
+#  define UINT64_C(x) x ## UL
+# endif
+#else
+# error "ArchIndTypes.h: Internal error defining int literal macros."
+#endif
+
+//****************************************************************************
+
+// printf format strings for constant sized types
+// (Analagous to PRIab() on Solaris/Linux, provided by inttypes.h)
+
+#if defined(ARCH_32)
+# ifndef PRId64 /* use as a synecdoche for PRIa64 */
+#  define PRId64    "lld"
+#  define PRIu64    "llu"
+#  define PRIx64    "llx"
+# endif
+#elif defined(ARCH_64)
+# ifndef PRId64 /* use as a synecdoche for PRIa64 */
+#  define PRId64    "ld"
+#  define PRIu64    "lu"
+#  define PRIx64    "lx"
+# endif
+#else
+# error "ArchIndTypes.h: Internal error defining printf macros."
+#endif
 
 //****************************************************************************
 
