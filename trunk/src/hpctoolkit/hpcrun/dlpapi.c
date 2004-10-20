a03131c03c2108934467fe91ad128e6db52d6a05
eraxxon <eraxxon@b1b131fe-c5db-9c05-3d80-cf3d96147e2e>
2004-10-20 00:57:53 +0000

    initial checkin

---
+++ trunk/src/hpctoolkit/hpcrun/dlpapi.c
@@ -0,0 +1,106 @@
+/* -*-Mode: C;-*- */
+/* $Header$ */
+
+/****************************************************************************
+//
+// File: 
+//    $Source$
+//
+// Purpose:
+//    Dynamically link PAPI library.
+//
+// Description:
+//    [The set of functions, macros, etc. defined in the file]
+//
+// Author:
+//    Written by John Mellor-Crummey and Nathan Tallent, Rice University.
+//
+*****************************************************************************/
+
+/************************** System Include Files ****************************/
+
+#include <unistd.h>
+#include <stdio.h>
+#include <dlfcn.h>
+
+/**************************** User Include Files ****************************/
+
+#include "dlpapi.h"
+
+/**************************** Forward Declarations **************************/
+
+dl_PAPI_is_initialized_t dl_PAPI_is_initialized = NULL;
+
+dl_PAPI_library_init_t dl_PAPI_library_init = NULL;
+
+dl_PAPI_get_opt_t dl_PAPI_get_opt = NULL;
+
+dl_PAPI_get_hardware_info_t dl_PAPI_get_hardware_info = NULL;
+
+dl_PAPI_get_event_info_t dl_PAPI_get_event_info = NULL;
+
+dl_PAPI_enum_event_t dl_PAPI_enum_event = NULL;
+
+static void* libpapi = NULL;
+
+/****************************************************************************/
+
+static void handle_any_dlerror();
+
+/* X is the routine name as called from C (i.e. not a string) */
+#define CALL_DLSYM(X) \
+    dl_ ## X = (dl_ ## X ## _t)dlsym(libpapi, #X)
+
+int
+dlopen_papi()
+{
+  /* Open PAPI lib */
+  libpapi = dlopen(HPC_PAPI_DLOPEN, RTLD_LAZY);
+  handle_any_dlerror();
+  
+  /* Initialize entry points */
+  CALL_DLSYM(PAPI_is_initialized);
+  handle_any_dlerror();
+
+  CALL_DLSYM(PAPI_library_init);
+  handle_any_dlerror();
+
+  CALL_DLSYM(PAPI_get_opt);
+  handle_any_dlerror();
+
+  CALL_DLSYM(PAPI_get_hardware_info);
+  handle_any_dlerror();
+
+  CALL_DLSYM(PAPI_get_event_info);
+  handle_any_dlerror();
+
+  CALL_DLSYM(PAPI_enum_event);
+  handle_any_dlerror();
+
+  return 0;
+}
+
+#undef CALL_DLSYM
+
+int 
+dlclose_papi()
+{
+  dlclose(libpapi);
+  handle_any_dlerror();
+  return 0;
+}
+
+
+static void
+handle_any_dlerror()
+{
+  /* Note: We assume dlsym() or something similar has just been called! */
+  char *error;
+  if ((error = dlerror()) != NULL) {
+    fprintf(stderr, "%s\n", error); 
+    exit(1);
+  }
+}
+
+
+/****************************************************************************/
---
+++ trunk/src/hpctoolkit/hpcrun/dlpapi.h
@@ -0,0 +1,69 @@
+/* -*-Mode: C;-*- */
+/* $Header$ */
+
+/****************************************************************************
+//
+// File:
+//    $Source$
+//
+// Purpose:
+//    Dynamically link PAPI library.
+//
+// Description:
+//    [The set of functions, macros, etc. defined in the file]
+//
+// Author:
+//    Written by John Mellor-Crummey and Nathan Tallent, Rice University.
+//
+*****************************************************************************/
+
+#ifndef dlpapi_h
+#define dlpapi_h
+
+/************************** System Include Files ****************************/
+
+/**************************** User Include Files ****************************/
+
+#include "hpcpapi.h"
+
+/**************************** Forward Declarations **************************/
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Dynamically open and link entry points.  These routines may be
+   called multiple times, but the number of closes should equal the
+   number of opens. */
+
+extern int dlopen_papi();
+extern int dlclose_papi();
+
+
+/* PAPI entry points */
+
+typedef int (*dl_PAPI_is_initialized_t)(void);
+extern dl_PAPI_is_initialized_t dl_PAPI_is_initialized;
+
+typedef int (*dl_PAPI_library_init_t)(int);
+extern dl_PAPI_library_init_t dl_PAPI_library_init;
+
+typedef int (*dl_PAPI_get_opt_t)(int, PAPI_option_t*);
+extern dl_PAPI_get_opt_t dl_PAPI_get_opt;
+
+typedef const PAPI_hw_info_t* (*dl_PAPI_get_hardware_info_t)(void);
+extern dl_PAPI_get_hardware_info_t dl_PAPI_get_hardware_info;
+
+typedef int (*dl_PAPI_get_event_info_t)(int, PAPI_event_info_t*);
+extern dl_PAPI_get_event_info_t dl_PAPI_get_event_info;
+
+typedef int (*dl_PAPI_enum_event_t)(int*, int);
+extern dl_PAPI_enum_event_t dl_PAPI_enum_event;
+
+#ifdef __cplusplus
+}
+#endif
+
+/****************************************************************************/
+
+#endif /* dlpapi_h */
