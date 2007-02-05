a31877059b1342c8f97f4af5ca4766559ebba33e
eraxxon <eraxxon@b1b131fe-c5db-9c05-3d80-cf3d96147e2e>
2007-02-05 22:34:02 +0000

    Minor cleanup.

--- branches/4.2.1-optimized-code-redesign/src/hpctoolkit/hpcview/Args.cpp
+++ branches/4.2.1-optimized-code-redesign/src/hpctoolkit/hpcview/Args.cpp
@@ -38,13 +38,13 @@
 //***************************************************************************
 //
 // File:
-//    Args.C
+//   $Source$
 //
 // Purpose:
-//    [The purpose of this file]
+//   [The purpose of this file]
 //
 // Description:
-//    [The set of functions, macros, etc. defined in the file]
+//   [The set of functions, macros, etc. defined in the file]
 //
 //***************************************************************************
 
@@ -183,11 +183,13 @@ Args::Args()
   Ctor();
 }
 
+
 Args::Args(int argc, const char* const argv[])
 {
   Ctor();
   Parse(argc, argv);
 }
+
 
 void
 Args::Ctor()
