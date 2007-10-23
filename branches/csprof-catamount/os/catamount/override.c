6cb4521d7c89739de5b67eb4400dcb220eb79547
johnmc <johnmc@b1b131fe-c5db-9c05-3d80-cf3d96147e2e>
2007-10-23 22:34:12 +0000

    add support for statically wrapping malloc and friends

--- branches/csprof-catamount/os/catamount/override.c
+++ branches/csprof-catamount/os/catamount/override.c
@@ -1,30 +1,31 @@
+#include <assert.h>
 #include <signal.h>
 #include <stdlib.h>
 
-extern int real_sigprocmask(int, const sigset_t *, sigset_t *);
-extern int real_sigaction(int sig, struct sigaction *act,
+extern int __real_sigprocmask(int, const sigset_t *, sigset_t *);
+extern int __real_sigaction(int sig, struct sigaction *act,
 			  struct sigaction *oact);
-extern sig_t real_signal(int sig, sig_t func);
+extern sig_t __real_signal(int sig, sig_t func);
 
-extern void *real_calloc(size_t nmemb, size_t size);
-extern void *real_malloc(size_t size);
-extern void *real_realloc(void *ptr, size_t size);
+extern void *__real_calloc(size_t nmemb, size_t size);
+extern void *__real_malloc(size_t size);
+extern void *__real_realloc(void *ptr, size_t size);
 
-int sigprocmask(int mask, const sigset_t *set1, sigset_t *set2){
-  return real_sigprocmask(mask,set1,set2);
+int __wrap_sigprocmask(int mask, const sigset_t *set1, sigset_t *set2){
+  return __real_sigprocmask(mask,set1,set2);
 }
-int sigaction(int sig, const struct sigaction *act,struct sigaction *oact){
-  return real_sigaction(sig,act,oact);
+int __wrap_sigaction(int sig, const struct sigaction *act,struct sigaction *oact){
+  return __real_sigaction(sig,act,oact);
 }
-sig_t signal(int sig, sig_t func){
-  return real_signal(sig,func);
+sig_t __wrap_signal(int sig, sig_t func){
+  return __real_signal(sig,func);
 }
 
-void *calloc(size_t nmemb, size_t size){
-  return real_calloc(nmemb,size);
+void *__wrap_calloc(size_t nmemb, size_t size){
+  return __real_calloc(nmemb,size);
 }
-void *realloc(void *ptr, size_t size){
-  return real_realloc(ptr,size);
+void *__wrap_realloc(void *ptr, size_t size){
+  return __real_realloc(ptr,size);
 }
 
 /* override various functions here */
@@ -32,12 +33,13 @@ void *realloc(void *ptr, size_t size){
 int _csprof_in_malloc = 0;
 int csprof_need_more = 0;
 
-void *malloc(size_t s){
+void *__wrap_malloc(size_t s){
   void *alloc;
 
   _csprof_in_malloc = 1;
-  alloc = real_malloc(s);
+  alloc = __real_malloc(s);
   if (csprof_need_more){
+    assert(0);
     /* alloc more space here */
     csprof_need_more = 0;
   }
