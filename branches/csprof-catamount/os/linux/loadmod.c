0356857b6b9643c9d33c2a5978f22994c6b9b455
johnmc <johnmc@b1b131fe-c5db-9c05-3d80-cf3d96147e2e>
2008-02-06 04:52:31 +0000

    move loadmod.c and loadmod.h from processor/x86 directory to os/linux directory, where they should be.

--- branches/csprof-catamount/os/linux/loadmod.c
+++ branches/csprof-catamount/os/linux/loadmod.c
@@ -19,36 +19,104 @@ static int segment_is_executable(char *);
 static char *xname(char *);
 static unsigned long htoll(char *);
 static unsigned long segment_size(char *);
+static void segment_start_end(char *line, unsigned long long *start, unsigned long long *end);
 
 /* MWF added utility f dumping w.o. modifying epoch stuff */
 void
 csprof_dump_loaded_modules(void){
+#ifndef STATIC_ONLY
+  char filename[PATH_MAX];
+  char mapline[PATH_MAX+80];
+  char *p;
+  FILE *fs; 
+  pid_t pid = getpid();
+
+  sprintf(filename, "/proc/%d/maps", pid);
+  fs = fopen(filename, "r");
+
+  do {
+    p = fgets(mapline, PATH_MAX+80, fs);
+
+    if(p) {
+      if(segment_is_executable(mapline)) {
+	char *name = strdup(xname(mapline));
+	unsigned long offset = htoll(mapline);
+
+	MSG(CSPROF_MSG_EPOCH, "Load module %s loaded at %p",
+	    name, (void *) offset);
+      }
+    }
+  } while(p);
+#else
+  MSG(1,"STATIC ONLY: dump loaded modules = NOOP");
+#endif
+}
+
+
+int
+find_load_module(unsigned long long addr, 
+		 char *module_name,
+		 unsigned long long *start, 
+		 unsigned long long *end)
+{
+#ifndef STATIC_ONLY
     char filename[PATH_MAX];
     char mapline[PATH_MAX+80];
     char *p;
     FILE *fs;
     pid_t pid = getpid();
+    unsigned long long module_start, module_end;
 
-#ifndef STATIC_ONLY
     sprintf(filename, "/proc/%d/maps", pid);
     fs = fopen(filename, "r");
 
+    
+    module_name[0] = 0;
+    module_start   = ~(0ULL);
+    module_end     = 0;
     do {
         p = fgets(mapline, PATH_MAX+80, fs);
 
         if(p) {
             if(segment_is_executable(mapline)) {
-                char *name = strdup(xname(mapline));
-                unsigned long offset = htoll(mapline);
-		unsigned long thesize = segment_size(mapline);
+	      unsigned long long seg_start, seg_end;
+	      char *seg_name;
 
-		MSG(CSPROF_MSG_EPOCH, "Load module %s loaded at %p",
-		    name, (void *) offset);
+	      seg_name = xname(mapline);
+	      segment_start_end(mapline, &seg_start, &seg_end);
+
+	      if (strcmp(module_name, seg_name) != 0) {
+		/*--------------------------------------
+		 * at end of one module and beginning a
+		 * new module. test addr for inclusion in 
+		 * old module before beginning new one.
+		 *------------------------------------*/
+		if (module_start <= addr  && addr < module_end) {
+		  *start = module_start;
+		  *end = module_end;
+		  fclose(fs);
+		  return 1;
+		}
+		strcpy(module_name, xname(mapline));
+		module_start = seg_start;
+		module_end   = seg_end;
+	      } else {
+		if (seg_start < module_start) module_start = seg_start;
+		if (seg_end   > module_end)   module_end   = seg_end;
+	      }
             }
         }
     } while(p);
-#else
-    MSG(1,"STATIC ONLY: dump loaded modules = NOOP");
+    /*--------------------------------------
+     * test for inclusion in final module
+     *------------------------------------*/
+    if (module_start <= addr  && addr < module_end) {
+      *start = module_start;
+      *end = module_end;
+      fclose(fs);
+      return 1;
+    }
+    return 0;
 #endif
 }
 
@@ -56,13 +124,13 @@ void
 csprof_epoch_get_loaded_modules(csprof_epoch_t *epoch,
                                 csprof_epoch_t *previous_epoch)
 {
+#ifndef STATIC_ONLY
     char filename[PATH_MAX];
     char mapline[PATH_MAX+80];
     char *p;
     FILE *fs;
     pid_t pid = getpid();
 
-#ifndef STATIC_ONLY
     sprintf(filename, "/proc/%d/maps", pid);
     fs = fopen(filename, "r");
 
@@ -71,20 +139,22 @@ csprof_epoch_get_loaded_modules(csprof_epoch_t *epoch,
 
         if(p) {
             if(segment_is_executable(mapline)) {
-                char *name = strdup(xname(mapline));
+                char *segname = xname(mapline);
+		int segname_len = strlen(segname);
                 unsigned long offset = htoll(mapline);
                 /** HACK: /proc is different, no size, but end address **/
 		unsigned long thesize = segment_size(mapline);
                 csprof_epoch_module_t *newmod;
 
                 newmod = csprof_malloc(sizeof(csprof_epoch_module_t));
-                newmod->module_name = name;
+                newmod->module_name = 
+		  strcpy(csprof_malloc(segname_len + 1), segname);
                 newmod->mapaddr = (void *)offset;
                 newmod->vaddr = NULL;
 		newmod->size = thesize;
 
 		MSG(CSPROF_MSG_EPOCH, "Load module %s loaded at %p",
-		    name, (void *) offset);
+		    newmod->module_name, (void *) offset);
                 newmod->next = epoch->loaded_modules;
                 epoch->loaded_modules = newmod;
                 epoch->num_modules++;
@@ -92,10 +162,11 @@ csprof_epoch_get_loaded_modules(csprof_epoch_t *epoch,
         }
     } while(p);
 #else
+    csprof_epoch_module_t *newmod;
+
     MSG(1,"STATIC_ONLY: epoch loads no modules");
     MSG(1,"STATIC_ONLY: build fake initial epoch");
 
-    csprof_epoch_module_t *newmod;
 
     newmod = csprof_malloc(sizeof(csprof_epoch_module_t));
     newmod->module_name = csprof_get_executable_name();
@@ -116,7 +187,6 @@ csprof_epoch_get_loaded_modules(csprof_epoch_t *epoch,
 #endif
 }
 
-
 /* support procedures */
 
 static int
@@ -128,15 +198,30 @@ segment_is_executable(char *line)
     return (blank && *(blank + 3) == 'x' && slash);
 }
 
+
+static void 
+segment_start_end(char *line, unsigned long long *start, unsigned long long *end)
+{
+  char *dash;
+
+  *start = htoll(line);
+
+  dash = index(line, (int) '-');
+
+  *end = htoll(dash+1);
+}
+
+
 static unsigned long
 segment_size(char *line)
 {
-  unsigned long long start = htoll(line);
-  char *dash = index(line, (int) '-');
-  unsigned long long end = htoll(dash+1);
+  unsigned long long start, end;
+
+  segment_start_end(line, &start, &end);
 
   return end - start;
 }
+
 
 static char *
 xname(char *line)
