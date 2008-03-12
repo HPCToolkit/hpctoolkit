03b9c29b541aff7419bb73a4dc1f4994a9a9ad1d
johnmc <johnmc@b1b131fe-c5db-9c05-3d80-cf3d96147e2e>
2008-03-12 17:54:32 +0000

    remove root placeholder in the CCT, which is no longer needed since the sampling core enforces a set of conditions that guarantees that we have a tree rather than a forest.

--- branches/csprof-catamount/sampling/types/callstack/cct.c
+++ branches/csprof-catamount/sampling/types/callstack/cct.c
@@ -483,12 +483,6 @@ csprof_cct_insert_backtrace(csprof_cct_t *x, void *treenode, int metric_id,
 
 
   if (csprof_cct__isempty(x)) {
-    // introduce placeholder root node to prevent forests
-    x->tree_root = csprof_cct_node__create(frm->as_info, 0, 0, 0);
-    x->num_nodes = 1;
-  }
-
-  if (csprof_cct__isempty(x)) {
     // LUSH:FIXME: introduce bogus root to handle possible forests?
 
     x->tree_root = csprof_cct_node__create(frm->as_info, frm->ip, 
