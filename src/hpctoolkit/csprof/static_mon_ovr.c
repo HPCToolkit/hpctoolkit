a84304a4ad46f38e189014f0c489164ef1e723b2
johnmc <johnmc@b1b131fe-c5db-9c05-3d80-cf3d96147e2e>
2008-03-24 04:55:37 +0000

    Makefile.in 	change name from mon_ovr.c to libmonitor_upcalls.c mon_ovr.c 	renamed to libmonitor_upcalls.c libmonitor_upcalls.c 	renamed from mon_ovr.c static_mon_ovr.c 	adjust so it is consistent with new libmonitor init/fini process upcalls

--- src/hpctoolkit/csprof/static_mon_ovr.c
+++ src/hpctoolkit/csprof/static_mon_ovr.c
@@ -13,13 +13,18 @@ extern void csprof_fini_internal(void);
 // always need this variable, but only init thread support will turn it on
 int csprof_using_threads = 0;
 
-void monitor_init_process(char *process, int *argc, char **argv,unsigned pid){
+void *
+monitor_init_process(int *argc, char **argv, void *data)
+{
   M("monitor calling csprof_init_internal");
   csprof_set_executable_name(argv[0]);
   csprof_init_internal();
+  return data;
 }
 
-void monitor_fini_process(void){
+void 
+monitor_fini_process(void *data)
+{
   M("monitor calling csprof_fini_internal");
   csprof_fini_internal();
 }
