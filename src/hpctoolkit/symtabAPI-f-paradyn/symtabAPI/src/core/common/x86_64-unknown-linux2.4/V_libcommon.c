2d8788cfab2d00e3c9622bd6e3ff8794788e2715
mfagan <mfagan@b1b131fe-c5db-9c05-3d80-cf3d96147e2e>
2008-03-29 21:08:14 +0000

    rename libelf.so back to libelf.so.0 (it is what is in the elf info) Makefile.conf has a message about make.config.local Makefile changed to supply ln -s for libelf.so (if link not there)

--- src/hpctoolkit/symtabAPI-f-paradyn/symtabAPI/src/core/common/x86_64-unknown-linux2.4/V_libcommon.c
+++ src/hpctoolkit/symtabAPI-f-paradyn/symtabAPI/src/core/common/x86_64-unknown-linux2.4/V_libcommon.c
@@ -1,4 +1,4 @@
 /* Automated build version identification */
 extern const char V_libcommon[];
        const char V_libcommon[] =
-    "$common: v1.0-0 libcommon #0 2008/03/28 14:50 mfagan@andre1.cs.rice.edu $";
+    "$common: v1.0-0 libcommon #0 2008/03/29 15:31 mfagan@andre1.cs.rice.edu $";
