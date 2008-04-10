5fd6f6e9b3d61a16f98bb6fbbb3dff9cb2fe5bca
mfagan <mfagan@b1b131fe-c5db-9c05-3d80-cf3d96147e2e>
2008-04-10 18:32:57 +0000

    remove symbols symtabAPI (separate projects now)

--- trunk/src/hpctoolkit/csprof/symbols/code-ranges.h
+++
@@ -1,5 +0,0 @@
-bool is_valid_code_address(void *addr);
-
-void new_code_range(void *start, void *end, long offset);
-
-void process_code_ranges(bool fn_discovery);
--- trunk/src/hpctoolkit/csprof/symbols/process-ranges.h
+++
@@ -1,6 +0,0 @@
-void process_range_init();
-void process_range(long offset, void *vstart, void *vend, bool fn_discovery);
-
-
-
-
