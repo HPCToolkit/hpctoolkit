c09ca53cc03f5e3e7ab8f7bf455b566163f75c94
mfagan <mfagan@b1b131fe-c5db-9c05-3d80-cf3d96147e2e>
2008-05-14 17:06:08 +0000

    delete fnbounds_server.[ch], as these have been replaced by   system_server.[ch]
    Alter system_server.c to use NMSG, EXIT_ON_ERROR instead of printf

--- trunk/src/hpctoolkit/csprof/system_server.c
+++ trunk/src/hpctoolkit/csprof/system_server.c
@@ -33,15 +33,17 @@
 //-------------
 #include "monitor.h"
 
+#include "pmsg.h"
 #include "system_server.h"
 
 
+#if 0
 //*******************************************************************************
 // forward declarations
 //*******************************************************************************
 
 static void exit_on_error(int ret, int ret_expected, char *format, ...);
-
+#endif
 
 
 //*******************************************************************************
@@ -82,17 +84,17 @@ system_server_start()
   // set up pipes for communication between client and server
   // --------------------------------------------------------------------------------------------------
   ret = pipe(fnbounds_server_pipes[CLIENT_TO_SERVER]);
-  exit_on_error(ret, 0, "fnbounds server: failed to create 1st pipe\n");
+  EXIT_ON_ERROR(ret, 0, "fnbounds server: failed to create 1st pipe");
 
   ret = pipe(fnbounds_server_pipes[SERVER_TO_CLIENT]);
-  exit_on_error(ret, 0, "fnbounds server: failed to create 2nd pipe\n");
+  EXIT_ON_ERROR(ret, 0, "fnbounds server: failed to create 2nd pipe");
 
   // --------------------------------------------------------------------------------------------------
   // fork server from client
   // --------------------------------------------------------------------------------------------------
   server_pid = monitor_real_fork();
   if (server_pid == 0) { /* child process */
-     printf("in child\n");
+     NMSG(SYSTEM_SERVER,"in child");
 
      // --------------------------------------------------------------------------------------------------
      // avoid monitoring processes that we spawn with system
@@ -103,7 +105,7 @@ system_server_start()
      // server: close unused file descriptor to send data to self 
      // --------------------------------------------------------------------------------------------------
      ret = close(WRITE_FD(CLIENT_TO_SERVER));
-     exit_on_error(ret, 0, "fnbounds server: failed to close pipe in server\n");
+     EXIT_ON_ERROR(ret, 0, "fnbounds server: failed to close pipe in server");
 
      for(;;) {
        int nbytes;
@@ -112,10 +114,10 @@ system_server_start()
        // receive command string length from client
        // --------------------------------------------------------------------------------------------------
        ret = read(READ_FD(CLIENT_TO_SERVER), &nbytes, sizeof(nbytes));
-       exit_on_error(ret, sizeof(nbytes), "fnbounds server fatal error: read failed\n");
+       EXIT_ON_ERROR(ret, sizeof(nbytes), "fnbounds server fatal error: read failed");
 
        if (nbytes > 0) {
-          printf("fnbounds server: expecting command of %d bytes\n", nbytes);
+          TMSG(SYSTEM_SERVER,"fnbounds server: expecting command of %d bytes", nbytes);
 
 	  // --------------------------------------------------------------------------------------------------
 	  // allocate space for command string 
@@ -126,8 +128,8 @@ system_server_start()
 	  // receive command string from client
 	  // --------------------------------------------------------------------------------------------------
           ret = read(READ_FD(CLIENT_TO_SERVER), command, nbytes);
-          exit_on_error(ret, nbytes, "fnbounds server fatal error: "
-	                "read only %d of %d bytes for a command %d\n", ret, nbytes);
+          EXIT_ON_ERROR(ret, nbytes, "fnbounds server fatal error: "
+	                "read only %d of %d bytes for a command %d", ret, nbytes);
 
 	  // --------------------------------------------------------------------------------------------------
 	  // execute command 
@@ -136,8 +138,8 @@ system_server_start()
 	  //             for fork and exec as part of system because monitor has 
 	  //             control of server process 
 	  // --------------------------------------------------------------------------------------------------
-	  printf("executing command: '%s'\n", command);
-          int status = __libc_system(command);
+	  TMSG(SYSTEM_SERVER,"executing command: '%s'\n", command);
+          int status = monitor_real_system(command);
 
 	  // --------------------------------------------------------------------------------------------------
 	  // send command result to client
@@ -150,7 +152,7 @@ system_server_start()
 	  free(command);
        } else {
 	 if (nbytes != server_done) {
-            exit_on_error(nbytes, server_done, "fnbounds server fatal error: "
+            EXIT_ON_ERROR(nbytes, server_done, "fnbounds server fatal error: "
 			  "received negative length %d for command string", nbytes);
 	 }
 	 // --------------------------------------------------------------------------------------------------
@@ -168,7 +170,7 @@ system_server_start()
   // client: close unused file descriptor to send data to self 
   // --------------------------------------------------------------------------------------------------
   ret = close(WRITE_FD(SERVER_TO_CLIENT));
-  exit_on_error(ret, 0, "fnbounds server: failed to close pipe in client\n");
+  EXIT_ON_ERROR(ret, 0, "fnbounds server: failed to close pipe in client");
 
   return (server_pid != -1) ? 0 : -1;
 }
@@ -181,7 +183,7 @@ system_server_shutdown()
   // send command string length value of -1 to terminate 
   // --------------------------------------------------------------------------------------------------
   int ret = write(WRITE_FD(CLIENT_TO_SERVER), &server_done, sizeof(server_done));
-  exit_on_error(ret, sizeof(server_done), "fnbounds server shutdown failed\n");
+  EXIT_ON_ERROR(ret, sizeof(server_done), "fnbounds server shutdown failed");
 }
  
 
@@ -194,21 +196,21 @@ system_server_execute_command(char *command)
   // --------------------------------------------------------------------------------------------------
   // send command string length to server
   // --------------------------------------------------------------------------------------------------
-  printf("fnbounds server: sending command size = %d bytes\n", len);
+  TMSG(SYSTEM_COMMAND,"fnbounds server: sending command size = %d bytes", len);
   ret = write(WRITE_FD(CLIENT_TO_SERVER), &len, sizeof(len));
-  exit_on_error(ret, sizeof(len), "fnbounds server fatal error: write of command size failed\n");
+  EXIT_ON_ERROR(ret, sizeof(len), "fnbounds server fatal error: write of command size failed");
 
   // --------------------------------------------------------------------------------------------------
   // send command string to server
   // --------------------------------------------------------------------------------------------------
   ret = write(WRITE_FD(CLIENT_TO_SERVER), command, len);
-  exit_on_error(ret, len, "fnbounds server fatal error: write command failed\n");
+  EXIT_ON_ERROR(ret, len, "fnbounds server fatal error: write command failed");
 
   // --------------------------------------------------------------------------------------------------
   // receive command result from server 
   // --------------------------------------------------------------------------------------------------
   ret = read(READ_FD(SERVER_TO_CLIENT), &status, sizeof(status));
-  exit_on_error(ret, sizeof(status), "fnbounds server fatal error: read status failed\n");
+  EXIT_ON_ERROR(ret, sizeof(status), "fnbounds server fatal error: read status failed\n");
 
   return status;
 }
@@ -217,7 +219,7 @@ system_server_execute_command(char *command)
 // private operations  
 //*******************************************************************************
 
-
+#if 0
 static void 
 exit_on_error(int ret, int ret_expected, char *format, ...) 
 {
@@ -229,6 +231,8 @@ exit_on_error(int ret, int ret_expected, char *format, ...)
   }
   va_end(args);
 }
+
+#endif
 	
 #if 0
 main()
