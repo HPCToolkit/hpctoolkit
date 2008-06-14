05f52aebaa99684950f818450007ed29f6a1c5c1
johnmc <johnmc@b1b131fe-c5db-9c05-3d80-cf3d96147e2e>
2008-06-14 14:51:24 +0000

    processor/x86-64/backtrace.c backtrace.h 	clean out cruft in preparation for platform independence

--- src/hpctoolkit/csprof/backtrace.h
+++ src/hpctoolkit/csprof/backtrace.h
@@ -1,41 +1,43 @@
 // -*-Mode: C++;-*- // technically C99
 // $Id$
 
-/* backtrace.h -- an interface for performing stack unwinding */
 
 #ifndef CSPROF_BACKTRACE_H
 #define CSPROF_BACKTRACE_H
 
-//************************* System Include Files ****************************
+//***************************************************************************
+// file: backtrace.h
+//
+// purpose:
+//     an interface for performing stack unwinding 
+//
+//***************************************************************************
+
+
+//***************************************************************************
+// system include files 
+//***************************************************************************
 
 #include <stddef.h>
 #include <ucontext.h>
 
-//*************************** User Include Files ****************************
+
+
+//***************************************************************************
+// local include files 
+//***************************************************************************
 
 #include "state.h"
 
-//*************************** Forward Declarations **************************
 
-// Amount to chop off call stack samples b/c of the profiler.
-// Beginning from top:
-//   csprof_backtrace()
-//   csprof_sample_callstack()
-//   csprof_take_profile_sample()
-//   csprof_signal_handler()
-//   libc_signal_trampoline()
-/* FIXME: depending on how the whole trampoline_backend thing falls out,
-   this will probably need to be changed for *both* backends */
-#if defined(CSPROF_SETJMP_SUPPORT)
-#define CSPROF_CS_CHOP 5
-#else
-#define CSPROF_CS_CHOP 4
-#endif
+
+//***************************************************************************
+// forward declarations
+//***************************************************************************
 
 csprof_cct_node_t*
 csprof_sample_callstack(csprof_state_t *state, ucontext_t* context, 
 			int metric_id, size_t sample_count);
 
-//***************************************************************************
 
 #endif
