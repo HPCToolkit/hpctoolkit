a7618d2925c547d7bf58ccf59d68a5f5e5f2bb3d
johnmc <johnmc@b1b131fe-c5db-9c05-3d80-cf3d96147e2e>
2008-06-14 14:51:24 +0000

    processor/x86-64/backtrace.c backtrace.h 	clean out cruft in preparation for platform independence

--- trunk/src/hpctoolkit/csprof/processor/x86-64/backtrace.c
+++ trunk/src/hpctoolkit/csprof/processor/x86-64/backtrace.c
@@ -1,22 +1,19 @@
 // -*-Mode: C++;-*- // technically C99
 // $Id$
-/* x86-64_backtrace.c -- unwinding the stack on x86-64 w/ frame pointers */
 
-//************************* System Include Files ****************************
+
+//***************************************************************************
+// system include files
+//***************************************************************************
 
 #include <sys/types.h>
 #include <ucontext.h>
 
-/* FIXME: configure backtrace to use libunwind if that is wanted
-   FORNOW: just use binary analyzer
-#ifndef PRIM_UNWIND
-#include <libunwind.h>
-#else
-#include "unwind.h"
-#endif
-*/
 
-//*************************** User Include Files ****************************
+
+//***************************************************************************
+// local include files
+//***************************************************************************
 
 #include "unwind.h"
 
@@ -28,16 +25,16 @@
 #include "monitor.h"
 #include "sample_event.h"
 
-//*************************** Forward Declarations **************************
 
-#define USE_LIBUNWIND_TO_START 0
 
-extern void _start();
+//***************************************************************************
+// forward declarations 
+//***************************************************************************
 
-#if 1
-#define debug_dump_backtraces(x,y)
-#else
+#ifdef DEBUG
 #define debug_dump_backtraces(x,y) dump_backtraces(x,y)
+#else
+#define debug_dump_backtraces(x,y)
 #endif
 
 
@@ -45,93 +42,58 @@ static csprof_cct_node_t*
 csprof_sample_callstack_from_frame(csprof_state_t *, int,
 				   size_t, unw_cursor_t *);
 
+
+
+//***************************************************************************
+// interface functions
 //***************************************************************************
 
-
-
-
-/* FIXME: some of the checks from libunwind calls are checked for errors.
-   I suppose that's a good thing, but there should be some way (CSPROF_PERF?)
-   to turn off the checking to avoid branch prediction penalties...
-
-   Then again, branch prediction penalities after you've already made a
-   function call and decoded DWARF unwind info and written several words
-   of memory...maybe we don't care that much. */
-
-// If successful, returns the leaf node representing the sample;
-// otherwise, returns NULL.
+//-----------------------------------------------------------------------------
+// function: csprof_sample_callstack
+// purpose:
+//     if successful, returns the leaf node representing the sample;
+//     otherwise, returns NULL.
+//-----------------------------------------------------------------------------
 csprof_cct_node_t*
 csprof_sample_callstack(csprof_state_t *state, ucontext_t* context, 
 			int metric_id, size_t sample_count)
 {
-#if 0
-  int first_ever_unwind = (state->bufstk == state->bufend);
-  void *sp1 = first_ever_unwind ? (void *) -1 : state->bufstk->sp;
-#endif
-
-#if 0
-#ifndef PRIM_UNWIND
-  unw_context_t unwctxt;
-#endif
-#endif
-
   unw_cursor_t frame;
 
   csprof_state_verify_backtrace_invariants(state);
 
-#if 0
-#if USE_LIBUNWIND_TO_START
-  /* would be nice if we could just copy in the signal context... */
-  if(unw_getcontext(&unwctxt) < 0) {
-    DIE("Could not initialize unwind context!", __FILE__, __LINE__);
-  }
-#else
-#ifndef PRIM_UNWIND
-  memcpy(&unwctxt.uc_mcontext, mctxt, sizeof(mcontext_t));
-#else
-  unw_init_mcontext(mctxt,&frame);
-  MSG(1,"back from cursor init: pc = %p, bp = %p\n",frame.pc,frame.bp);
-#endif
-#endif
-#endif
-
   unw_init_cursor(context, &frame);
   MSG(1,"back from cursor init: pc = %p, bp = %p\n",frame.pc,frame.bp);
-
-#if 0
-#ifndef PRIM_UNWIND
-  if(unw_init_local(&frame, &unwctxt) < 0) {
-    DIE("Could not initialize unwind cursor!", __FILE__, __LINE__);
-  }
-#endif
-#if USE_LIBUNWIND_TO_START
-  {
-    int i;
-
-    /* unwind through libcsprof and the signal handler */
-    for(i=0; i<CSPROF_SAMPLE_CALLSTACK_DEPTH; ++i) {
-      if(unw_step(&frame) < 0) {
-	DIE("An error occurred while unwinding", __FILE__, __LINE__);
-      }
-    }
-  }
-#endif
-#endif
 
   csprof_cct_node_t* n;
   n = csprof_sample_callstack_from_frame(state, metric_id,
 					 sample_count, &frame);
-#if 0
-  if (!n){
-    ERRMSG("failure recording callstack sample", __FILE__, __LINE__);
-  }
-#endif
+
   return n;
 }
 
 
+
+//***************************************************************************
+// private operations 
+//***************************************************************************
+
+//---------------------------------------------------------------------------
+// function: csprof_sample_filter
+//
+// purpose:
+//     ignore any samples that aren't rooted at designated call sites in 
+//     monitor that should be at the base of all process and thread call 
+//     stacks. 
+//
+// implementation notes:
+//     to support this, in monitor we define a pair of labels surrounding the 
+//     call site of interest. it is possible to get a sample between the pair 
+//     of labels that is outside the call. in that case, the length of the 
+//     sample's callstack would be 1, and we ignore it.
+//-----------------------------------------------------------------------------
 static int
-csprof_sample_filter(int len,csprof_frame_t *start, csprof_frame_t *last)
+csprof_sample_filter(int len, csprof_frame_t *start, csprof_frame_t *last)
 {
   return (! ( monitor_in_start_func_narrow(last->ip) && (len > 1) ));
 }
@@ -142,10 +104,14 @@ csprof_sample_callstack_from_frame(csprof_state_t *state, int metric_id,
 				   size_t sample_count, unw_cursor_t *cursor)
 {
   unw_word_t ip;
-  int first = 1;
   int ret;
 
-  // FIXME: why cannot some of these be local variables
+  //--------------------------------------------------------------------
+  // note: these variables are not local variables so that if a SIGSEGV 
+  // occurs and control returns up several procedure frames, the values 
+  // are accessible to a dumping routine that will tell us where we ran 
+  // into a problem.
+  //--------------------------------------------------------------------
   state->unwind   = state->btbuf;
   state->bufstk   = state->bufend;
   state->treenode = NULL;
@@ -157,11 +123,6 @@ csprof_sample_callstack_from_frame(csprof_state_t *state, int metric_id,
     if (unw_get_reg(cursor, UNW_REG_IP, &ip) < 0) {
       MSG(1,"get_reg break");
       break;
-    }
-
-    if (first){
-      MSG(1,"BTIP = %lp",(void *)ip);
-      /*        first = 0; */
     }
 
     state->unwind_pc = (void *) ip; // mark starting point in case of failure
