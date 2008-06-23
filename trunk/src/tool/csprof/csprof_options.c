d93fb74f184ae2ca803956915957d7ab3ba8827c
johnmc <johnmc@b1b131fe-c5db-9c05-3d80-cf3d96147e2e>
2008-06-23 15:27:59 +0000

    csprof_options.c 	move handling for output file to files.c 	this was necessary because option processing can't all occur at the same time anymore. 	output files have to be opened before papi initialized.

--- trunk/src/tool/csprof/csprof_options.c
+++ trunk/src/tool/csprof/csprof_options.c
@@ -6,11 +6,12 @@
 
 #include "all_sample_sources.h"
 #include "csprof_options.h"
-#include "registered_sample_sources.h"
+#include "files.h"
 #include "process_event_list.h"
 #include "csprof_misc_fn_stat.h"
 #include "env.h"
 #include "pmsg.h"
+#include "registered_sample_sources.h"
 
 /* option handling */
 /* FIXME: this needs to be split up a little bit for different backends */
@@ -33,40 +34,13 @@ csprof_options__fini(csprof_options_t* x)
 int
 csprof_options__getopts(csprof_options_t* x)
 {
-
-  char tmp[CSPROF_PATH_SZ];
-  char* s;
-  int i = 0;
-
   /* Option: CSPROF_OPT_LUSH_AGENTS */
-  s = getenv(CSPROF_OPT_LUSH_AGENTS);
+  char *s = getenv(CSPROF_OPT_LUSH_AGENTS);
   if (s) {
     strcpy(x->lush_agent_paths, s);
   }
   else {
     x->lush_agent_paths[0] = '\0';
-  }
-
-  /* Option: CSPROF_OPT_OUT_PATH */
-  s = getenv(CSPROF_OPT_OUT_PATH);
-  if (s) {
-    i = strlen(s);
-    if(i==0) {
-      strcpy(tmp, ".");
-    }
-    if((i + 1) > CSPROF_PATH_SZ) {
-      EMSG("value of option `%s' [%s] has a length greater than %d",CSPROF_OPT_OUT_PATH, s, CSPROF_PATH_SZ);
-      abort();
-    }
-    strcpy(tmp, s);
-  }
-  else {
-    strcpy(tmp, ".");
-  }
-  
-  if (realpath(tmp, x->out_path) == NULL) {
-    EMSG("could not access path `%s': %s", tmp, strerror(errno));
-    abort();
   }
 
   // process event list
