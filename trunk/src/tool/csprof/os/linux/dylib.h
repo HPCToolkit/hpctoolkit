a5ec9665e84205de3d428ca7aea43b5faf7eeea3
eraxxon <eraxxon@b1b131fe-c5db-9c05-3d80-cf3d96147e2e>
2008-06-23 19:46:12 +0000

    Insert a few comments.

--- trunk/src/tool/csprof/os/linux/dylib.h
+++ trunk/src/tool/csprof/os/linux/dylib.h
@@ -8,7 +8,9 @@ void dylib_map_executable();
 int dylib_addr_is_mapped(unsigned long long addr) ;
 
 
+// tallent: FIXME(double check): why not use a void* or uintptr_t instead of a 'long long'?  The latter tends to only bring trouble during ports.
 int dylib_find_module_containing_addr(unsigned long long addr, 
+				      // output parameters
 				      char *module_name,
 				      unsigned long long *start, 
 				      unsigned long long *end);
