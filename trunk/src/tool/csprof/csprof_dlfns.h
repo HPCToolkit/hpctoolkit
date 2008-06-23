d5fdead276a3cc276a24724ecb5397b075cbffe8
johnmc <johnmc@b1b131fe-c5db-9c05-3d80-cf3d96147e2e>
2008-06-23 15:25:35 +0000

    csprof_dlfns.c csprof_dlfns.h 	add const to an argument to avoid compiler warnings

--- trunk/src/tool/csprof/csprof_dlfns.h
+++ trunk/src/tool/csprof/csprof_dlfns.h
@@ -1,4 +1,4 @@
 void csprof_pre_dlopen();
 int csprof_dlopen_pending();
-void csprof_dlopen(char *module_name, int flags, void *handle);
+void csprof_dlopen(const char *module_name, int flags, void *handle);
 void csprof_dlclose(void *handle);
