0653139a8433d7f23245996df639f8ab242afd36
johnmc <johnmc@b1b131fe-c5db-9c05-3d80-cf3d96147e2e>
2008-08-05 22:52:22 +0000

    adjust the unstripper so that it yields fewer false positives by: 	- don't suggest as a function start the instruction following the sequence CALL; JMP 	- don't suggest an instruction as a function start if it follows an unconditional 	  control transfer and has the signature mov rax, ... 	- adjust suggested function starts to use address after an unconditional control 	  transfer. don't add a function start before pad bytes if there is one after them. 	- if a backward branch branches to an instruction following pad bytes, extend the 	  protected range to include the pad bytes as well. 	- don't suggest an unaligned function start.

--- trunk/src/tool/symbols/function-entries.h
+++ trunk/src/tool/symbols/function-entries.h
@@ -4,6 +4,7 @@ using namespace std;
 
 void add_function_entry(void *address, const string *comment);
 void add_stripped_function_entry(void *function_entry);
+bool contains_function_entry(void *address);
 
 void add_branch_range(void *start, void *end);
 int is_possible_fn(void *addr);
--- trunk/src/tool/symbols/x86-process-ranges.cpp
+++ trunk/src/tool/symbols/x86-process-ranges.cpp
@@ -17,16 +17,29 @@ extern "C" {
 
 
 /******************************************************************************
+ * macros
+ *****************************************************************************/
+
+#define is_aligned(addr) ((((unsigned long) (addr)) & 0x3) == 0) 
+
+
+#define is_call_iclass(x) \
+   ((x == XED_ICLASS_CALL_FAR) || (x == XED_ICLASS_CALL_NEAR))
+
+
+
+/******************************************************************************
  * forward declarations 
  *****************************************************************************/
 
-static void process_call(char *ins, xed_decoded_inst_t *xptr, long offset, 
+static void process_call(char *ins, long offset, xed_decoded_inst_t *xptr,
 			 void *start, void *end);
 
-static void process_branch(char *ins, xed_decoded_inst_t *xptr);
+static void process_branch(char *ins, long offset, xed_decoded_inst_t *xptr);
 
-static void after_unconditional(long offset, char *ins, xed_decoded_inst_t *xptr);
+static void after_unconditional(char *ins, long offset, xed_decoded_inst_t *xptr);
 
+static bool is_mov_rax(unsigned char *ins);
 
 
 /******************************************************************************
@@ -58,8 +71,10 @@ process_range(long offset, void *vstart, void *vend, bool fn_discovery)
   int error_count = 0;
   char *ins = (char *) vstart;
   char *end = (char *) vend;
+  
 
   xed_decoded_inst_zero_set_mode(xptr, &xed_machine_state_x86_64);
+  xed_iclass_enum_t prev_xiclass = XED_ICLASS_INVALID;
 
   while (ins < end) {
 
@@ -76,14 +91,21 @@ process_range(long offset, void *vstart, void *vend, bool fn_discovery)
     switch(xiclass) {
     case XED_ICLASS_CALL_FAR:
     case XED_ICLASS_CALL_NEAR:
-      /* if (fn_discovery) */ process_call(ins, xptr, offset, vstart, vend);
+      /* if (fn_discovery) */ process_call(ins, offset, xptr, vstart, vend);
       break;
 
     case XED_ICLASS_JMP: 
     case XED_ICLASS_JMP_FAR:
+      if (fn_discovery  && !is_call_iclass(prev_xiclass)) {
+        // regarding use of !is_call above: don't infer function start 
+        // if we run into code from C++ that consists of a call followed 
+        // by an unconditional jump
+	after_unconditional(ins, offset, xptr);
+      }
+      break;
     case XED_ICLASS_RET_FAR:
     case XED_ICLASS_RET_NEAR:
-      if (fn_discovery) after_unconditional(offset, ins ,xptr);
+      if (fn_discovery) after_unconditional(ins, offset, xptr);
       break;
 
     case XED_ICLASS_JB:
@@ -103,12 +125,13 @@ process_range(long offset, void *vstart, void *vend, bool fn_discovery)
     case XED_ICLASS_JRCXZ:
     case XED_ICLASS_JS:
     case XED_ICLASS_JZ:
-      if (fn_discovery) process_branch(ins + offset, xptr);
+      if (fn_discovery) process_branch(ins, offset , xptr);
       break;
 
     default:
       break;
     }
+    prev_xiclass = xiclass;
 
     ins += xed_decoded_inst_get_length(xptr);
   }
@@ -128,12 +151,19 @@ is_padding(int c)
 
 
 static void 
-after_unconditional(long offset, char *ins, xed_decoded_inst_t *xptr)
+after_unconditional(char *ins, long offset, xed_decoded_inst_t *xptr)
 {
   ins += xed_decoded_inst_get_length(xptr);
   unsigned char *uins = (unsigned char *) ins;
+  unsigned char *potential_function_addr = uins + offset;
   for (; is_padding(*uins); uins++); // skip remaining padding 
-  add_stripped_function_entry(uins + offset); // potential function entry point
+
+  // only infer a function entry before padding bytes if there isn't already one after padding bytes
+  if (contains_function_entry(uins + offset) == false) {
+    if (is_aligned(uins + offset) && !is_mov_rax(uins)) {
+      add_stripped_function_entry(potential_function_addr); // potential function entry point
+    }
+  }
 }
 
 
@@ -159,8 +189,7 @@ get_branch_target(char *ins, xed_decoded_inst_t *xptr, xed_operand_values_t *val
 
 
 static void 
-process_call(char *ins, xed_decoded_inst_t *xptr, long offset, void *start, 
-	     void *end)
+process_call(char *ins, long offset, xed_decoded_inst_t *xptr, void *start, void *end)
 { 
   const xed_inst_t *xi = xed_decoded_inst_inst(xptr);
   const xed_operand_t *op0 =  xed_inst_operand(xi, 0);
@@ -181,8 +210,9 @@ process_call(char *ins, xed_decoded_inst_t *xptr, long offset, void *start,
 
 
 static void 
-process_branch(char *ins, xed_decoded_inst_t *xptr)
+process_branch(char *ins, long offset, xed_decoded_inst_t *xptr)
 { 
+  char *relocated_ins = ins + offset; 
   const xed_inst_t *xi = xed_decoded_inst_inst(xptr);
   const xed_operand_t *op0 =  xed_inst_operand(xi, 0);
   xed_operand_enum_t   op0_name = xed_operand_name(op0);
@@ -193,16 +223,63 @@ process_branch(char *ins, xed_decoded_inst_t *xptr)
     xed_operand_values_t *vals = xed_decoded_inst_operands(xptr);
 
     if (xed_operand_values_has_branch_displacement(vals)) {
-      void *target = get_branch_target(ins,xptr,vals);
+      char *target = (char *) get_branch_target(relocated_ins, xptr, vals);
       void *start, *end;
-      if (target < ins) {
+      if (target < relocated_ins) {
+        unsigned char *tloc = (unsigned char *) target - offset;
+        for (; is_padding(*(tloc-1)); tloc--) { // extend branch range to before padding
+            target--;
+	}
 	start = target;
-	end = ins; 
+	end = relocated_ins; 
       } else {
-	start = ins;
+	start = relocated_ins;
 	end = ((char *) target) + 1; // add one to ensure that the branch target is part of the "covered" range
       }
       add_branch_range(start, end);
     }
   }
 }
+
+
+
+static bool
+is_mov_rax(unsigned char *ins)
+{
+  xed_decoded_inst_t xdi;
+
+  xed_decoded_inst_zero_set_mode(&xdi, &xed_machine_state_x86_64);
+  xed_error_enum_t xed_error = xed_decode(&xdi, (uint8_t*) ins, 15);
+
+  if ((xed_error == XED_ERROR_NONE) && 
+      (xed_decoded_inst_get_iclass(&xdi) == XED_ICLASS_MOV)) {
+    const xed_inst_t *xi = xed_decoded_inst_inst(&xdi);
+    const xed_operand_t *op1 =  xed_inst_operand(xi, 1);
+    xed_operand_enum_t   op1_name = xed_operand_name(op1);
+
+    if ((op1_name == XED_OPERAND_REG1) && 
+	(xed_decoded_inst_get_reg(&xdi, op1_name) == XED_REG_RAX)) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+
+
+void x86_dump_ins(void *ins)
+{
+  xed_decoded_inst_t xedd;
+  xed_decoded_inst_t *xptr = &xedd;
+  xed_error_enum_t xed_error;
+  char inst_buf[1024];
+
+  xed_decoded_inst_zero_set_mode(xptr, &xed_machine_state_x86_64);
+  xed_error = xed_decode(xptr, (uint8_t*) ins, 15);
+
+  xed_format_xed(xptr, inst_buf, sizeof(inst_buf), (uint64_t) ins);
+  printf("(%p, %d bytes, %s) %s \n" , ins, xed_decoded_inst_get_length(xptr),
+         xed_iclass_enum_t2str(xed_decoded_inst_get_iclass(xptr)), inst_buf);
+}
+
