8e11e08963176055786eab49f57953b3010b9e91
krentel <krentel@b1b131fe-c5db-9c05-3d80-cf3d96147e2e>
2008-08-30 19:46:47 +0000

    Rework how we fetch the source tarfiles.  For tarfiles, the package Makefiles should set FETCH_TARFILE and FETCH_URL_1, etc, and may set multiple urls.
    We are now well setup for checking in any subset of the
    tarfiles in externals/distfiles.

--- trunk/externals/build-utils/fetch-ftp.sh
+++ trunk/externals/build-utils/fetch-ftp.sh
@@ -1,11 +1,23 @@
 #!/bin/sh
 #
-#  Download and unpack a tarfile using wget.
+#  Download and unpack a tarfile using wget.  The tarfile can be
+#  already downloaded in DISTDIR or externals/distfiles, or else this
+#  script will first download it and put it there.
+#
+#  This script is called from Makefile.inc from within the package
+#  directory, it is not used interactively.  The package Makefile
+#  should set FETCH_TARFILE, FETCH_URL_1, FETCH_URL_2, etc, and these
+#  are passed as follows.
+#
+#  Usage: fetch-ftp.sh FETCH_TARFILE FETCH_URL_1 ...
 #
 #  $Id$
 #
-url="$1"
-tarfile="$2"
+
+default_distdir=../distfiles
+tarfile="$1"
+url_1="$2"
+shift
 
 die()
 {
@@ -14,56 +26,93 @@ die()
 }
 
 #
-# Select directory to hold tarfiles as: DISTDIR environ variable,
-# or else distfiles directory.
+# Step 1 -- Search for the file in DISTDIR (if set) or else in
+# externals/distfiles.
 #
-if test ! -d "$DISTDIR" ; then
-    DISTDIR=../distfiles
+the_file=
+if test -d "$DISTDIR" && test -f "${DISTDIR}/${tarfile}" ; then
+    the_file="${DISTDIR}/${tarfile}"
+elif test -f "${default_distdir}/${tarfile}" ; then
+    the_file="${default_distdir}/${tarfile}"
+fi
+if test "x$the_file" != x ; then
+    echo "found tarfile: $the_file"
 fi
 
 #
-# Step 1 -- See if tarfile is in DISTDIR and download if not.
+# Step 2 -- If not found in step 1, then try the command-line
+# arguments (FETCH_URL_1. FETCH_URL_2, ...)  and save in DISTDIR or
+# externals/distfiles.
 #
-orig_dir=`pwd`
-mkdir -p "$DISTDIR"
-
-case "$url" in
-    ftp* | http* )
-	;;
-    * )
-	die "unknown url type: $url"
-	;;
-esac
-
-if test "x$tarfile" = x ; then
-    tarfile=`expr "$url" : '.*/\(.*\)'`
-fi
-test "x$tarfile" != x || die "unable to parse: $url"
-
-if test ! -f "${DISTDIR}/${tarfile}" ; then
-    if type wget >/dev/null 2>&1 ; then
-	cd "$DISTDIR" || die "unable to cd: $DISTDIR"
-	echo "wget $url"
-	wget "$url"
-	test $? -eq 0 || die "wget failed"
+if test "x$the_file" = x ; then
+    if type wget >/dev/null 2>&1 ; then :
     else
 	die "unable to find wget"
     fi
+
+    write_dir="$default_distdir"
+    if test -d "$DISTDIR" ; then
+	write_dir="$DISTDIR"
+    fi
+
+    for url in "$@"
+    do
+	if test "x$url" = x ; then
+	    continue
+	fi
+	echo "wget $url"
+	wget -t 4 -O "${write_dir}/${tarfile}" "$url"
+	if test $? -eq 0 && test -s "${write_dir}/${tarfile}" ; then
+	    the_file="${write_dir}/${tarfile}"
+	    break
+	else
+	    echo "wget failed: $url"
+	fi
+    done
 fi
 
 #
-# Step 2 -- Unpack the tarfile.
+# If unable to fetch the file, then tell the user what to try
+# manually.
 #
-cd "$orig_dir" || die "unable to cd: $orig_dir"
+if test "x$the_file" = x ; then
+    cat <<EOF
 
-case "$tarfile" in
-    *.tar.gz | *.tgz )
-	echo tar xvzf "${DISTDIR}/${tarfile}"
-	tar xvzf "${DISTDIR}/${tarfile}"
-	test $? -eq 0 || die "unable to untar: $tarfile"
+Unable to download the file: $tarfile
+Try downloading this file manually and putting it in the
+externals/distfiles directory and rerunning make fetch.
+
+  $url_1
+
+The package Makefile may have other URLs to try.
+
+EOF
+    die "make fetch failed"
+fi
+
+#
+# Step 3 -- Unpack the tarfile.
+#
+case "$the_file" in
+    *.tar )
+	echo tar xvf "$the_file"
+	tar -x -v -f "$the_file"
+	test $? -eq 0 || die "unable to untar: $the_file"
 	;;
+
+    *.tar.gz | *.tgz )
+	echo tar xvzf "$the_file"
+	tar -x -v -z -f "$the_file"
+	test $? -eq 0 || die "unable to untar: $the_file"
+	;;
+
+    *.tar.bz | *.tar.bz2 | *.tbz )
+	echo tar xvjf "$the_file"
+	tar -x -v -j -f "$the_file"
+	test $? -eq 0 || die "unable to untar: $the_file"
+	;;
+
     * )
 	die "unknown file format: $tarfile"
 	;;
 esac
-
