c4203ea01829e6ef2a2babd854f7ba19d8751433
krentel <krentel@b1b131fe-c5db-9c05-3d80-cf3d96147e2e>
2008-12-08 18:13:36 +0000

    Update hpcfnbounds-bin to allow three output formats: -b for binary format, -c for classic C format (legacy), and -t for text, allow writing to stdout or into a directory, and allow for multiple outputs in one run.
    Rewrite the hpcfnbounds wrapper script, add -v option (verbose) for
    internal debugging, and move it to the src/tool/hpcfnbounds directory.
    
    Remove the stripped output altogether, except in C format where it's
    the constant 0.

--- trunk/src/tool/hpcfnbounds/fnbounds-file-header.h
+++ trunk/src/tool/hpcfnbounds/fnbounds-file-header.h
@@ -6,6 +6,14 @@
 #define _FNBOUNDS_FILE_HEADER_
 
 //
+// Printf format strings for fnbounds file names and extensions.
+// The %s conversion args are directory and basename.
+//
+#define FNBOUNDS_BINARY_FORMAT  "%s/%s.fnbounds.bin"
+#define FNBOUNDS_C_FORMAT       "%s/%s.fnbounds.c"
+#define FNBOUNDS_TEXT_FORMAT    "%s/%s.fnbounds.txt"
+
+//
 // The extra info in the binary file of function addresses, written by
 // hpcfnbounds-bin and read in the main process.  We call it "header",
 // even though it's actually at the end of the file.
@@ -17,7 +25,6 @@ struct fnbounds_file_header {
     long magic;
     long num_entries;
     int  relocatable;
-    int  stripped;
 };
 
 #endif
--- trunk/src/tool/hpcfnbounds/symbols.cpp
+++ trunk/src/tool/hpcfnbounds/symbols.cpp
@@ -4,6 +4,12 @@
 
 #include <vector>
 #include <string>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <err.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -18,10 +24,6 @@ using namespace std;
 using namespace Dyninst;
 using namespace SymtabAPI;
 
-#define BINARY_FORMAT_ARG  "-b"  // write output in binary format
-#define NO_DISCOVERY_ARG   "-d"  // don't perform function discovery
-#define HELP_ARG           "-h"
-
 #define SECTION_SYMTAB ".symtab"
 #define SECTION_INIT   ".init"
 #define SECTION_FINI   ".fini"
@@ -31,7 +33,7 @@ using namespace SymtabAPI;
 #define PATHSCALE_EXCEPTION_HANDLER_PREFIX "Handler."
 #define STRLEN(s) (sizeof(s) - 1)
 
-static void usage(char *command);
+static void usage(char *command, int status);
 static void dump_file_info(const char *filename, bool fn_discovery);
 
 extern "C" {
@@ -46,42 +48,126 @@ return strdup(s);
 
 };
 
-static int use_binary_format = 0;
+// We write() the binary format to a file descriptor and fprintf() the
+// text and classic C formats to a FILE pointer.
+//
+static int   the_binary_fd = -1;
+static FILE *the_c_fp = NULL;
+static FILE *the_text_fp = NULL;
 
 
 //*****************************************************************
 // interface operations
 //*****************************************************************
 
+// Parse the options to determine which output formats to write, and
+// open the files or set to stdout.  We allow multiple formats, but
+// only one to stdout.
+//
 int 
 main(int argc, char **argv)
 {
   bool fn_discovery = 1;
+  char buf[PATH_MAX], *object_file, *output_dir, *base;
+  int num_fmts = 0, do_binary = 0, do_c = 0, do_text = 0;
   int n;
 
   for (n = 1; n < argc; n++) {
-    if (strcmp(argv[n], BINARY_FORMAT_ARG) == 0)
-      use_binary_format = 1;
-    else if (strcmp(argv[n], NO_DISCOVERY_ARG) == 0)
+    if (strcmp(argv[n], "-b") == 0) {
+      do_binary = 1;
+    }
+    else if (strcmp(argv[n], "-c") == 0) {
+      do_c = 1;
+    }
+    else if (strcmp(argv[n], "-d") == 0) {
       fn_discovery = 0;
-    else if (strcmp(argv[n], HELP_ARG) == 0)
-      usage(argv[0]);
+    }
+    else if (strcmp(argv[n], "-h") == 0 || strcmp(argv[n], "--help") == 0) {
+      usage(argv[0], 0);
+    }
+    else if (strcmp(argv[n], "-t") == 0) {
+      do_text = 1;
+    }
+    else if (strcmp(argv[n], "-v") == 0) {
+      // Ignored at this layer, used in hpcfnbounds script.
+    }
     else
       break;
   }
+  num_fmts = do_binary + do_c + do_text;
 
-  if (n >= argc)
-    usage(argv[0]);
+  // Must specify at least the object file.
+  if (n >= argc) {
+    usage(argv[0], 1);
+  }
+  object_file = argv[n];
+  base = rindex(object_file, '/');
+  base = (base == NULL) ? object_file : base + 1;
 
-  dump_file_info(argv[n], fn_discovery);
+  // If don't specify the output directory, then limited to one output
+  // format and stdout.
+  if (n + 1 >= argc && num_fmts > 1) {
+    fprintf(stderr,
+      "Error: when writing two or more output formats at the same time,\n"
+      "you must specify an output directory.\n\n");
+    usage(argv[0], 1);
+  }
+  output_dir = (n + 1 < argc) ? argv[n+1] : NULL;
 
+  // If no output formats given, then use text.
+  if (num_fmts == 0)
+    do_text = 1;
+
+  // For each output format, open the file or set to stdout.
+  if (output_dir != NULL) {
+    if (do_binary) {
+      sprintf(buf, FNBOUNDS_BINARY_FORMAT, output_dir, base);
+      the_binary_fd = open(buf, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+      if (the_binary_fd < 0)
+        err(1, "open failed on: %s", buf);
+    }
+    if (do_c) {
+      sprintf(buf, FNBOUNDS_C_FORMAT, output_dir, base);
+      the_c_fp = fopen(buf, "w");
+      if (the_c_fp == NULL)
+        err(1, "open failed on: %s", buf);
+    }
+    if (do_text) {
+      sprintf(buf, FNBOUNDS_TEXT_FORMAT, output_dir, base);
+      the_text_fp = fopen(buf, "w");
+      if (the_text_fp == NULL)
+        err(1, "open failed on: %s", buf);
+    }
+  } else {
+    // At most one format when writing to stdout.
+    if (do_binary)
+      the_binary_fd = 1;
+    else if (do_c)
+      the_c_fp = stdout;
+    else
+      the_text_fp = stdout;
+  }
+
+  dump_file_info(object_file, fn_discovery);
   return 0;
 }
 
 int
-binary_format(void)
+binary_fmt_fd(void)
 {
-  return (use_binary_format);
+  return (the_binary_fd);
+}
+
+FILE *
+c_fmt_fp(void)
+{
+  return (the_c_fp);
+}
+
+FILE *
+text_fmt_fp(void)
+{
+  return (the_text_fp);
 }
 
 
@@ -89,27 +175,23 @@ binary_format(void)
 // private operations
 //*****************************************************************
 
-static bool 
-file_is_stripped(Symtab *syms)
-{
-  Section *sec;
-  if (syms->findSection(sec, SECTION_SYMTAB)  == false) return true;
-  return bool(sec == NULL);
-}
-
-
 static void 
-usage(char *command)
+usage(char *command, int status)
 {
   fprintf(stderr, 
-	  "Usage: %s [%s] [%s] [%s] object-file\n"
-	  "\t%s\twrite output in binary format\n"
-	  "\t%s\tdon't perform function discovery on stripped code\n"
-	  "\t%s\tprint this help message and exit\n",
-	  command, BINARY_FORMAT_ARG, NO_DISCOVERY_ARG, HELP_ARG,
-	  BINARY_FORMAT_ARG, NO_DISCOVERY_ARG, HELP_ARG);
+    "Usage: hpcfnbounds [options] object-file [output-directory]\n\n"
+    "\t-b\twrite output in binary format\n"
+    "\t-c\twrite output in C source code\n"
+    "\t-d\tdon't perform function discovery on stripped code\n"
+    "\t-h\tprint this help message and exit\n"
+    "\t-t\twrite output in text format\n"
+    "\t-v\tturn on verbose output in hpcfnbounds script\n\n"
+    "Multiple output formats (-b, -c, -t) may be specified in one run.\n"
+    "If output-directory is not specified, then output is written to\n"
+    "stdout and only one format may be used.\n"
+    "If no format is specified, then text mode is used.\n");
 
-  exit(0);
+  exit(status);
 }
 
 
@@ -218,34 +300,42 @@ dump_symbols(Symtab *syms, vector<Symbol *> &symvec, bool fn_discovery)
 static void 
 dump_file_symbols(Symtab *syms, vector<Symbol *> &symvec, bool fn_discovery)
 {
-  if (! binary_format()) {
-    printf("unsigned long csprof_nm_addrs[] = {\n");
+  if (c_fmt_fp() != NULL) {
+    fprintf(c_fmt_fp(), "unsigned long csprof_nm_addrs[] = {\n");
   }
 
   dump_symbols(syms, symvec, fn_discovery);
 
-  if (! binary_format()) {
-    printf("};\nint csprof_nm_addrs_len = "
+  if (c_fmt_fp() != NULL) {
+    fprintf(c_fmt_fp(), "};\nint csprof_nm_addrs_len = "
 	   "sizeof(csprof_nm_addrs) / sizeof(csprof_nm_addrs[0]);\n");
   }
 }
 
 
+// We call it "header", even though it comes at end of file.
+//
 static void
-dump_header_info(int relocatable, int stripped)
+dump_header_info(int relocatable)
 {
   struct fnbounds_file_header fh;
 
-  if (binary_format()) {
+  if (binary_fmt_fd() >= 0) {
     memset(&fh, 0, sizeof(fh));
     fh.magic = FNBOUNDS_MAGIC;
     fh.num_entries = num_function_entries();
     fh.relocatable = relocatable;
-    fh.stripped = stripped;
-    write(1, &fh, sizeof(fh));
-  } else {
-    printf("unsigned int csprof_relocatable = %d;\n", relocatable);
-    printf("unsigned int csprof_stripped = %d;\n", stripped);
+    write(binary_fmt_fd(), &fh, sizeof(fh));
+  }
+
+  if (c_fmt_fp() != NULL) {
+    fprintf(c_fmt_fp(), "unsigned int csprof_relocatable = %d;\n", relocatable);
+    fprintf(c_fmt_fp(), "unsigned int csprof_stripped = %d;\n", 0);
+  }
+
+  if (text_fmt_fp() != NULL) {
+    fprintf(text_fmt_fp(), "num symbols = %ld, relocatable = %d\n",
+	    num_function_entries(), relocatable);
   }
 }
 
@@ -258,7 +348,6 @@ dump_file_info(const char *filename, bool fn_discovery)
   vector<Symbol *> symvec;
   Symtab::openFile(syms, sfile);
   int relocatable = 0;
-  int stripped = 0;
 
 
 #if 0
@@ -289,5 +378,5 @@ dump_file_info(const char *filename, bool fn_discovery)
     dump_file_symbols(syms, symvec, fn_discovery);
     relocatable = syms->isExec() ? 0 : 1;
   }
-  dump_header_info(relocatable, stripped);
+  dump_header_info(relocatable);
 }
