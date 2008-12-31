4abfd6cbfd4eb1a9b51ee2610a27cc1bd4351110
tallent <tallent@b1b131fe-c5db-9c05-3d80-cf3d96147e2e>
2008-12-31 18:00:48 +0000

    A first pass at mips64 unwinding support: "unwind support" routines.

--- src/tool/hpcrun/unwind/mips64/mips64-build-intervals.c
+++ src/tool/hpcrun/unwind/mips64/mips64-build-intervals.c
@@ -1,25 +1,35 @@
+// -*-Mode: C++;-*- // technically C99
+// $Id$
+
+//************************* System Include Files ****************************
+
+#include <stdlib.h>
+
+//*************************** User Include Files ****************************
+
 #include "pmsg.h"
 #include "intervals.h"
 
-#ifndef NULL
-#define NULL 0
-#endif
+//*************************** Forward Declarations **************************
 
-/******************************************************************************
- * interface operations 
- *****************************************************************************/
+
+//***************************************************************************
+// interface operations
+//***************************************************************************
+
+//***************************************************************************
 
 interval_status 
 build_intervals(char *ins, unsigned int len)
 {
-   interval_status stat;
-
-   unwind_interval *ui = new_ui(ins, RA_BP_FRAME, 0, 16, BP_SAVED, 0, 0, NULL);
-   ui->endaddr = ins + len;
-
-   stat.first_undecoded_ins = NULL;
-   stat.errcode = 0;
-   stat.first = ui;
-
-   return stat; 
+  interval_status stat;
+  
+  unwind_interval *ui = new_ui(ins, RA_BP_FRAME, 0, 16, BP_SAVED, 0, 0, NULL);
+  ui->endaddr = ins + len;
+  
+  stat.first_undecoded_ins = NULL;
+  stat.errcode = 0;
+  stat.first = ui;
+  
+  return stat; 
 }
