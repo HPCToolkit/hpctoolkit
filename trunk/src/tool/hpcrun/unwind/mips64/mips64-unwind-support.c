f8f6e9bb2002553980d32b06c8b1894edf12d9e0
tallent <tallent@b1b131fe-c5db-9c05-3d80-cf3d96147e2e>
2008-12-31 18:00:48 +0000

    A first pass at mips64 unwinding support: "unwind support" routines.

--- trunk/src/tool/hpcrun/unwind/mips64/mips64-unwind-support.c
+++ trunk/src/tool/hpcrun/unwind/mips64/mips64-unwind-support.c
@@ -1,14 +1,33 @@
-#include "context.h"
-#include "monitor.h"
-#include "pmsg.h"
-#include "splay.h"
+// -*-Mode: C++;-*- // technically C99
+// $Id$
+
+//************************* System Include Files ****************************
+
+#include <ucontext.h>
+#include <assert.h>
+
+//*************************** User Include Files ****************************
+
 #include "unwind.h"
-#include "unwind_cursor.h"
+
+//*************************** Forward Declarations **************************
+
+#define MIPS_SP	29 /* stack pointer */
+#define MIPS_FP	30 /* base pointer */
+#define MIPS_RA	31 /* return address */
+
+#define UCONTEXT_REG(uc, reg) (uc->uc_mcontext.gregs[reg])
+
+#define UCONTEXT_PC(uc)       (void*)(uc->uc_mcontext.pc)
+#define UCONTEXT_RA(uc)       (void*)(UCONTEXT_REG(uc, MIPS_RA))
+
+#define UCONTEXT_BP(uc)       (void**)(UCONTEXT_REG(uc, MIPS_FP))
+#define UCONTEXT_SP(uc)       (void**)(UCONTEXT_REG(uc, MIPS_SP))
 
 
-#define RA_OFFSET_FROM_BP 2
-#define NEXT_PC_FROM_BP(bp) ((void *) *(((long *) bp) + RA_OFFSET_FROM_BP))
-
+//***************************************************************************
+// interface functions
+//***************************************************************************
 
 void
 unw_init_arch(void)
@@ -16,57 +35,30 @@ unw_init_arch(void)
 }
 
 
-int 
-unw_step(unw_cursor_t *cursor)
+void*
+context_pc(void* context)
 {
-  void *next_pc, **next_sp, **next_bp;
+  ucontext_t* uc = (ucontext_t*)context;
+  return UCONTEXT_PC(uc);
+}
 
-  // current frame
-  void **bp  = cursor->bp;
-  void **sp  = cursor->sp;
-  void *pc   = cursor->pc;
 
-  //-----------------------------------------------------------
-  // if we fail this check, we are done with the unwind.
-  //-----------------------------------------------------------
-  if (monitor_in_start_func_wide(pc)) return 0;
-  
-  //-----------------------------------------------------------
-  //  BP relative unwind
-  //-----------------------------------------------------------
-  next_pc  = NEXT_PC_FROM_BP(bp);
-  next_bp  = *bp;                                    // follow back chain
-  next_sp  = *bp;
+void 
+unw_init_cursor_arch(void* context, unw_cursor_t* cursor)
+{
+  ucontext_t* uc = (ucontext_t*)context;
 
-  if ((unsigned long) next_sp < (unsigned long) sp) return 0; 
+  cursor->pc = UCONTEXT_PC(uc);
+  cursor->bp = UCONTEXT_BP(uc);
+  cursor->sp = UCONTEXT_SP(uc);
+}
 
-  cursor->intvl = csprof_addr_to_interval(next_pc);
 
-  if (!cursor->intvl) {
-    if (((void *)next_sp) >= monitor_stack_bottom()) return 0; 
+int 
+unw_get_reg_arch(unw_cursor_t *cursor, int reg_id, void **reg_value)
+{
+  assert(reg_id == UNW_REG_IP);
+  *reg_value = cursor->pc;
 
-    // there isn't a valid PC saved in the LR slot in this frame.
-    // try one frame deeper. 
-    //
-    // NOTE: this should only happen in the top frame on the stack.
-    next_pc = NEXT_PC_FROM_BP(next_bp);
-    cursor->intvl = csprof_addr_to_interval(next_pc);
-  }
-
-  if (!cursor->intvl) return 0;
-
-  cursor->pc = next_pc;
-  cursor->bp = next_bp;
-  cursor->sp = next_sp;
-
-#if 0
-  if (debug_unw) {
-    PMSG(UNW,"dumping the found interval");
-    dump_ui(cursor->intvl,1); // debug for now
-  }
-
-  PMSG(UNW,"NEXT frame pc = %p, frame bp = %p\n", cursor->pc, cursor->bp);
-#endif
-
-  return 1;
+  return 0;
 }
