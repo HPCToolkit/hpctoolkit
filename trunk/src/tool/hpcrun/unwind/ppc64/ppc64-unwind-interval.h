2c06fd520e6f4a10581061b559d27dc7f90453e7
johnmc <johnmc@b1b131fe-c5db-9c05-3d80-cf3d96147e2e>
2009-01-12 05:26:25 +0000

    mostly formatting of ppc64 unwinder.

--- trunk/src/tool/hpcrun/unwind/ppc64/ppc64-unwind-interval.h
+++ trunk/src/tool/hpcrun/unwind/ppc64/ppc64-unwind-interval.h
@@ -1,43 +1,52 @@
-#ifndef INTERVALS_H
-#define INTERVALS_H
+#ifndef ppc64_unwind_interval_h
+#define ppc64_unwind_interval_h
+
+
+/******************************************************************************
+ * local includes
+ *****************************************************************************/
 
 #include "splay-interval.h"
 
-/*************************************************************************************
+
+
+/******************************************************************************
  * type declarations 
- ************************************************************************************/
+ *****************************************************************************/
+
 typedef enum {
   RA_SP_RELATIVE, RA_STD_FRAME, RA_BP_FRAME, RA_REGISTER, POISON
 } ra_loc;
+
 
 typedef enum {
   BP_UNCHANGED, BP_SAVED, BP_HOSED
 } bp_loc;
 
+
 struct unwind_interval_t {
   struct splay_interval_s common; // common splay tree fields
 
   ra_loc ra_status; /* how to find the return address */
-
   bp_loc bp_status; /* how to find the bp register */
-
-  int bp_ra_pos; /* return address offset from bp */
+  int bp_ra_pos;    /* return address offset from bp */
 };
 
-#define lstartaddr ((unsigned long) startaddr)
-#define lendaddr ((unsigned long) endaddr)
 
 typedef struct unwind_interval_t unwind_interval;
 
-/*************************************************************************************
+
+
+/******************************************************************************
  * global variables 
- ************************************************************************************/
+ *****************************************************************************/
 extern const unwind_interval poison_ui;
 
 
-/*************************************************************************************
+
+/******************************************************************************
  * interface operations
- ************************************************************************************/
+ *****************************************************************************/
 
 #ifdef __cplusplus
 extern "C" {
@@ -64,4 +73,4 @@ extern "C" {
 
 
 
-#endif // INTERVALS_H
+#endif // ppc64_unwind_interval_h
