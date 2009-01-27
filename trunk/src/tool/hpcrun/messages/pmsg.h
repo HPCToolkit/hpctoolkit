2aa37b3fa1da0dbf254a267722dae25429254d90
mfagan <mfagan@b1b131fe-c5db-9c05-3d80-cf3d96147e2e>
2009-01-27 23:38:52 +0000

    For messages system, combine -dd namespace with -dc namespace.
       [ RFC from Mark Krentel ]

--- trunk/src/tool/hpcrun/messages/pmsg.h
+++ trunk/src/tool/hpcrun/messages/pmsg.h
@@ -12,11 +12,15 @@ typedef enum {
 #include "pmsg.src"
 } pmsg_category;
 
+typedef pmsg_category dbg_category;
+
+#if 0
 #undef D
 #define D(s) CTL_PREFIX(s)
 typedef enum {
 #include "ctl.src"
 } ctl_category;
+#endif
 
 extern void pmsg_init();
 extern void pmsg_fini(void);
@@ -27,7 +31,7 @@ extern void csprof_pmsg(pmsg_category flag,const char *fmt,...);
 extern void csprof_nmsg(pmsg_category flag,const char *fmt,...);
 extern void csprof_stderr_msg(const char *fmt,...);
 extern void csprof_exit_on_error(int ret, int ret_expected, const char *fmt,...);
-extern int  csprof_ctl(ctl_category flag);
+extern int  csprof_dbg(dbg_category flag);
 extern int  csprof_logfile_fd(void);
 
 extern void csprof_abort_w_info(void (*info)(void),const char *fmt,...);
@@ -45,8 +49,8 @@ extern void csprof_up_pmsg_count(void);
 #define TMSG(f,...) csprof_pmsg(DBG_PREFIX(f),#f ": " __VA_ARGS__)
 #define NMSG(f,...) csprof_nmsg(DBG_PREFIX(f),#f ": " __VA_ARGS__)
 #define EXIT_ON_ERROR(r,e,...) csprof_exit_on_error(r,e,__VA_ARGS__)
-#define CTL(f)      csprof_ctl(CTL_PREFIX(f))
-#define ENABLED(f)         CTL(f)
+#define DBG(f)      csprof_dbg(DBG_PREFIX(f))
+#define ENABLED(f)         DBG(f)
 #define IF_ENABLED(f)      if ( ENABLED(f) )
 #define IF_DISABLED(f) if ( ! ENABLED(f) )
 
