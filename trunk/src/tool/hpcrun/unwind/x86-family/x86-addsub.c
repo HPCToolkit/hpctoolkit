d346a4a6ad26e28d1b2da1cce949ea10993c6648
tallent <tallent@b1b131fe-c5db-9c05-3d80-cf3d96147e2e>
2009-02-11 04:49:40 +0000

    Wrap lines over 80 characters (while debugging a bad unwind interval problem).

--- trunk/src/tool/hpcrun/unwind/x86-family/x86-addsub.c
+++ trunk/src/tool/hpcrun/unwind/x86-family/x86-addsub.c
@@ -1,3 +1,6 @@
+// -*-Mode: C++;-*- // technically C99
+// $Id$
+
 #include "x86-unwind-analysis.h"
 
 unwind_interval *
@@ -13,9 +16,9 @@ process_addsub(char *ins, xed_decoded_inst_t *xptr, const xed_inst_t *xi,
 
   if ((op0_name == XED_OPERAND_REG0) &&
       (xed_decoded_inst_get_reg(xptr, op0_name) == XED_REG_RSP)) {
-    //---------------------------------------------------------------------------
+    //-----------------------------------------------------------------------
     // we are adjusting the stack pointer
-    //---------------------------------------------------------------------------
+    //-----------------------------------------------------------------------
 
     if (xed_operand_name(op1) == XED_OPERAND_IMM0) {
       int sign = (iclass_eq(xptr, XED_ICLASS_ADD)) ? -1 : 1;
@@ -23,16 +26,17 @@ process_addsub(char *ins, xed_decoded_inst_t *xptr, const xed_inst_t *xi,
       ra_loc istatus = current->ra_status;
       if ((istatus == RA_STD_FRAME) && (immedv > 0) &&
 	  (highwatermark->type == HW_SPSUB)) {
-	//---------------------------------------------------------------------------
-	// if we are in a standard frame and we see a second subtract, it is time
-	// to convert interval to a BP frame to minimize the chance we get the 
-	// wrong offset for the return address in a routine that manipulates 
-	// SP frequently (as in leapfrog_mod_leapfrog_ in the 
-	// SPEC CPU2006 benchmark 459.GemsFDTD, when compiled with PGI 7.0.3 with
-	// high levels of optimization).
+	//-------------------------------------------------------------------
+	// if we are in a standard frame and we see a second subtract,
+	// it is time to convert interval to a BP frame to minimize
+	// the chance we get the wrong offset for the return address
+	// in a routine that manipulates SP frequently (as in
+	// leapfrog_mod_leapfrog_ in the SPEC CPU2006 benchmark
+	// 459.GemsFDTD, when compiled with PGI 7.0.3 with high levels
+	// of optimization).
 	//
 	// 9 December 2007 -- John Mellor-Crummey
-	//---------------------------------------------------------------------------
+	//-------------------------------------------------------------------
 #if PREFER_BP_FRAME
 	istatus = RA_BP_FRAME;
 	*bp_frames_found = true;
@@ -44,18 +48,22 @@ process_addsub(char *ins, xed_decoded_inst_t *xptr, const xed_inst_t *xi,
 		    current->bp_bp_pos, current);
 
       if (immedv > 0) {
-	if (highwatermark->type != HW_SPSUB && highwatermark->type != HW_BPSAVE_AFTER_SUB && highwatermark->type != HW_BPHOSED) {
-	  //-------------------------------------------------------------------------
-	  // set the highwatermark and canonical interval upon seeing the FIRST
-	  // subtract from SP; take no action on subsequent subtracts.
+	if (highwatermark->type != HW_SPSUB && 
+	    highwatermark->type != HW_BPSAVE_AFTER_SUB && 
+	    highwatermark->type != HW_BPHOSED) {
+	  //-----------------------------------------------------------------
+	  // set the highwatermark and canonical interval upon seeing
+	  // the FIRST subtract from SP; take no action on subsequent
+	  // subtracts.
 	  //
-	  // test case: main in SPEC CPU2006 benchmark 470.lbm contains multiple 
-	  // subtracts from SP when compiled with PGI 7.0.3 with high levels of 
-	  // optimization. the first subtract from SP is to set up the frame; 
-	  // subsequent ones are to reserve space for arguments passed to functions.
+	  // test case: main in SPEC CPU2006 benchmark 470.lbm
+	  // contains multiple subtracts from SP when compiled with
+	  // PGI 7.0.3 with high levels of optimization. the first
+	  // subtract from SP is to set up the frame; subsequent ones
+	  // are to reserve space for arguments passed to functions.
 	  //
 	  // 9 December 2007 -- John Mellor-Crummey
-	  //-------------------------------------------------------------------------
+	  //-----------------------------------------------------------------
 	  highwatermark->uwi = next;
 	  highwatermark->succ_inst_ptr = ins + xed_decoded_inst_get_length(xptr);
 	  highwatermark->type = HW_SPSUB;
@@ -64,11 +72,12 @@ process_addsub(char *ins, xed_decoded_inst_t *xptr, const xed_inst_t *xi,
       }
     } else {
       if (current->ra_status != RA_BP_FRAME){
-	//---------------------------------------------------------------------------
-	// no immediate in add/subtract from stack pointer; switch to BP_FRAME
+	//-------------------------------------------------------------------
+	// no immediate in add/subtract from stack pointer; switch to
+	// BP_FRAME
 	//
 	// 9 December 2007 -- John Mellor-Crummey
-	//---------------------------------------------------------------------------
+	//-------------------------------------------------------------------
 	next = new_ui(ins + xed_decoded_inst_get_length(xptr), RA_BP_FRAME, 
 		      current->sp_ra_pos, current->bp_ra_pos, current->bp_status, 
 		      current->sp_bp_pos, current->bp_bp_pos, current);
