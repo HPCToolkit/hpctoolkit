2a8810b046c50c04e88e35f5a7b4556b0443aa6c
tallent <tallent@b1b131fe-c5db-9c05-3d80-cf3d96147e2e>
2009-02-26 04:02:19 +0000

    - range_contains_control_flow(): create generic (non-x86) stub to facilitate compilation on non-x86 platforms.
    - A bit of tidying.

--- trunk/src/tool/hpcfnbounds/process-ranges.h
+++ trunk/src/tool/hpcfnbounds/process-ranges.h
@@ -1,8 +1,13 @@
+// -*-Mode: C++;-*-
+// $Id$
+
+#ifndef process_ranges_hpp
+#define process_ranges_hpp
+
 void process_range_init();
+
 void process_range(long offset, void *vstart, void *vend, bool fn_discovery);
+
 bool range_contains_control_flow(void *vstart, void *vend);
 
-
-
-
-
+#endif // process_ranges_hpp
