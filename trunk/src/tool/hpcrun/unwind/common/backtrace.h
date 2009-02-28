61b03dbc8cbeeb0532d4640d5c8a3f30dfd0e20a
tallent <tallent@b1b131fe-c5db-9c05-3d80-cf3d96147e2e>
2009-02-28 21:57:23 +0000

    Some cleanup.

--- trunk/src/tool/hpcrun/unwind/common/backtrace.h
+++ trunk/src/tool/hpcrun/unwind/common/backtrace.h
@@ -38,11 +38,6 @@ csprof_cct_node_t*
 csprof_sample_callstack(csprof_state_t *state, ucontext_t* context, 
 			int metric_id, size_t sample_count);
 
-#if (HPC_UNW_LITE)
-int
-hpcrun_backtrace_lite(void** buffer, int size, ucontext_t* context);
-#endif
-
 // FIXME: tallent: relocate when 'csprof state' trash is untangled
 void 
 dump_backtrace(csprof_state_t *state, csprof_frame_t *unwind);
