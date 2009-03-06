cf1f10b317f154629104ac6baf589af7a31dbad8
tallent <tallent@b1b131fe-c5db-9c05-3d80-cf3d96147e2e>
2009-03-06 21:49:04 +0000

    - Separate dropped samples messages from segv messages - Create a DROP message category for dropped samples (discussed with Mike)

--- src/tool/hpcrun/segv_handler.c
+++ src/tool/hpcrun/segv_handler.c
@@ -14,6 +14,8 @@
 
 #include "memchk.h"
 
+// FIXME: tallent: should this be together with csprof_drop_sample?
+
 /* catch SIGSEGVs */
 
 int segv_count = 0;
@@ -21,29 +23,44 @@ extern int csprof_sample;
 extern int filtered_samples;
 
 int
-csprof_sigsegv_signal_handler(int sig, siginfo_t *siginfo, void *context)
+hpcrun_sigsegv_handler(int sig, siginfo_t* siginfo, void* context)
 {
-  if (csprof_is_handling_sample()){
-    sigjmp_buf_t *it = &(TD_GET(bad_unwind));
-    if (memchk((void *)it,'\0',sizeof(*it))) {
-      EMSG("GOT ALL 0 jmpbuf during segv handling !!!. Major problem !!!");
-#ifdef ABORT_RE_SEGV
-        monitor_real_abort();
-#else
-        return 0;
-#endif
-    }
+  if (csprof_is_handling_sample()) {
     segv_count++;
-    siglongjmp(it->jb,9);  // make sure we don't hose ourselves by doing re-entrant backtracing
+
+    thread_data_t *td = csprof_get_thread_data();
+
+    // -----------------------------------------------------
+    // print context
+    // -----------------------------------------------------
+    csprof_state_t* state = td->state;
+    void* unw_pc = (state) ? state->unwind_pc : NULL;
+    void* ctxt_pc = context_pc(context);
+    PMSG_LIMIT(EMSG("error: segv: context-pc=%p, unwind-pc=%p", ctxt_pc, unw_pc));
+    // TODO: print more context details
+
+    // -----------------------------------------------------
+    // longjump
+    // -----------------------------------------------------
+    sigjmp_buf_t *it = &(td->bad_unwind);
+    if (memchk((void *)it, '\0', sizeof(*it))) {
+      EMSG("error: segv handler: invalid jmpbuf");
+      return 0; // monitor_real_abort();
+    }
+    
+    siglongjmp(it->jb,9);
   }
-  return 1; /* pass the segv to the application handler */
+  else {
+    // pass segv to another handler
+    return 1;
+  }
 }
 
 
 int
 setup_segv(void)
 {
-  int ret = monitor_sigaction(SIGSEGV, &csprof_sigsegv_signal_handler, 0, NULL);
+  int ret = monitor_sigaction(SIGSEGV, &hpcrun_sigsegv_handler, 0, NULL);
 
   if(ret != 0) {
     EMSG("Unable to install SIGSEGV handler", __FILE__, __LINE__);
