d858f6b3c2c2924a61ffa357959373895ad0aeca
mfagan <mfagan@b1b131fe-c5db-9c05-3d80-cf3d96147e2e>
2009-03-12 23:41:49 +0000

    Intermediate cleanup:     cold path code factored out into separate routine.     [ Functionality is the same ]

---
+++ trunk/src/tool/hpcrun/unwind/x86-family/x86-cold-path.h
@@ -0,0 +1,6 @@
+#include <stdbool.h>
+#include "x86-unwind-analysis.h"
+#include "x86-interval-arg.h"
+
+extern bool hpcrun_is_cold_code(void *ins, xed_decoded_inst_t *xptr, interval_arg_t *iarg);
+extern void hpcrun_cold_code_fixup(unwind_interval *current, unwind_interval *warm);
