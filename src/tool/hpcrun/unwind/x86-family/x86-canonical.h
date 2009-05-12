1232b43df130541a0a2bd9ff1b379891a02cb812
mfagan <mfagan@b1b131fe-c5db-9c05-3d80-cf3d96147e2e>
2009-05-12 21:05:41 +0000

    Remove some gratuitous #ifdef, and clean up x86-canonical.c refactoring

--- src/tool/hpcrun/unwind/x86-family/x86-canonical.h
+++ src/tool/hpcrun/unwind/x86-family/x86-canonical.h
@@ -10,17 +10,9 @@
  *****************************************************************************/
 
 void 
-#ifdef IARGR
 reset_to_canonical_interval(xed_decoded_inst_t *xptr,
 			    unwind_interval **next,
 			    bool irdebug,
                             interval_arg_t *iarg);
-#endif
-reset_to_canonical_interval(xed_decoded_inst_t *xptr, unwind_interval *current, 
-			    unwind_interval **next, char *ins, char *end, 
-			    bool irdebug, unwind_interval *first, 
-			    highwatermark_t *highwatermark, 
-			    unwind_interval **canonical_interval, 
-			    bool bp_frames_found);
 
-#endif
+#endif //x86_canonical_h
