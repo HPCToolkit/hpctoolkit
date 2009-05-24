4ab6d1a207949e942a1473c9930ac5aa45cf6242
tallent <tallent@b1b131fe-c5db-9c05-3d80-cf3d96147e2e>
2009-05-24 02:37:29 +0000

    A small bit of cleanup.

--- trunk/src/include/gcc-attr.h
+++ trunk/src/include/gcc-attr.h
@@ -59,10 +59,14 @@
 //****************************************************************************
 
 #ifdef __GNUC__
+#  define GCC_VERSION (__GNUC__*1000 \
+		       + __GNUC_MINOR__*100 \
+		       + __GNUC_PATCHLEVEL__)
 #  define GCC_ATTR_NORETURN __attribute__ ((noreturn))
 #  define GCC_ATTR_UNUSED   __attribute__ ((unused))
 #  define GCC_ATTR_VAR_CACHE_ALIGN __attribute__ ((aligned (HOST_CACHE_LINE_SZ)))
 #else
+#  define GCC_VERSION 0
 #  define GCC_ATTR_NORETURN
 #  define GCC_ATTR_UNUSED
 #  define GCC_ATTR_VAR_CACHE_ALIGN
