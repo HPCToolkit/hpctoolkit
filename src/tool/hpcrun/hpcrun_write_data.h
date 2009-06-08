436fea614e5cf1c4ad3a810263cc4b7c21b63018
mfagan <mfagan@b1b131fe-c5db-9c05-3d80-cf3d96147e2e>
2009-06-08 16:23:47 +0000

    cosmetic changes to hpcrun_write_data

--- src/tool/hpcrun/hpcrun_write_data.h
+++ src/tool/hpcrun/hpcrun_write_data.h
@@ -1,8 +1,8 @@
-#ifndef HPCRUN_WRITE_DATA_H
-#define HPCRUN_WRITE_DATA_H
+#ifndef WRITE_DATA_H
+#define WRITE_DATA_H
 
 #include "state.h"
 
 extern int hpcrun_write_profile_data(csprof_state_t *state);
 
-#endif // HPCRUN_WRITE_DATA_H
+#endif // WRITE_DATA_H
