6456c240ce7437ce7a437b527676a5b847b48482
tallent <tallent@b1b131fe-c5db-9c05-3d80-cf3d96147e2e>
2009-06-28 02:20:51 +0000

    hpcrun/utilities/spinlock: make these routines inlinable.

--- src/tool/hpcrun/utilities/spinlock.h
+++ src/tool/hpcrun/utilities/spinlock.h
@@ -1,5 +1,12 @@
+// -*-Mode: C++;-*- // technically C99
+// $Id$
+
 #ifndef _simple_lock_h_
 #define _simple_lock_h_
+
+#include <stdbool.h>
+
+#include "cmpxchg.h"
 
 /*
  * Simple spin lock.
@@ -7,11 +14,31 @@
  */
 
 typedef volatile long spinlock_t;
-
-void spinlock_lock(spinlock_t *l);
-void spinlock_unlock(spinlock_t *l);
-int spinlock_is_locked(spinlock_t *l);
-
 #define SPINLOCK_UNLOCKED ((spinlock_t) 0L)
+
+static inline void 
+spinlock_lock(spinlock_t *l)
+{
+  /* test-and-test-and-set lock */
+  for(;;) {
+    while (*l);
+    if (cmpxchg(l, 0, 1) == 0) return; 
+  }
+}
+
+
+static inline void 
+spinlock_unlock(spinlock_t *l)
+{
+  *l = SPINLOCK_UNLOCKED;
+}
+
+
+static inline bool 
+spinlock_is_locked(spinlock_t *l)
+{
+  return (*l != SPINLOCK_UNLOCKED);
+}
+
 
 #endif  
