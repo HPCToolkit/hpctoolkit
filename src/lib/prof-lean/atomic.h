271c49ef2f8f86305212f2b0091c78922d611f7b
johnmc <johnmc@b1b131fe-c5db-9c05-3d80-cf3d96147e2e>
2009-07-12 03:54:00 +0000

    cast return value to void for hpcrun_atomicIncr and hpcrun_atomicDecr whose return values are never used.

--- src/lib/prof-lean/atomic.h
+++ src/lib/prof-lean/atomic.h
@@ -66,8 +66,8 @@ csprof_atomic_swap_l(volatile long* addr, long new)
 // FIXME:tallent: this should be replace the "csprof" routines
 // Careful: return the *new* value
 #if (GCC_VERSION >= 4100)
-#  define hpcrun_atomicIncr(x)      __sync_add_and_fetch(x, 1)
-#  define hpcrun_atomicDecr(x)      __sync_sub_and_fetch(x, 1)
+#  define hpcrun_atomicIncr(x)      (void) __sync_add_and_fetch(x, 1)
+#  define hpcrun_atomicDecr(x)      (void) __sync_sub_and_fetch(x, 1)
 #  define hpcrun_atomicAdd(x, val)  __sync_add_and_fetch(x, val)
 #  define hpcrun___sync_add_and_fetch
 
