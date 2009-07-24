6796c41235b7e10865cf47a35a753d8de3f3a513
johnmc <johnmc@b1b131fe-c5db-9c05-3d80-cf3d96147e2e>
2009-07-24 00:14:41 +0000

    update implementation of atomic ops to use gcc atomics if compiled by gcc.

---
+++ src/lib/prof-lean/atomic-op-asm.h
@@ -0,0 +1,177 @@
+// -*-Mode: C++;-*- // technically C99
+
+//***************************************************************************
+//
+// File: 
+//   $Source$
+//
+// Purpose:
+//   Atomics.
+//
+// Description:
+//   [The set of functions, macros, etc. defined in the file]
+//
+// Author:
+//   [...]
+//
+//***************************************************************************
+
+#ifndef prof_lean_atomic_op_asm_h
+#define prof_lean_atomic_op_asm_h
+
+//************************* System Include Files ****************************
+
+#include <stdint.h>
+#include <assert.h>
+
+//*************************** User Include Files ****************************
+
+#include "atomic-op.i"
+
+//***************************************************************************
+
+#if defined(LL_BODY)
+
+inline static unsigned long 
+load_linked(volatile unsigned long* ptr)
+{
+  volatile unsigned long result;
+  LL_BODY;
+  return result;
+}
+
+#endif
+
+//***************************************************************************
+
+#if defined(SC_BODY) 
+
+/* if successful, return 1 */
+inline static int 
+store_conditional(volatile unsigned long* ptr, unsigned long val)
+{
+  int result;
+  SC_BODY;
+  return result;
+}
+
+#endif
+
+//***************************************************************************
+
+#if defined (LL_BODY) && defined(SC_BODY) && !defined(CAS_BODY)
+
+#define CAS_BODY                                                            \
+  for(;;) {                                                                 \
+    prev = load_linked(ptr);                                                \
+    if (prev != old || store_conditional(ptr, new)) break;                  \
+  }
+#endif
+
+
+#if defined(CAS_BODY) 
+
+static inline unsigned long
+compare_and_swap(volatile void *ptr, unsigned long old, unsigned long new)
+{
+  unsigned long prev;
+  CAS_BODY;
+  return prev;
+}
+
+
+#define compare_and_swap_ptr(vptr, oldptr, newptr)			\
+  ((void*) compare_and_swap((volatile void*)vptr,			\
+			    (unsigned long)oldptr, (unsigned long)newptr))
+
+#endif
+
+
+static inline int32_t
+compare_and_swap_i32(volatile int32_t *ptr, int32_t old, int32_t new)
+{
+  int32_t prev;
+#if defined (CAS4_BODY)
+  CAS4_BODY;
+#else
+# warning "compare_and_swap_i32() is unimplemented!"
+  assert(0 && "compare_and_swap_i32() is unimplemented!");
+#endif
+  return prev;
+
+}
+
+
+//***************************************************************************
+
+#if defined (LL_BODY) && defined(SC_BODY) 
+
+#define read_modify_write(type, addr, expn, result) {			\
+  type __new;								\
+  do {								        \
+    result = (type) load_linked((unsigned long*)addr);	         	\
+    __new = expn;							\
+  } while (!store_conditional((unsigned long*)addr, (unsigned long) __new)); \
+}
+
+#else
+
+#define read_modify_write(type, addr, expn, result) {                        \
+    type __new;                                                              \
+    do {                                                                     \
+      result = *addr;                                                        \
+      __new = expn;                                                          \
+    } while (compare_and_swap(addr, result, __new) != result);               \
+}
+
+#endif
+
+//***************************************************************************
+
+static inline long
+fetch_and_add(volatile long* addr, long val)
+{
+  long result;
+  read_modify_write(long, addr, result + val, result);
+  return result;
+}
+
+//***************************************************************************
+
+#if defined(FAS_BODY)
+
+static inline unsigned long
+fetch_and_store(volatile long* ptr, unsigned long new)
+{
+  unsigned long prev;
+  FAS_BODY;
+  return prev;
+}
+
+
+static inline unsigned long
+fetch_and_store_i32(volatile int32_t* ptr, int32_t new)
+{
+  int32_t prev;
+  FAS_BODY;
+  return prev;
+}
+
+#else
+
+static inline long
+fetch_and_store(volatile long* addr, long new)
+{
+  long result;
+  read_modify_write(long, addr, new, result);
+  return result;
+}
+
+#endif
+
+#define fetch_and_store_ptr(vaddr, ptr) \
+  ((void *) fetch_and_store((volatile long *)vaddr, (long) ptr))
+
+//***************************************************************************
+
+#endif // prof_lean_atomic_op_asm_h
---
+++ src/lib/prof-lean/atomic-op-gcc.h
@@ -0,0 +1,63 @@
+#ifndef atomic_op_gcc_h
+#define atomic_op_gcc_h
+
+//***************************************************************************
+//
+// File: atomic-op-gcc.h 
+//
+// Description:
+//   implement the small set of atomic operations needed by hpctoolkit
+//   using gcc atomics
+//
+// Author:
+//   23 July 2009 - John Mellor-Crummey - created
+//
+//***************************************************************************
+
+//***************************************************************************
+// macros
+//***************************************************************************
+
+#define fetch_and_add(addr, val) \
+	__sync_fetch_and_add(addr, val) 
+
+#define fetch_and_add_i32(addr, val) \
+	__sync_fetch_and_add(addr, val) 
+
+#define fetch_and_add_i64(addr, val) \
+	__sync_fetch_and_add(addr, val) 
+
+
+// -------------------------------------------------------------
+// requirement: _sync_lock_test_and_set(addr, newval) must 
+// implement an exchange on your architecture. if not, this 
+// won't work.
+// -------------------------------------------------------------
+#define fetch_and_store(addr, newval)  \
+	__sync_lock_test_and_set(addr, newval) 
+
+#define fetch_and_store_i32(addr, newval) \
+	fetch_and_store(addr, newval) 
+
+#define fetch_and_store_i64(addr, newval) \
+	fetch_and_store(addr, newval) 
+
+#define fetch_and_store_ptr(addr, newval) \
+	fetch_and_store(addr, newval)
+
+
+#define compare_and_swap(addr, oldval, newval) \
+	__sync_val_compare_and_swap(addr, oldval, newval)
+
+#define compare_and_swap_i32(addr, oldval, newval) \
+	compare_and_swap(addr, oldval, newval) 
+
+#define compare_and_swap_i64(addr, oldval, newval) \
+	compare_and_swap(addr, oldval, newval) 
+
+#define compare_and_swap_ptr(addr, oldval, newval) \
+	compare_and_swap(addr, oldval, newval) 
+
+
+
+#endif
