e5d9b5dcc1587495d242ff2c85d850e02687eee1
tallent <tallent@b1b131fe-c5db-9c05-3d80-cf3d96147e2e>
2009-08-21 20:32:07 +0000

    hpcprof-mpi: - create skeletal code - until stable, only enable upon explicit request via --enable-mpi

---
+++ trunk/src/tool/hpcprof-mpi/main.cpp
@@ -0,0 +1,137 @@
+// -*-Mode: C++;-*-
+// $HeadURL$
+
+// * BeginRiceCopyright *****************************************************
+// 
+// Copyright ((c)) 2002-2007, Rice University 
+// All rights reserved.
+// 
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are
+// met:
+// 
+// * Redistributions of source code must retain the above copyright
+//   notice, this list of conditions and the following disclaimer.
+// 
+// * Redistributions in binary form must reproduce the above copyright
+//   notice, this list of conditions and the following disclaimer in the
+//   documentation and/or other materials provided with the distribution.
+// 
+// * Neither the name of Rice University (RICE) nor the names of its
+//   contributors may be used to endorse or promote products derived from
+//   this software without specific prior written permission.
+// 
+// This software is provided by RICE and contributors "as is" and any
+// express or implied warranties, including, but not limited to, the
+// implied warranties of merchantability and fitness for a particular
+// purpose are disclaimed. In no event shall RICE or contributors be
+// liable for any direct, indirect, incidental, special, exemplary, or
+// consequential damages (including, but not limited to, procurement of
+// substitute goods or services; loss of use, data, or profits; or
+// business interruption) however caused and on any theory of liability,
+// whether in contract, strict liability, or tort (including negligence
+// or otherwise) arising in any way out of the use of this software, even
+// if advised of the possibility of such damage. 
+// 
+// ******************************************************* EndRiceCopyright *
+
+//***************************************************************************
+//
+// File:
+//   $HeadURL$
+//
+// Purpose:
+//   [The purpose of this file]
+//
+// Description:
+//   [The set of functions, macros, etc. defined in the file]
+//
+//***************************************************************************
+
+//**************************** MPI Include Files ****************************
+
+#include <mpi.h>
+
+//************************* System Include Files ****************************
+
+#include <iostream>
+#include <fstream>
+
+#include <string>
+using std::string;
+
+//*************************** User Include Files ****************************
+
+#include "Args.hpp"
+
+#include <lib/support/diagnostics.h>
+#include <lib/support/RealPathMgr.hpp>
+
+
+//*************************** Forward Declarations ***************************
+
+//****************************************************************************
+
+int realmain(int argc, char* const* argv);
+
+int 
+main(int argc, char* const* argv) 
+{
+  int ret;
+
+  try {
+    ret = realmain(argc, argv);
+  }
+  catch (const Diagnostics::Exception& x) {
+    DIAG_EMsg(x.message());
+    exit(1);
+  } 
+  catch (const std::bad_alloc& x) {
+    DIAG_EMsg("[std::bad_alloc] " << x.what());
+    exit(1);
+  } 
+  catch (const std::exception& x) {
+    DIAG_EMsg("[std::exception] " << x.what());
+    exit(1);
+  } 
+  catch (...) {
+    DIAG_EMsg("Unknown exception encountered!");
+    exit(2);
+  }
+
+  MPI_Finalize();
+  return ret;
+}
+
+
+int
+realmain(int argc, char* const* argv) 
+{
+  Args args(argc, argv); 
+  // exits on special options (--help, --version)
+
+  // -------------------------------------------------------
+  MPI_Init(&argc, (char***)&argv);
+  // -------------------------------------------------------
+
+  RealPathMgr::singleton().searchPaths(args.searchPathStr());
+
+  int rank;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank); 
+
+  int sendbuf = rank;
+  int recvbuf;
+  MPI_Allreduce(&sendbuf, &recvbuf, 1, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
+  
+  printf("rank= %d recvbuf= %d\n", rank, recvbuf);
+
+  // -------------------------------------------------------
+  // MPI_Finalize() called in parent
+  // -------------------------------------------------------
+  
+  return 0;
+} 
+
+
+//****************************************************************************
+
