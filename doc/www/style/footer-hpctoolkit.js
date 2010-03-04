c9367af3014cac91e0d785d90cdbb784f0857319
tallent <tallent@b1b131fe-c5db-9c05-3d80-cf3d96147e2e>
2010-03-04 19:00:04 +0000

    Revamp main page and overview.

--- doc/www/style/footer-hpctoolkit.js
+++ doc/www/style/footer-hpctoolkit.js
@@ -17,8 +17,8 @@ document.write('  &copy;2000-2010 <a title="Rice University" href="http://www.ri
 document.write('</td>');
 document.write('<td align="right">');
 document.write('  [<a href="info-contact.html">Contact Us</a> |');
-document.write('  <a href="info-acks.html">Acknowledgments</a> |');
 document.write('  <a href="info-people.html">People</a> |');
+document.write('  <a href="info-acks.html">Acknowledgments</a> |');
 document.write('  <a title="RiceCS" href="http://www.cs.rice.edu">Rice Computer Science</a> ]');
 document.write('</td>');
 document.write('</tr>');
