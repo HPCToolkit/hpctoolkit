512a1842241799ff3bc155a8d91f69ee11b4c22e
tallent <tallent@b1b131fe-c5db-9c05-3d80-cf3d96147e2e>
2010-03-06 02:54:44 +0000

    Normalize copyrights for 2010.

--- src/tool/hpcrun/monitor-exts/openmp.c
+++ src/tool/hpcrun/monitor-exts/openmp.c
@@ -8,25 +8,25 @@
 // -----------------------------------
 // Part of HPCToolkit (hpctoolkit.org)
 // -----------------------------------
-//
-// Copyright ((c)) 2009, Rice University
+// 
+// Copyright ((c)) 2002-2010, Rice University 
 // All rights reserved.
-//
+// 
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions are
 // met:
-//
+// 
 // * Redistributions of source code must retain the above copyright
 //   notice, this list of conditions and the following disclaimer.
-//
+// 
 // * Redistributions in binary form must reproduce the above copyright
 //   notice, this list of conditions and the following disclaimer in the
 //   documentation and/or other materials provided with the distribution.
-//
+// 
 // * Neither the name of Rice University (RICE) nor the names of its
 //   contributors may be used to endorse or promote products derived from
 //   this software without specific prior written permission.
-//
+// 
 // This software is provided by RICE and contributors "as is" and any
 // express or implied warranties, including, but not limited to, the
 // implied warranties of merchantability and fitness for a particular
@@ -37,8 +37,8 @@
 // business interruption) however caused and on any theory of liability,
 // whether in contract, strict liability, or tort (including negligence
 // or otherwise) arising in any way out of the use of this software, even
-// if advised of the possibility of such damage.
-//
+// if advised of the possibility of such damage. 
+// 
 // ******************************************************* EndRiceCopyright *
 
 // Note: this must remain a separate file for proper linking with
