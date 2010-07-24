2fac4f5d309504a8b7144d719bd893f078fb9647
tallent <tallent@b1b131fe-c5db-9c05-3d80-cf3d96147e2e>
2010-07-24 20:11:04 +0000

    Update WWW page.

--- doc/www/style/header-hpctoolkit.js
+++ doc/www/style/header-hpctoolkit.js
@@ -7,19 +7,30 @@ document.write('<a name="top"></a>');
 // Locate the 'HPCToolkit' Image (height = 71 pixels)
 document.write('<img style="position:absolute; top:0px; left:0px" src="style/header.gif">');
 
-// Row 1 of the menu items (over the image)
-document.write('<div style="position:absolute; top:60px; right:15px;">');
-document.write('[ <a href="index.html">HPCToolkit Home</a> |');
-document.write('  <a href="overview.html">Overview</a> |');
-//document.write('  <a href="examples.html">Examples</a> |');
-document.write('  <a href="papers.html">Papers</a> |');
-document.write('  <a href="software.html">Software</a> |');
-document.write('  <a href="documentation.html">Documentation</a> ]');
+// Menu
+document.write('<div style="position:absolute; top:60px; right:15px; font-family: sans-serif; font-size: small;">');
+document.write('\
+  [ <a href="index.html">Home</a> \
+  | <a href="overview.html">Overview</a> \
+  | <a href="publications.html">Publications</a> ] \
+  &bull; \
+  [ <a href="software.html">Software</a> \
+  | <a href="documentation.html">Documentation</a> ] \
+  &bull; \
+  [ <a href="info-contact.html">Contacts</a> | \
+    <a href="info-acks.html">Acks</a> ]');
 document.write('</div>');
-document.write('<br>');
-document.write('<br>');
-document.write('<br>');
-document.write('<br>');
+
+// document.write('  <a href="info-people.html">People</a> |');
+
+document.write('<div style="position:absolute; top:75px; left:0px;">');
+document.write('\
+  <table width="100%" border="0" cellspacing="0" cellpadding="0"> \
+  <tr><hr></tr> \
+  </table>');
+document.write('</div>');
+
+document.write('<br> <br> <br> <br>');
 
 // ***************************************************************************
 
