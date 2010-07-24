60967437fb55112692e592df0c974dea748c140b
tallent <tallent@b1b131fe-c5db-9c05-3d80-cf3d96147e2e>
2010-07-24 20:11:04 +0000

    Update WWW page.

--- trunk/doc/www/style/footer-hpctoolkit.js
+++ trunk/doc/www/style/footer-hpctoolkit.js
@@ -1,25 +1,15 @@
 // We use JavaScript as a way to easily include this footer
 // information in several HTML pages.
 
-//   note: uses the 'top' anchor from header_hpctools.js
-document.write('<table width="100%" border="0" cellspacing="0" cellpadding="0">');
-
-// horizontal rule
-document.write('<tr>');
-document.write('<hr>');
-document.write('</tr>');
-
-// General links and copyright 
-
-document.write('<tr>');
-document.write('<td align="left">');
-document.write('  &copy;2000-2010 <a title="Rice University" href="http://www.rice.edu">Rice University</a>');
-document.write('</td>');
-document.write('<td align="right">');
-document.write('  [<a href="info-contact.html">Contact Us</a> |');
-document.write('  <a href="info-people.html">People</a> |');
-document.write('  <a href="info-acks.html">Acknowledgments</a> |');
-document.write('  <a title="RiceCS" href="http://www.cs.rice.edu">Rice Computer Science</a> ]');
-document.write('</td>');
-document.write('</tr>');
-document.write('</table>');
+document.write('<div style="font-family: sans-serif; font-size: small;">');
+document.write('\
+<table width="100%" border="0" cellspacing="0" cellpadding="0"> \
+  <tr><hr></tr> \
+  <tr> \
+    <td align="left"> \
+      &copy;2000-2010 <a title="Rice University" href="http://www.rice.edu">Rice University</a> &bull; <a title="Rice CS" href="http://www.cs.rice.edu">Rice Computer Science</a> \
+    </td> \
+    <td align="right"> </td> \
+  </tr> \
+</table>');
+document.write('</div>');
