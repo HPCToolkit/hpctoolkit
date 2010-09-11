d5b8850415948787d5aae3e79c9b142d40a763cb
tallent <tallent@b1b131fe-c5db-9c05-3d80-cf3d96147e2e>
2010-09-11 04:30:08 +0000

    Web page now uses validated XHTML 1.0 (instead of HTML 4.0).

--- trunk/doc/www/style/footer-hpctoolkit.js
+++ trunk/doc/www/style/footer-hpctoolkit.js
@@ -1,15 +1,15 @@
-// We use JavaScript as a way to easily include this footer
-// information in several HTML pages.
-
-document.write('<div style="font-family: sans-serif; font-size: small;">');
+// Footer
 document.write('\
-<table width="100%" border="0" cellspacing="0" cellpadding="0"> \
-  <tr><hr></tr> \
-  <tr> \
-    <td align="left"> \
-      &copy;2000-2010 <a title="Rice University" href="http://www.rice.edu">Rice University</a> &bull; <a title="Rice CS" href="http://www.cs.rice.edu">Rice Computer Science</a> \
-    </td> \
-    <td align="right"> </td> \
-  </tr> \
-</table>');
-document.write('</div>');
+  <div style="font-size: small;"> \
+    <hr /> \
+    <p style="margin: 0px; font-size: small;"> \
+      &copy;2000-2010 <a href="http://www.rice.edu">Rice University</a> \
+      &bull; \
+      <a href="http://www.cs.rice.edu">Rice Computer Science</a> \
+    </p> \
+    <a href="http://validator.w3.org/check/referer"> \
+      <img src="http://www.w3.org/Icons/valid-xhtml10" alt="" height="15" /></a> \
+    <a href="http://jigsaw.w3.org/css-validator/check/referer"> \
+      <img src="http://www.w3.org/Icons/valid-css" alt="" height="15" /></a> \
+  </div> \
+');
--- trunk/doc/www/style/header-hpctoolkit.js
+++ trunk/doc/www/style/header-hpctoolkit.js
@@ -1,51 +1,25 @@
-// We use JavaScript as a way to easily include this header
-// information in several HTML pages.
-
-// The 'top' anchor (used by footer_hpctools.js)
-document.write('<a name="top"></a>');
-
-// Locate the 'HPCToolkit' Image (height = 71 pixels)
-document.write('<img style="position:absolute; top:0px; left:0px" src="style/header.gif">');
-
-// Menu
-document.write('<div style="position:absolute; top:60px; right:15px; font-family: sans-serif; font-size: small;">');
+// 1. HPCToolkit Image (height = 71 pixels)
 document.write('\
-  [ <a href="index.html">Home</a> \
-  | <a href="overview.html">Overview</a> \
-  | <a href="publications.html">Publications</a> ] \
-  &bull; \
-  [ <a href="software.html">Software</a> \
-  | <a href="documentation.html">Documentation</a> ] \
-  &bull; \
-  [ <a href="info-contact.html">Contacts</a> | \
-    <a href="info-acks.html">Acks</a> ]');
-document.write('</div>');
+  <img style="position: absolute; top: 0px; left: 0px" \
+       src="style/header.gif" /> \
+');
 
-// document.write('  <a href="info-people.html">People</a> |');
-
-document.write('<div style="position:absolute; top:75px; left:0px;">');
+// 2. Menu and horizontal rule
 document.write('\
-  <table width="100%" border="0" cellspacing="0" cellpadding="0"> \
-  <tr><hr></tr> \
-  </table>');
-document.write('</div>');
-
-document.write('<br> <br> <br> <br>');
-
-// ***************************************************************************
-
-// Old version for Netscape 4.  
-
-//document.write('<table width="100%" border="0" cellspacing="0" cellpadding="0">');
-//document.write('<tr>');
-//document.write('<td align="right">');
-//document.write('[ <a href="index.html">HPCToolkit Home</a> |');
-//document.write('  <a href="overview.html">Overview</a> |');
-//document.write('  <a href="examples.html">Examples</a> |');
-//document.write('  <a href="papers.html">Papers</a> |');
-//document.write('  <a href="downloads.html">Downloads</a> |');
-//document.write('  <a href="documentation.html">Documentation</a> ]');
-//document.write('</td>');
-//document.write('</tr>');
-//document.write('</table>');
-//document.write('<hr>');
+  <div style="position: absolute; top: 60px; right: 2em;"> \
+  <p style="margin: 0px; font-size: small;"> \
+    [ <a href="index.html">Home</a> \
+    | <a href="overview.html">Overview</a> \
+    | <a href="publications.html">Publications</a> ] \
+    &bull; \
+    [ <a href="software.html">Software</a> \
+    | <a href="documentation.html">Documentation</a> ] \
+    &bull; \
+    [ <a href="info-contact.html">Contacts</a> | \
+      <a href="info-acks.html">Acks</a> ] \
+  </p> \
+  </div> \
+  <div style="position: absolute; top: 75px; width: 100%;"> \
+    <hr /> \
+  </div> \
+');
