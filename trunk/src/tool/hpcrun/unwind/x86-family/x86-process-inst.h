d25a82572cf698c4122e9f3b7b6966f63578ca52
tallent <tallent@b1b131fe-c5db-9c05-3d80-cf3d96147e2e>
2010-12-23 23:39:09 +0000

    - Run 'normalize sources' script - Update copyright string for 2011

--- trunk/src/tool/hpcrun/unwind/x86-family/x86-process-inst.h
+++ trunk/src/tool/hpcrun/unwind/x86-family/x86-process-inst.h
@@ -7,29 +7,29 @@
 //
 // --------------------------------------------------------------------------
 // Part of HPCToolkit (hpctoolkit.org)
-// 
+//
 // Information about sources of support for research and development of
 // HPCToolkit is at 'hpctoolkit.org' and in 'README.Acknowledgments'.
 // --------------------------------------------------------------------------
-// 
-// Copyright ((c)) 2002-2010, Rice University 
+//
+// Copyright ((c)) 2002-2011, Rice University
 // All rights reserved.
-// 
+//
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions are
 // met:
-// 
+//
 // * Redistributions of source code must retain the above copyright
 //   notice, this list of conditions and the following disclaimer.
-// 
+//
 // * Redistributions in binary form must reproduce the above copyright
 //   notice, this list of conditions and the following disclaimer in the
 //   documentation and/or other materials provided with the distribution.
-// 
+//
 // * Neither the name of Rice University (RICE) nor the names of its
 //   contributors may be used to endorse or promote products derived from
 //   this software without specific prior written permission.
-// 
+//
 // This software is provided by RICE and contributors "as is" and any
 // express or implied warranties, including, but not limited to, the
 // implied warranties of merchantability and fitness for a particular
@@ -40,8 +40,8 @@
 // business interruption) however caused and on any theory of liability,
 // whether in contract, strict liability, or tort (including negligence
 // or otherwise) arising in any way out of the use of this software, even
-// if advised of the possibility of such damage. 
-// 
+// if advised of the possibility of such damage.
+//
 // ******************************************************* EndRiceCopyright *
 
 #ifndef x86_process_inst_h
