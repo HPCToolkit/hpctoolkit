8d03c32b6a078f00791e259d5e856fb2bee77c49
johnmc <johnmc@b1b131fe-c5db-9c05-3d80-cf3d96147e2e>
2010-12-30 17:43:41 +0000

    change to use the blue variants of the valid seals.

--- trunk/doc/www/style/footer-hpctoolkit.js
+++ trunk/doc/www/style/footer-hpctoolkit.js
@@ -8,8 +8,8 @@ document.write('\
       <a href="http://www.cs.rice.edu">Rice Computer Science</a> \
     </p> \
     <a href="http://validator.w3.org/check/referer"> \
-      <img src="http://www.w3.org/Icons/valid-xhtml10" alt="" height="15" /></a> \
+      <img src="http://www.w3.org/Icons/valid-xhtml10-blue" alt="" height="15" /></a> \
     <a href="http://jigsaw.w3.org/css-validator/check/referer"> \
-      <img src="http://www.w3.org/Icons/valid-css" alt="" height="15" /></a> \
+      <img src="http://www.w3.org/Icons/valid-css-blue" alt="" height="15" /></a> \
   </div> \
 ');
