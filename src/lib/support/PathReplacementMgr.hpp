9b66131097f9d8d0f316d146b23d5d69b5cde163
tallent <tallent@b1b131fe-c5db-9c05-3d80-cf3d96147e2e>
2011-01-28 19:42:30 +0000

    PathReplacementMgr::getReplacedPath(): make a const function

--- src/lib/support/PathReplacementMgr.hpp
+++ src/lib/support/PathReplacementMgr.hpp
@@ -84,25 +84,25 @@ public:
   singleton();
 
 
-  //Searches through the list of paths to see if any portion of
-  //'original' needs to be replaced. If a substring of 'original' is
-  //found to be in 'pathReplacement', we update that portion of the
-  //string with the associated new path.
+  // Searches through the list of paths to see if any portion of
+  // 'original' needs to be replaced. If a substring of 'original' is
+  // found to be in 'pathReplacement', we update that portion of the
+  // string with the associated new path.
   //
-  //@param original: The file path we want to update
-  //@return: A new path that is the updated equivalent of 'original'
-  //         if any portion of it needed updating, otherwise we return
-  //         'original'.
+  // @param original: The file path we want to update
+  // @return: A new path that is the updated equivalent of 'original'
+  //          if any portion of it needed updating, otherwise we return
+  //          'original'.
   std::string
-  getReplacedPath(const std::string& original);
+  getReplacedPath(const std::string& original) const;
 
   
-  //Adds an old path and its associated new path as a pair to the
-  //pathReplacement vector and then sorts the list in descending
-  //order of size
-  // 
-  //@param originalPath: The original partial path
-  //@param newPath: The new partial path to replace originalPath.
+  // Adds an old path and its associated new path as a pair to the
+  // pathReplacement vector and then sorts the list in descending
+  // order of size
+  //  
+  // @param originalPath: The original partial path
+  // @param newPath: The new partial path to replace originalPath.
   //
   // **note** - "partial path" can mean any substring of a path, from
   //   a single file name to a full path.
