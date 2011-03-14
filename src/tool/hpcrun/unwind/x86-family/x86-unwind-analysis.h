9e67d5df5f82b301da729ae044e8b54617ff9156
tallent <tallent@b1b131fe-c5db-9c05-3d80-cf3d96147e2e>
2011-03-14 18:13:05 +0000

    Minor cleanup. - Separate XED include files from system and user includes. - Locate includes in top section of source file.

--- src/tool/hpcrun/unwind/x86-family/x86-unwind-analysis.h
+++ src/tool/hpcrun/unwind/x86-family/x86-unwind-analysis.h
@@ -47,11 +47,20 @@
 #ifndef x86_unwind_analysis_h
 #define x86_unwind_analysis_h
 
-#include "xed-interface.h"
+//************************** XED Include Files ******************************
+
+#include <xed-interface.h>
+
+//*************************** User Include Files ****************************
+
 #include "x86-unwind-interval.h"
+
 #include <memory/hpcrun-malloc.h>
+
+//***************************************************************************
 
 extern void *x86_get_branch_target(void *ins,xed_decoded_inst_t *xptr);
 
 #define FIX_INTERVALS_AT_RETURN
+
 #endif  // x86_unwind_analysis_h
