6ecf0a843e894a91b3af96848ab9b240b52afa02
mfagan <mfagan@b1b131fe-c5db-9c05-3d80-cf3d96147e2e>
2011-09-02 16:06:13 +0000

    Fix memory leak in cct2metrics (added debug logging)
    Rename "the_map" pseudo-variable to THREAD_LOCAL_MAP()
    to ensure that it's macro property is visible.

--- trunk/src/tool/hpcrun/cct2metrics.c
+++ trunk/src/tool/hpcrun/cct2metrics.c
@@ -2,6 +2,8 @@
 // cct_node -> metrics map
 //
 #include <stdbool.h>
+#include <string.h>
+#include <stdlib.h>
 
 #include <messages/messages.h>
 #include <memory/hpcrun-malloc.h>
@@ -32,7 +34,7 @@ struct cct2metrics_t {
 // interface functions implicitly reference this map
 // 
 
-#define the_map TD_GET(cct2metrics_map)
+#define THREAD_LOCAL_MAP() TD_GET(cct2metrics_map)
 
 //
 // ******** initialization
@@ -47,6 +49,7 @@ struct cct2metrics_t {
 void
 hpcrun_cct2metrics_init(cct2metrics_t** map)
 {
+  TMSG(CCT2METRICS, "Init, map = %p", *map);
   *map = NULL;
 }
 //
@@ -54,10 +57,36 @@ hpcrun_cct2metrics_init(cct2metrics_t** map)
 // mapping implemented as a splay tree 
 //
 
+void
+help_splay_tree_dump(cct2metrics_t* tree, int indent)
+{
+  if (! tree) return;
+
+  help_splay_tree_dump(tree->right, indent+2);
+
+  char buf[300];
+  for (int i=0; i<indent; i++) buf[i] = ' ';
+  buf[indent] = '\0';
+  snprintf(buf+indent, sizeof(buf)-(indent+1), "%p", tree->node);
+  TMSG(CCT2METRICS, "%s", buf);
+
+  help_splay_tree_dump(tree->left, indent+2);
+}
+
+void
+splay_tree_dump(cct2metrics_t* map)
+{
+  TMSG(CCT2METRICS, "Splay tree %p appears below", map);
+  help_splay_tree_dump(map, 0);
+}
+
+
 static cct2metrics_t*
 splay(cct2metrics_t* map, cct_node_id_t node)
 {
+  TMSG(CCT2METRICS, "splay map = %p, node = %p", map, node);
   REGULAR_SPLAY_TREE(cct2metrics_t, map, node, node, left, right);
+  TMSG(CCT2METRICS, "new map = %p, top node = %p", map, map->node);
   return map;
 }
 
@@ -68,17 +97,11 @@ cct2metrics_new(cct_node_id_t node, metric_set_t* metrics)
   rv->node = node;
   rv->metrics = metrics;
   rv->left = rv->right = NULL;
+  TMSG(CCT2METRICS, "Node: %p, Metrics: %p", rv->node, rv->metrics);
   return rv;
 }
 // ******** Interface operations **********
-// 
 //
-
-// for a given cct node, see if there is a mapping
-// from that node to a set of metrics.
-// That is, check the 
-//
-
 // for a given cct node, return the metric set
 // associated with it.
 //
@@ -89,9 +112,13 @@ cct2metrics_new(cct_node_id_t node, metric_set_t* metrics)
 metric_set_t*
 hpcrun_reify_metric_set(cct_node_id_t cct_id)
 {
+  TMSG(CCT2METRICS, "REIFY: %p", cct_id);
   metric_set_t* rv = hpcrun_get_metric_set(cct_id);
+  TMSG(CCT2METRICS, " -- Metric set found = %p", rv);
   if (rv) return rv;
+  TMSG(CCT2METRICS, " -- Metric set was null, allocating new metric set");
   cct2metrics_assoc(cct_id, rv = hpcrun_metric_set_new());
+  TMSG(CCT2METRICS, "REIFY returns %p", rv);
   return rv;
 }
 
@@ -101,13 +128,18 @@ hpcrun_reify_metric_set(cct_node_id_t cct_id)
 metric_set_t*
 hpcrun_get_metric_set(cct_node_id_t cct_id)
 {
-  cct2metrics_t* map = the_map;
+  cct2metrics_t* map = THREAD_LOCAL_MAP();
+  TMSG(CCT2METRICS, "GET_METRIC_SET for %p, using map %p", cct_id, map);
   if (! map) return NULL;
 
   map = splay(map, cct_id);
-  the_map = map;
+  THREAD_LOCAL_MAP() = map;
+  TMSG(CCT2METRICS, " -- After Splay map = %p", cct_id, map);
 
-  if (map->node == cct_id) return map->metrics;
+  if (map->node == cct_id) return ( TMSG(CCT2METRICS, " -- found %p,"
+                                         " returning metrics", map->node),
+                                    map->metrics );
+  TMSG(CCT2METRICS, " -- cct_id NOT, found. Return NULL");
   return NULL;
 }
 
@@ -126,29 +158,38 @@ hpcrun_has_metric_set(cct_node_id_t cct_id)
 void
 cct2metrics_assoc(cct_node_id_t node, metric_set_t* metrics)
 {
-  cct2metrics_t* map = the_map;
+  cct2metrics_t* map = THREAD_LOCAL_MAP();
+  TMSG(CCT2METRICS, "CCT2METRICS_ASSOC for %p, using map %p", node, map);
   if (! map) {
     map = cct2metrics_new(node, metrics);
+    TMSG(CCT2METRICS, " -- new map created: %p", map);
   }
   else {
     cct2metrics_t* new = cct2metrics_new(node, metrics);
     map = splay(map, node);
+    TMSG(CCT2METRICS, " -- map after splay = %p,"
+         " node sought = %p, mapnode = %p", map, node, map->node);
     if (map->node == node) {
       EMSG("CCT2METRICS map assoc invariant violated");
     }
     else {
       if (map->node < node) {
-        new->left = map->left;
-        new->right = map;
-        map->left = NULL;
-      }
-      else {
+        TMSG(CCT2METRICS, " -- less-than insert %p < %p", map->node, node);
         new->left = map;
         new->right = map->right;
         map->right = NULL;
       }
+      else {
+        TMSG(CCT2METRICS, " -- greater-than insert %p > %p", map->node, node);
+        new->left = map->left;
+        new->right = map;
+        map->left = NULL;
+      }
       map = new;
+      TMSG(CCT2METRICS, " -- new map after insertion %p.(%p, %p)", map->node, map->left, map->right);
     }
   }
-  the_map = map;
+  THREAD_LOCAL_MAP() = map;
+  TMSG(CCT2METRICS, "METRICS_ASSOC final, THREAD_LOCAL_MAP = %p", THREAD_LOCAL_MAP());
+  if (ENABLED(CCT2METRICS)) splay_tree_dump(THREAD_LOCAL_MAP());
 }
