5fb171c83513b3d4d84c6d1e405f506f1ef104cf
krentel <krentel@b1b131fe-c5db-9c05-3d80-cf3d96147e2e>
2012-02-25 22:14:00 +0000

    Update copyright notices to 2012.  It takes months of nights and weekends to track down every little file. :-)

--- trunk/src/tool/hpcrun/env.h
+++ trunk/src/tool/hpcrun/env.h
@@ -12,7 +12,7 @@
 // HPCToolkit is at 'hpctoolkit.org' and in 'README.Acknowledgments'.
 // --------------------------------------------------------------------------
 //
-// Copyright ((c)) 2002-2011, Rice University
+// Copyright ((c)) 2002-2012, Rice University
 // All rights reserved.
 //
 // Redistribution and use in source and binary forms, with or without
@@ -45,7 +45,7 @@
 // ******************************************************* EndRiceCopyright *
 
 /*
-  Copyright ((c)) 2002, Rice University 
+  Copyright ((c)) 2002-2012, Rice University 
   All rights reserved.
 
   Redistribution and use in source and binary forms, with or without
