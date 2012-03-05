e415a7c9401cc6bfa96147959a07d0690842d267
xl10 <xl10@b1b131fe-c5db-9c05-3d80-cf3d96147e2e>
2012-03-05 23:56:37 +0000

    more files for this.

---
+++ branches/hpctoolkit-omp/src/tool/hpcrun/sample-sources/blame-shift.h
@@ -0,0 +1,17 @@
+#ifndef blame_shift_h
+#define blame_shift_h
+
+#include <cct/cct.h>
+
+typedef void (*bs_fn_t)(cct_node_t *node, int metric_value);
+
+typedef struct bs_fn_entry_s {
+ struct bs_fn_entry_s *next;
+ bs_fn_t fn;
+} bs_fn_entry_t;
+
+
+void blame_shift_register(bs_fn_entry_t *entry);
+void blame_shift_apply(cct_node_t *node, int metric_value);
+
+#endif
