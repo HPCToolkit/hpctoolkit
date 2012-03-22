1049f9e659562f101b709c7e256fb0a5b8445d0d
xl10 <xl10@b1b131fe-c5db-9c05-3d80-cf3d96147e2e>
2012-03-22 03:41:12 +0000

    Support nested parallelism (1) handle dynamic thread pool creation and destruction (2) add three idleness metrics:     idleness of requested cores: consider the max cores used in the program     idleness of all cores: consider the total hardware cores in the program     idleness of all threads: consider the total number of threads created by the current parallel region (3) temporarily use libmonitor to cache thread creation and destruction, which didn't take the helper thread into consideration. Since libgomp doesn't have helper threads, the method works fine now. Will insert callback functions into libgomp later.

--- src/tool/hpcrun/sample-sources/idle.c
+++ src/tool/hpcrun/sample-sources/idle.c
@@ -87,6 +87,8 @@
 #include <hpcrun/sample_sources_registered.h>
 #include <hpcrun/sample_event.h>
 #include <hpcrun/thread_data.h>
+#include <hpcrun/cct/cct.h>
+#include <hpcrun/cct2metrics.h>
 #include <utilities/tokenize.h>
 #include <messages/messages.h>
 #include <lush/lush-backtrace.h>
@@ -117,15 +119,23 @@ static void start_fn();
 static void end_fn();
 
 static void process_blame_for_sample(cct_node_t *node, int metric_value);
+void normalize_fn(cct_node_t *node, cct_op_arg_t arg, size_t level);
 
 /******************************************************************************
  * local variables
  *****************************************************************************/
 static uint64_t work = 1L;// by default work is 1 (1 thread)
+static uint64_t thread_num = 1L;// by default thread is 1 (master thread)
+// record the max thread number active concurrently
+// use it to normalize the idleness at the end of measurement
+static uint64_t max_thread_num = 1L;
 
-static int idle_metric_id = -1;
+static int idle_metric_id = -1;//idle for requested cores
+static int core_idle_metric_id = -1;//idle for all hardware cores
+static int thread_idle_metric_id = -1;//idle for all threads
 static int work_metric_id = -1;
 static int overhead_metric_id = -1;
+static int count_metric_id = -1;
 
 static int omp_lm_id = -1;
 
@@ -146,6 +156,11 @@ METHOD_FN(thread_init)
 static void
 METHOD_FN(thread_init_action)
 {
+  // temporirily put stuffs here without considering helper threads
+  // FIXME
+  atomic_add_i64(&thread_num, 1L);
+  if(thread_num > max_thread_num)
+    fetch_and_store_i64(&max_thread_num, thread_num);
 }
 
 static void
@@ -156,11 +171,20 @@ METHOD_FN(start)
 static void
 METHOD_FN(thread_fini_action)
 {
+  // temporirily put stuffs here without considering helper threads
+  // FIXME
+  atomic_add_i64(&thread_num, -1L);
+  atomic_add_i64(&work, -1L);
 }
 
 static void
 METHOD_FN(stop)
 {
+  //scale the requested core idleness here
+  thread_data_t *td = hpcrun_get_thread_data();
+  cct_node_t *root;
+  root = td->epoch->csdata.top;
+  hpcrun_cct_walk_node_1st(root, normalize_fn, NULL);
 }
 
 static void
@@ -188,7 +212,15 @@ METHOD_FN(process_event_list, int lush_metrics)
 	blame_shift_register(&bs_entry);
 
 	idle_metric_id = hpcrun_new_metric();
-	hpcrun_set_metric_info_and_period(idle_metric_id, "p_idleness",
+	hpcrun_set_metric_info_and_period(idle_metric_id, "p_req_core_idleness",
+			MetricFlags_ValFmt_Real, 1);
+
+	core_idle_metric_id = hpcrun_new_metric();
+	hpcrun_set_metric_info_and_period(core_idle_metric_id, "p_all_core_idleness",
+			MetricFlags_ValFmt_Real, 1);
+
+	thread_idle_metric_id = hpcrun_new_metric();
+	hpcrun_set_metric_info_and_period(thread_idle_metric_id, "p_all_thread_idleness",
 			MetricFlags_ValFmt_Real, 1);
 
 	work_metric_id = hpcrun_new_metric();
@@ -197,6 +229,10 @@ METHOD_FN(process_event_list, int lush_metrics)
 
 	overhead_metric_id = hpcrun_new_metric();
 	hpcrun_set_metric_info_and_period(overhead_metric_id, "p_overhead",
+			MetricFlags_ValFmt_Int, 1);
+
+	count_metric_id = hpcrun_new_metric();
+	hpcrun_set_metric_info_and_period(count_metric_id, "count_helper",
 			MetricFlags_ValFmt_Int, 1);
 }
 
@@ -226,6 +262,20 @@ METHOD_FN(display_events)
 /******************************************************************************
  * private operations 
  *****************************************************************************/
+void normalize_fn(cct_node_t *node, cct_op_arg_t arg, size_t level)
+{
+  if(hpcrun_cct_is_leaf(node)) {
+    metric_set_t *set = hpcrun_get_metric_set(node);
+    if(set) {
+      cct_metric_data_t *mdata_idle  = hpcrun_metric_set_loc(set, idle_metric_id);
+      cct_metric_data_t *mdata_count = hpcrun_metric_set_loc(set, count_metric_id);
+      assert(mdata_idle);
+      assert(mdata_count);
+      mdata_idle->r *= (double)max_thread_num;
+      mdata_idle->r -= (double)mdata_count->i;
+    }
+  }
+}
 // for dynamically loaded case, think the overhead comes from the non-idle samples
 // in the openmp library
 static int
@@ -233,6 +283,7 @@ is_overhead(cct_node_t *node)
 {
   cct_addr_t *addr = hpcrun_cct_addr(node);
   load_module_t *lm = hpcrun_loadmap_findById(addr->ip_norm.lm_id);
+  if(!lm) return 0;
   if(lm->id == omp_lm_id) return 1;
   if(strstr(lm->name, OMPstr))
   {
@@ -249,20 +300,32 @@ process_blame_for_sample(cct_node_t *node, int metric_value)
   thread_data_t *td = hpcrun_get_thread_data();
   if (td->idle == 0) { // if thread is not idle
                 double work_l = (double) work;
-		double idle_l = (double)(omp_get_max_threads())- work_l;
-		cct_metric_data_increment(idle_metric_id, node, (cct_metric_data_t){.r = idle_l/work_l});
+		double idle_l = 1.0;
+		cct_metric_data_increment(idle_metric_id, node, 
+					  (cct_metric_data_t){.r = (idle_l/work_l)*metric_value});
+
+                idle_l = (double)(omp_get_num_procs()) - work_l;
+		cct_metric_data_increment(core_idle_metric_id, node, 
+					  (cct_metric_data_t){.r = (idle_l/work_l)*metric_value});
+		
+		idle_l = (double)(thread_num) - work_l;
+		cct_metric_data_increment(thread_idle_metric_id, node, 
+					  (cct_metric_data_t){.r = (idle_l/work_l)*metric_value});
                 if(is_overhead(node))
 		  cct_metric_data_increment(overhead_metric_id, node, (cct_metric_data_t){.i = metric_value});
 		else
 		  cct_metric_data_increment(work_metric_id, node, (cct_metric_data_t){.i = metric_value});
+  		cct_metric_data_increment(count_metric_id, node, (cct_metric_data_t){.i = 1});
   }
 }
 
 void idle_fn()
 {
+  hpcrun_async_block();
   atomic_add_i64(&work, -1L);
   thread_data_t *td = hpcrun_get_thread_data();
   td->idle = 1;
+  hpcrun_async_unblock();
 
   if(trace_isactive()) {
     hpcrun_async_block();
@@ -275,9 +338,11 @@ void idle_fn()
 
 void work_fn()
 {
+  hpcrun_async_block();
   atomic_add_i64(&work, 1L);
   thread_data_t *td = hpcrun_get_thread_data();
   td->idle = 0;
+  hpcrun_async_unblock();
 
   if(trace_isactive()) {
     hpcrun_async_block();
