cd0973802c9c2205a84cf9e891753c7d8c1e3829
mfagan <mfagan@b1b131fe-c5db-9c05-3d80-cf3d96147e2e>
2012-04-14 22:47:11 +0000

    Correct data writing problem:    When a cct node is both a full path AND a prefix of    another path, do not change the sign of the persistent id.

--- trunk/src/tool/hpcrun/cct/cct.h
+++ trunk/src/tool/hpcrun/cct/cct.h
@@ -131,6 +131,12 @@ extern cct_node_t* hpcrun_cct_parent(cct_node_t* node);
 extern int32_t hpcrun_cct_persistent_id(cct_node_t* node);
 extern cct_addr_t* hpcrun_cct_addr(cct_node_t* node);
 extern bool hpcrun_cct_is_leaf(cct_node_t* node);
+//
+// NOTE: having no children is not exactly the same as being a leaf
+//       A leaf represents a full path. There might be full paths
+//       that are a prefix of other full paths. So, a "leaf" can have children
+//
+extern bool hpcrun_cct_no_children(cct_node_t* node);
 extern bool hpcrun_cct_is_root(cct_node_t* node);
 
 //
