475fa995b155d7611038164a63a0fc752c1b88b3
tallent <tallent@b1b131fe-c5db-9c05-3d80-cf3d96147e2e>
2012-04-20 05:55:18 +0000

    Fold --stats option into -M/--metric option.
    The semantics and syntax of the -M/--metric is now more expressive
    (and closer to the original if-I-had-time intention).
      Specify metrics to compute, where <metric> is one of the following:
        sum:   sum over threads/processes
        stats: sum, mean, standard dev, coef of var, min, & max over 
          threads/processes
        thread: per-thread metrics
      May pass multiple times

--- trunk/src/lib/analysis/Args.hpp
+++ trunk/src/lib/analysis/Args.hpp
@@ -143,25 +143,37 @@ public:
   // Attribution/Correlation arguments: special
   // -------------------------------------------------------
 
-  // TODO: Flag for ThreadMetrics; flag for SummaryMetrics; name of Sum Set
-  enum MetricSet {
-    MetricSet_NULL,
-    MetricSet_ThreadOnly,
-    MetricSet_ThreadAndSum,
-    MetricSet_SumOnly
+  enum MetricFlg {
+    MetricFlg_NULL     = 0,
+    MetricFlg_Thread   = (1 << 1),
+    MetricFlg_StatsSum = (1 << 2),
+    MetricFlg_StatsAll = (1 << 3)
   };
 
-  static bool
-  doThreadMetrics(MetricSet x)
-  { return (x == MetricSet_ThreadOnly || x == MetricSet_ThreadAndSum); }
+  static inline bool
+  MetricFlg_isSet(uint flags, MetricFlg x)
+  { return (flags & x); }
 
-  static bool
-  doSummaryMetrics(MetricSet x)
-  { return (x == MetricSet_ThreadAndSum || x == MetricSet_SumOnly); }
+  static inline void
+  MetricFlg_set(uint& flags, MetricFlg x)
+  { flags |= x; }
 
-  bool prof_computeStatistics;
+  static inline void
+  MetricFlg_clear(uint& flags, MetricFlg x)
+  { flags &= ~x; }
 
-  MetricSet prof_metrics;
+  static inline bool
+  MetricFlg_isThread(uint flags)
+  { return MetricFlg_isSet(flags, MetricFlg_Thread); }
+
+  static inline bool
+  MetricFlg_isSum(uint flags)
+  {
+    return (MetricFlg_isSet(flags,  MetricFlg_StatsSum)
+	    || MetricFlg_isSet(flags, MetricFlg_StatsAll));
+  }
+
+  uint prof_metrics;
 
   // TODO: Currently this is always true even though we only need to
   // compute final metric values for (1) hpcproftt (flat) and (2)
--- trunk/src/lib/prof/Metric-Mgr.hpp
+++ trunk/src/lib/prof/Metric-Mgr.hpp
@@ -108,11 +108,12 @@ public:
   // ------------------------------------------------------------
 
   uint
-  makeSummaryMetrics(bool needMultiOccurance, bool needStatistics,
+  makeSummaryMetrics(bool needAllStats, bool needMultiOccurance,
 		     uint srcBegId = Mgr::npos, uint srcEndId = Mgr::npos);
 
   uint
-  makeSummaryMetricsIncr(bool needStatistics, uint srcBegId = Mgr::npos, 
+  makeSummaryMetricsIncr(bool needAllStats,
+			 uint srcBegId = Mgr::npos,
                          uint srcEndId = Mgr::npos);
 
 
