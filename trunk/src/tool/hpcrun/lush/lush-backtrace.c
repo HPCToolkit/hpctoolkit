8029974b9e3d0f87577da7aebb28a187398dbd06
mfagan <mfagan@b1b131fe-c5db-9c05-3d80-cf3d96147e2e>
2012-06-19 14:44:58 +0000

    Restore attachment of thread context by default.  -dd SKIP_THREAD_CTXT will disable the attachment.

--- trunk/src/tool/hpcrun/lush/lush-backtrace.c
+++ trunk/src/tool/hpcrun/lush/lush-backtrace.c
@@ -150,6 +150,7 @@ lush_backtrace2cct(cct_bundle_t* cct, ucontext_t* context,
   // FIXME: unwind/common/backtrace.c
   thread_data_t* td = hpcrun_get_thread_data();
   td->btbuf_cur   = td->btbuf_beg;  // innermost
+
   td->btbuf_sav   = td->btbuf_end;
 
   // ---------------------------------------------------------
