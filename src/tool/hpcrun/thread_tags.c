a9e161f27dc16421fc2a11ac765d8b0edb8f7947
xl10 <xl10@b1b131fe-c5db-9c05-3d80-cf3d96147e2e>
2012-11-14 16:47:47 +0000

    add thread tags in the trace view

---
+++ src/tool/hpcrun/thread_tags.c
@@ -0,0 +1,273 @@
+#include <string.h>
+#include "thread_tags.h"
+
+void thread0() {}
+void thread1() {}
+void thread2() {}
+void thread3() {}
+void thread4() {}
+void thread5() {}
+void thread6() {}
+void thread7() {}
+void thread8() {}
+void thread9() {}
+void thread10() {}
+void thread11() {}
+void thread12() {}
+void thread13() {}
+void thread14() {}
+void thread15() {}
+void thread16() {}
+void thread17() {}
+void thread18() {}
+void thread19() {}
+void thread20() {}
+void thread21() {}
+void thread22() {}
+void thread23() {}
+void thread24() {}
+void thread25() {}
+void thread26() {}
+void thread27() {}
+void thread28() {}
+void thread29() {}
+void thread30() {}
+void thread31() {}
+void thread32() {}
+void thread33() {}
+void thread34() {}
+void thread35() {}
+void thread36() {}
+void thread37() {}
+void thread38() {}
+void thread39() {}
+void thread40() {}
+void thread41() {}
+void thread42() {}
+void thread43() {}
+void thread44() {}
+void thread45() {}
+void thread46() {}
+void thread47() {}
+void thread48() {}
+void thread49() {}
+void thread50() {}
+void thread51() {}
+void thread52() {}
+void thread53() {}
+void thread54() {}
+void thread55() {}
+void thread56() {}
+void thread57() {}
+void thread58() {}
+void thread59() {}
+void thread60() {}
+void thread61() {}
+void thread62() {}
+void thread63() {}
+void thread64() {}
+void thread65() {}
+void thread66() {}
+void thread67() {}
+void thread68() {}
+void thread69() {}
+void thread70() {}
+void thread71() {}
+void thread72() {}
+void thread73() {}
+void thread74() {}
+void thread75() {}
+void thread76() {}
+void thread77() {}
+void thread78() {}
+void thread79() {}
+void thread80() {}
+void thread81() {}
+void thread82() {}
+void thread83() {}
+void thread84() {}
+void thread85() {}
+void thread86() {}
+void thread87() {}
+void thread88() {}
+void thread89() {}
+void thread90() {}
+void thread91() {}
+void thread92() {}
+void thread93() {}
+void thread94() {}
+void thread95() {}
+void thread96() {}
+void thread97() {}
+void thread98() {}
+void thread99() {}
+void thread100() {}
+void thread101() {}
+void thread102() {}
+void thread103() {}
+void thread104() {}
+void thread105() {}
+void thread106() {}
+void thread107() {}
+void thread108() {}
+void thread109() {}
+void thread110() {}
+void thread111() {}
+void thread112() {}
+void thread113() {}
+void thread114() {}
+void thread115() {}
+void thread116() {}
+void thread117() {}
+void thread118() {}
+void thread119() {}
+void thread120() {}
+void thread121() {}
+void thread122() {}
+void thread123() {}
+void thread124() {}
+void thread125() {}
+void thread126() {}
+void thread127() {}
+void thread128() {}
+void thread129() {}
+void thread130() {}
+
+void *
+get_thread_func(int id)
+{
+  switch(id) {
+    case 0 : return *(void **)&thread0+16;
+    case 1 : return *(void **)&thread1+16;
+    case 2 : return *(void **)&thread2+16;
+    case 3 : return *(void **)&thread3+16;
+    case 4 : return *(void **)&thread4+16;
+    case 5 : return *(void **)&thread5+16;
+    case 6 : return *(void **)&thread6+16;
+    case 7 : return *(void **)&thread7+16;
+    case 8 : return *(void **)&thread8+16;
+    case 9 : return *(void **)&thread9+16;
+    case 10 : return *(void **)&thread10+16;
+    case 11 : return *(void **)&thread11+16;
+    case 12 : return *(void **)&thread12+16;
+    case 13 : return *(void **)&thread13+16;
+    case 14 : return *(void **)&thread14+16;
+    case 15 : return *(void **)&thread15+16;
+    case 16 : return *(void **)&thread16+16;
+    case 17 : return *(void **)&thread17+16;
+    case 18 : return *(void **)&thread18+16;
+    case 19 : return *(void **)&thread19+16;
+    case 20 : return *(void **)&thread20+16;
+    case 21 : return *(void **)&thread21+16;
+    case 22 : return *(void **)&thread22+16;
+    case 23 : return *(void **)&thread23+16;
+    case 24 : return *(void **)&thread24+16;
+    case 25 : return *(void **)&thread25+16;
+    case 26 : return *(void **)&thread26+16;
+    case 27 : return *(void **)&thread27+16;
+    case 28 : return *(void **)&thread28+16;
+    case 29 : return *(void **)&thread29+16;
+    case 30 : return *(void **)&thread30+16;
+    case 31 : return *(void **)&thread31+16;
+    case 32 : return *(void **)&thread32+16;
+    case 33 : return *(void **)&thread33+16;
+    case 34 : return *(void **)&thread34+16;
+    case 35 : return *(void **)&thread35+16;
+    case 36 : return *(void **)&thread36+16;
+    case 37 : return *(void **)&thread37+16;
+    case 38 : return *(void **)&thread38+16;
+    case 39 : return *(void **)&thread39+16;
+    case 40 : return *(void **)&thread40+16;
+    case 41 : return *(void **)&thread41+16;
+    case 42 : return *(void **)&thread42+16;
+    case 43 : return *(void **)&thread43+16;
+    case 44 : return *(void **)&thread44+16;
+    case 45 : return *(void **)&thread45+16;
+    case 46 : return *(void **)&thread46+16;
+    case 47 : return *(void **)&thread47+16;
+    case 48 : return *(void **)&thread48+16;
+    case 49 : return *(void **)&thread49+16;
+    case 50 : return *(void **)&thread50+16;
+    case 51 : return *(void **)&thread51+16;
+    case 52 : return *(void **)&thread52+16;
+    case 53 : return *(void **)&thread53+16;
+    case 54 : return *(void **)&thread54+16;
+    case 55 : return *(void **)&thread55+16;
+    case 56 : return *(void **)&thread56+16;
+    case 57 : return *(void **)&thread57+16;
+    case 58 : return *(void **)&thread58+16;
+    case 59 : return *(void **)&thread59+16;
+    case 60 : return *(void **)&thread60+16;
+    case 61 : return *(void **)&thread61+16;
+    case 62 : return *(void **)&thread62+16;
+    case 63 : return *(void **)&thread63+16;
+    case 64 : return *(void **)&thread64+16;
+    case 65 : return *(void **)&thread65+16;
+    case 66 : return *(void **)&thread66+16;
+    case 67 : return *(void **)&thread67+16;
+    case 68 : return *(void **)&thread68+16;
+    case 69 : return *(void **)&thread69+16;
+    case 70 : return *(void **)&thread70+16;
+    case 71 : return *(void **)&thread71+16;
+    case 72 : return *(void **)&thread72+16;
+    case 73 : return *(void **)&thread73+16;
+    case 74 : return *(void **)&thread74+16;
+    case 75 : return *(void **)&thread75+16;
+    case 76 : return *(void **)&thread76+16;
+    case 77 : return *(void **)&thread77+16;
+    case 78 : return *(void **)&thread78+16;
+    case 79 : return *(void **)&thread79+16;
+    case 80 : return *(void **)&thread80+16;
+    case 81 : return *(void **)&thread81+16;
+    case 82 : return *(void **)&thread82+16;
+    case 83 : return *(void **)&thread83+16;
+    case 84 : return *(void **)&thread84+16;
+    case 85 : return *(void **)&thread85+16;
+    case 86 : return *(void **)&thread86+16;
+    case 87 : return *(void **)&thread87+16;
+    case 88 : return *(void **)&thread88+16;
+    case 89 : return *(void **)&thread89+16;
+    case 90 : return *(void **)&thread90+16;
+    case 91 : return *(void **)&thread91+16;
+    case 92 : return *(void **)&thread92+16;
+    case 93 : return *(void **)&thread93+16;
+    case 94 : return *(void **)&thread94+16;
+    case 95 : return *(void **)&thread95+16;
+    case 96 : return *(void **)&thread96+16;
+    case 97 : return *(void **)&thread97+16;
+    case 98 : return *(void **)&thread98+16;
+    case 99 : return *(void **)&thread99+16;
+    case 100 : return *(void **)&thread100+16;
+    case 101 : return *(void **)&thread101+16;
+    case 102 : return *(void **)&thread102+16;
+    case 103 : return *(void **)&thread103+16;
+    case 104 : return *(void **)&thread104+16;
+    case 105 : return *(void **)&thread105+16;
+    case 106 : return *(void **)&thread106+16;
+    case 107 : return *(void **)&thread107+16;
+    case 108 : return *(void **)&thread108+16;
+    case 109 : return *(void **)&thread109+16;
+    case 110 : return *(void **)&thread110+16;
+    case 111 : return *(void **)&thread111+16;
+    case 112 : return *(void **)&thread112+16;
+    case 113 : return *(void **)&thread113+16;
+    case 114 : return *(void **)&thread114+16;
+    case 115 : return *(void **)&thread115+16;
+    case 116 : return *(void **)&thread116+16;
+    case 117 : return *(void **)&thread117+16;
+    case 118 : return *(void **)&thread118+16;
+    case 119 : return *(void **)&thread119+16;
+    case 120 : return *(void **)&thread120+16;
+    case 121 : return *(void **)&thread121+16;
+    case 122 : return *(void **)&thread122+16;
+    case 123 : return *(void **)&thread123+16;
+    case 124 : return *(void **)&thread124+16;
+    case 125 : return *(void **)&thread125+16;
+    case 126 : return *(void **)&thread126+16;
+    case 127 : return *(void **)&thread127+16;
+    case 128 : return *(void **)&thread128+16;
+    case 129 : return *(void **)&thread129+16;
+    case 130 : return *(void **)&thread130+16;
+  }
+  return NULL;
+}
