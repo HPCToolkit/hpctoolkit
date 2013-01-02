57885711c2dc0620399481f9fdc1a9289dafcb5e
krentel <krentel@b1b131fe-c5db-9c05-3d80-cf3d96147e2e>
2013-01-02 20:08:23 +0000

    Update copyright notices to 2013.

--- src/tool/hpcrun/unwind/ppc64/ppc64-unwind-interval.c
+++ src/tool/hpcrun/unwind/ppc64/ppc64-unwind-interval.c
@@ -13,7 +13,7 @@
 // HPCToolkit is at 'hpctoolkit.org' and in 'README.Acknowledgments'.
 // --------------------------------------------------------------------------
 //
-// Copyright ((c)) 2002-2012, Rice University
+// Copyright ((c)) 2002-2013, Rice University
 // All rights reserved.
 //
 // Redistribution and use in source and binary forms, with or without
