88b438fbfc9a3bf4869716686a4b52d1a3441e8e
jmellorcrummey@comcast.net <jmellorcrummey@comcast.net@b1b131fe-c5db-9c05-3d80-cf3d96147e2e>
2013-07-23 22:43:49 +0000

    commit chnges to task context.

--- branches/hpctoolkit-ompt/src/tool/hpcrun/utilities/task-cntxt.h
+++ branches/hpctoolkit-ompt/src/tool/hpcrun/utilities/task-cntxt.h
@@ -8,18 +8,13 @@
  * this interface is used for lazy callpath resolution 
 */
 
-/* return the task creation context (or NULL) */
-void* need_task_cntxt();
-
+#if 0
 /* collect full task context, using eager unwinds where needed */
 void task_cntxt_full();
-
-void* copy_task_cntxt(void *);
-
-void hack_task_context(frame_t **, frame_t **);
 
 // export registration interfaces for ompt
 void start_task_fn(ompt_data_t *parent_task_data, 
 		   ompt_frame_t *parent_task_frame, 
 		   ompt_data_t *new_task_data);
+#endif
 #endif
