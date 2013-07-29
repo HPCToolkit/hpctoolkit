141145a0b5f8b59a623e079ad559a9cbf800ae47
jmellorcrummey <jmellorcrummey@comcast.net>
2013-07-29 05:40:16 +0000

    clear out junk in idle.h

--- src/tool/hpcrun/sample-sources/idle.h
+++ src/tool/hpcrun/sample-sources/idle.h
@@ -1,8 +1,6 @@
 #ifndef _IDLE_H_
 #define _IDLE_H_
 
-#include <ompt.h>
-
 typedef int (*idle_blame_participant_fn)(void);
 
 void idle_metric_register_blame_source
@@ -15,16 +13,6 @@ void idle_metric_thread_start(void);
 void idle_metric_thread_end(void);
 
 void idle_metric_enable(); 
-
-
-#if 0
-void idle_fn(ompt_data_t *thread_data);
-void work_fn(ompt_data_t *thread_data);
-void start_fn(ompt_data_t *thread_data);
-void end_fn(ompt_data_t *thread_data);
-void bar_wait_begin(ompt_data_t *task_data, ompt_parallel_id_t parallel_id);
-void bar_wait_end(ompt_data_t *task_data, ompt_parallel_id_t parallel_id);
-#endif
 
 #endif
 
