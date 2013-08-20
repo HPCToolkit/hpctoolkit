105b13796618adeaea776d4f8974f00805f3d523
chabbi.milind@gmail.com <chabbi.milind@gmail.com@b1b131fe-c5db-9c05-3d80-cf3d96147e2e>
2013-08-20 04:35:44 +0000

    CPU_GPU_IDLE:removed support for collecting teh goodness metric for well overlapped regions. I did not find the feature useful in any real application.

--- trunk/src/tool/hpcrun/sample-sources/gpu_blame-overrides.c
+++ trunk/src/tool/hpcrun/sample-sources/gpu_blame-overrides.c
@@ -2026,21 +2026,10 @@ void gpu_blame_shifter(int metric_id, cct_node_t * node,  int metric_dc) {
     unfinished_event_list_head = g_unfinished_stream_list_head;
     
     if (num_unfinshed_streams) {
-        // CPU - GPU overlap
         
-        // Increment cpu_overlap by metric_incr
-        cct_metric_data_increment(cpu_overlap_metric_id, node, (cct_metric_data_t) {
-            .r = metric_incr}
-                                  );
-        
-        // Increment gpu_overlap by metric_incr/num_unfinshed_streams for each of the unfinshed streams
-        for (stream_node_t * unfinished_stream = unfinished_event_list_head; unfinished_stream; unfinished_stream = unfinished_stream->next_unfinished_stream) {
-            cct_metric_data_increment(gpu_overlap_metric_id, unfinished_stream->unfinished_event_node->launcher_cct, (cct_metric_data_t) {
-                .r = metric_incr * 1.0 / num_unfinshed_streams}
-                                      );
-
-            //SHARED BLAMING: kernels need to be blamed for idleness on other procs/threads.
-            if(SHARED_BLAMING_INITIALISED && ipc_data->num_threads_at_sync_all_procs && !g_num_threads_at_sync) {
+        //SHARED BLAMING: kernels need to be blamed for idleness on other procs/threads.
+        if(SHARED_BLAMING_INITIALISED && ipc_data->num_threads_at_sync_all_procs && !g_num_threads_at_sync) {
+            for (stream_node_t * unfinished_stream = unfinished_event_list_head; unfinished_stream; unfinished_stream = unfinished_stream->next_unfinished_stream) {
                 //TODO: FIXME: the local threads at sync need to be removed, /T has to be done while adding metric
                 //increment (either one of them).
                cct_metric_data_increment(cpu_idle_cause_metric_id, unfinished_stream->unfinished_event_node->launcher_cct, (cct_metric_data_t) {
@@ -2048,7 +2037,6 @@ void gpu_blame_shifter(int metric_id, cct_node_t * node,  int metric_dc) {
                                       );
             }
          }
-
     } else {
         
         /*** Code to account for Overload factor ***/
