a404daaeb2018f88df97f23833c6f68450f9bdd0
jmellorcrummey <jmellorcrummey@comcast.net>
2013-09-05 04:33:07 +0000

    OMPT entry points.

---
+++ src/tool/hpcrun/ompt/ompt-fns.h
@@ -0,0 +1,12 @@
+// OMPT API functions
+
+ompt_interface_fn(ompt_enumerate_state)
+ompt_interface_fn(ompt_set_callback)
+ompt_interface_fn(ompt_get_callback)
+ompt_interface_fn(ompt_get_parallel_id)
+ompt_interface_fn(ompt_get_state)
+ompt_interface_fn(ompt_get_idle_frame)
+ompt_interface_fn(ompt_get_task_id)
+ompt_interface_fn(ompt_get_task_frame)
+ompt_interface_fn(ompt_get_ompt_version)
+ompt_interface_fn(ompt_get_runtime_version)
