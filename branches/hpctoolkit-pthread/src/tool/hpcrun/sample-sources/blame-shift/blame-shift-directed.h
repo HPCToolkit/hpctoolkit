12a9ecc8e4eb7875f07a25384f62b59ef907ff45
jmellorcrummey@comcast.net <jmellorcrummey@comcast.net@b1b131fe-c5db-9c05-3d80-cf3d96147e2e>
2014-01-06 23:36:55 +0000

    new pthreads support.

---
+++ branches/hpctoolkit-pthread/src/tool/hpcrun/sample-sources/blame-shift/blame-shift-directed.h
@@ -0,0 +1,14 @@
+#ifndef __hpctoolkit_blame_shift_directed_h__
+#define __hpctoolkit_blame_shift_directed_h__
+
+#include <stdint.h>
+
+/******************************************************************************
+ * interface operations for clients 
+ *****************************************************************************/
+
+void directed_blame_shift_start(uint64_t obj) ;
+void directed_blame_shift_end();
+void directed_blame_accept(uint64_t obj);
+
+#endif // __hpctoolkit_blame_shift_directed_h__
