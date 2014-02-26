8917344ac60128f6b816dd277612a749d08cfa00
mwkrentel <mwkrentel@b1b131fe-c5db-9c05-3d80-cf3d96147e2e>
2014-02-26 22:25:01 +0000

    Time for the old hpctoolkit-inline branch to meet the true death. The inline2 branch is also deprecated and will be deleted in due time, but not just yet.

--- branches/hpctoolkit-inline/src/tool/hpcrun/utilities/arch/ucontext-manip.h
+++
@@ -1,7 +0,0 @@
-#ifndef UCONTEXT_MANIP
-#define UCONTEXT_MANIP
-
-#include "specific-ucontext-manip.h" // DO NOT chg to <> include syntax:
-                                     // uses -I cpp arg to select architecture
-
-#endif // UCONTEXT_MANIP
