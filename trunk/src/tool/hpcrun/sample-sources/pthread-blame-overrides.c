a3869b97a036610b579d50f264b8c74db71392db
mfagan451 <mfagan451@b1b131fe-c5db-9c05-3d80-cf3d96147e2e>
2014-11-09 03:36:31 +0000

    Add sem_wait to the pthread sample source (synchronous)

--- trunk/src/tool/hpcrun/sample-sources/pthread-blame-overrides.c
+++ trunk/src/tool/hpcrun/sample-sources/pthread-blame-overrides.c
@@ -1,6 +1,16 @@
 #include <pthread.h>
 #include <dlfcn.h>
 
+//
+// Investigate TBB
+//
+#include <sched.h>
+
+#include <semaphore.h>
+#include <stdio.h>
+
+#include <lib/prof-lean/atomic.h>
+
 #include <sample-sources/blame-shift/blame-map.h>
 #include <sample-sources/pthread-blame.h>
 #include <hpcrun/thread_data.h>
@@ -12,6 +22,11 @@
 #include <hpcrun/safe-sampling.h>
 #include <hpcrun/sample_event.h>
 #include <messages/messages.h>
+
+//
+// No need to grab monitor.h for a single function
+//
+extern void monitor_real_abort(void);
 
 //
 // NOTE 1: the following functions (apparently) need dlvsym instead of dlsym to obtain the 
@@ -60,6 +75,14 @@
 #define pthread_spin_lock_REAL        DL
 #define pthread_spin_unlock_REAL      DL
 
+//
+// TBB investigation
+//
+
+#define sched_yield_REAL              DL
+#define sem_wait_REAL                 DL
+#define sem_post_REAL                 DL
+#define sem_timedwait_REAL            DL
 
 // 
 // Typedefs and declarations for real routines that are overridden
@@ -254,4 +277,93 @@ override_lookupv(char* fname)
     hpcrun_abort("dlsym(RTLD_NEXT, %s) failed: %s", fname, e);
   }
   return rv;
+}
+
+//
+// Further determination of TBB mechanisms
+//
+
+// **** global vars for now ****
+//
+
+static unsigned int calls_to_sched_yield = 0;
+static unsigned int calls_to_sem_wait    = 0;
+
+REAL_TYPEDEF(int, sched_yield)(void);
+REAL_DCL(sched_yield);
+
+int
+OVERRIDE_NM(sched_yield)(void)
+{
+  REAL_INIT(sched_yield);
+
+  // TMSG(TBB_EACH, "sched_yield hit");
+
+  int retval = REAL_FN(sched_yield)();
+
+  // __sync_fetch_and_add(&calls_to_sched_yield, 1);
+  return retval;
+}
+
+REAL_TYPEDEF(int, sem_wait)(sem_t* sem);
+REAL_TYPEDEF(int, sem_post)(sem_t* sem);
+REAL_TYPEDEF(int, sem_timedwait)(sem_t* sem, const struct timespec* abs_timeout);
+
+
+REAL_DCL(sem_wait);
+REAL_DCL(sem_post);
+REAL_DCL(sem_timedwait);
+
+int
+OVERRIDE_NM(sem_wait)(sem_t* sem)
+{
+  REAL_INIT(sem_wait);
+
+  if (! pthread_blame_lockwait_enabled() ) {
+    return REAL_FN(sem_wait)(sem);
+  }
+  TMSG(TBB_EACH, "sem wait hit, sem = %p", sem);
+  pthread_directed_blame_shift_blocked_start(sem);
+  int retval = REAL_FN(sem_wait)(sem);
+  pthread_directed_blame_shift_end();
+
+  // hpcrun_atomicIncr(&calls_to_sem_wait);
+  // calls_to_sem_wait++;
+  return retval;
+}
+
+int
+OVERRIDE_NM(sem_post)(sem_t* sem)
+{
+  REAL_INIT(sem_post);
+
+  TMSG(LOCKWAIT, "sem_post ENCOUNTERED");
+  if (! pthread_blame_lockwait_enabled() ) {
+    return REAL_FN(sem_post)(sem);
+  }
+  TMSG(LOCKWAIT, "sem POST");
+  int retval = REAL_FN(sem_post)(sem);
+  pthread_directed_blame_accept(sem);
+  // TMSG(TBB_EACH, "sem post hit, sem = %p", sem);
+
+  return retval;
+}
+
+int
+OVERRIDE_NM(sem_timedwait)(sem_t* sem, const struct timespec* abs_timeout)
+{
+  REAL_INIT(sem_timedwait);
+
+  TMSG(TBB_EACH, "sem timedwait hit, sem = %p", sem);
+  int retval = REAL_FN(sem_timedwait)(sem, abs_timeout);
+
+  return retval;
+}
+
+void
+tbb_stats(void)
+{
+  AMSG("TBB stuff: ");
+  AMSG("Calls to sched yield: %d", calls_to_sched_yield);
+  AMSG("Calls to sem wait", calls_to_sem_wait);
 }
