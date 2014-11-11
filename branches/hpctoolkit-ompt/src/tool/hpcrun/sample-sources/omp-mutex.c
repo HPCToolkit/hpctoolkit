cdcca11160bab4069d0169aa745e5eb1817ca642
jmellorcrummey@comcast.net <jmellorcrummey@comcast.net@b1b131fe-c5db-9c05-3d80-cf3d96147e2e>
2014-11-11 13:20:33 +0000

    add missing file.

---
+++ branches/hpctoolkit-ompt/src/tool/hpcrun/sample-sources/omp-mutex.c
@@ -0,0 +1,173 @@
+// -*-Mode: C++;-*- // technically C99
+
+// * BeginRiceCopyright *****************************************************
+//
+// $HeadURL: $
+// $Id$
+//
+// --------------------------------------------------------------------------
+// Part of HPCToolkit (hpctoolkit.org)
+//
+// Information about sources of support for research and development of
+// HPCToolkit is at 'hpctoolkit.org' and in 'README.Acknowledgments'.
+// --------------------------------------------------------------------------
+//
+// Copyright ((c)) 2002-2012, Rice University
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are
+// met:
+//
+// * Redistributions of source code must retain the above copyright
+//   notice, this list of conditions and the following disclaimer.
+//
+// * Redistributions in binary form must reproduce the above copyright
+//   notice, this list of conditions and the following disclaimer in the
+//   documentation and/or other materials provided with the distribution.
+//
+// * Neither the name of Rice University (RICE) nor the names of its
+//   contributors may be used to endorse or promote products derived from
+//   this software without specific prior written permission.
+//
+// This software is provided by RICE and contributors "as is" and any
+// express or implied warranties, including, but not limited to, the
+// implied warranties of merchantability and fitness for a particular
+// purpose are disclaimed. In no event shall RICE or contributors be
+// liable for any direct, indirect, incidental, special, exemplary, or
+// consequential damages (including, but not limited to, procurement of
+// substitute goods or services; loss of use, data, or profits; or
+// business interruption) however caused and on any theory of liability,
+// whether in contract, strict liability, or tort (including negligence
+// or otherwise) arising in any way out of the use of this software, even
+// if advised of the possibility of such damage.
+//
+// ******************************************************* EndRiceCopyright *
+
+//
+// directed blame shifting for locks, critical sections, ...
+//
+
+/******************************************************************************
+ * system includes
+ *****************************************************************************/
+
+#include <string.h>
+
+
+
+/******************************************************************************
+ * local includes
+ *****************************************************************************/
+
+#include "simple_oo.h"
+#include "sample_source_obj.h"
+#include "common.h"
+
+
+#include "ompt/ompt-interface.h"
+
+
+
+/******************************************************************************
+ * macros
+ *****************************************************************************/
+
+#define DIRECTED_BLAME_NAME "OMP_MUTEX"
+
+
+
+/*--------------------------------------------------------------------------
+ | sample source methods
+ --------------------------------------------------------------------------*/
+
+static void
+METHOD_FN(init)
+{
+  self->state = INIT;
+}
+
+
+static void
+METHOD_FN(thread_init)
+{
+}
+
+
+static void
+METHOD_FN(thread_init_action)
+{
+}
+
+
+static void
+METHOD_FN(start)
+{
+  ompt_mutex_blame_shift_enable();
+}
+
+
+static void
+METHOD_FN(thread_fini_action)
+{
+}
+
+
+static void
+METHOD_FN(stop)
+{
+}
+
+
+static void
+METHOD_FN(shutdown)
+{
+  self->state = UNINIT;
+}
+
+
+static bool
+METHOD_FN(supports_event,const char *ev_str)
+{
+  return (strstr(ev_str, DIRECTED_BLAME_NAME) != NULL);
+}
+
+ 
+static void
+METHOD_FN(process_event_list, int lush_metrics)
+{
+  ompt_mutex_blame_shift_register();
+}
+
+
+static void
+METHOD_FN(gen_event_set,int lush_metrics)
+{
+}
+
+
+static void
+METHOD_FN(display_events)
+{
+  printf("===========================================================================\n");
+  printf("Available OpenMP directed blame shifting preset events\n");
+  printf("===========================================================================\n");
+  printf("Name\t\tDescription\n");
+  printf("---------------------------------------------------------------------------\n");
+  printf("%s\tWhen waiting for an OpenMP mutex (i.e., a lock or critical\n"
+         "\t\tsection), shift blame for waiting to code holding the mutex.\n"
+         "\t\tOnly suitable for OpenMP programs.\n",
+	 DIRECTED_BLAME_NAME);
+  printf("\n");
+}
+
+
+
+/*--------------------------------------------------------------------------
+ | sample source object
+ --------------------------------------------------------------------------*/
+
+#define ss_name ompt_directed_blame
+#define ss_cls SS_SOFTWARE
+
+#include "ss_obj.h"
