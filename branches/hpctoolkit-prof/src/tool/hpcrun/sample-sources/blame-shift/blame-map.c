2c08f1e34f65079fe33a7d3a987e7963745c7f14
mwkrentel <mwkrentel@b1b131fe-c5db-9c05-3d80-cf3d96147e2e>
2015-01-08 20:34:56 +0000

    sync hpctoolkit-prof with trunk rev 4673

--- branches/hpctoolkit-prof/src/tool/hpcrun/sample-sources/blame-shift/blame-map.c
+++ branches/hpctoolkit-prof/src/tool/hpcrun/sample-sources/blame-shift/blame-map.c
@@ -12,7 +12,7 @@
 // HPCToolkit is at 'hpctoolkit.org' and in 'README.Acknowledgments'.
 // --------------------------------------------------------------------------
 //
-// Copyright ((c)) 2002-2012, Rice University
+// Copyright ((c)) 2002-2015, Rice University
 // All rights reserved.
 //
 // Redistribution and use in source and binary forms, with or without
