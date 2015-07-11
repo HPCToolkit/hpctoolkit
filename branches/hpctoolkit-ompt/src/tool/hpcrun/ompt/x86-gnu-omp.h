477adbde0aad60517a4587e11a2974f6b4518047
jmellorcrummey@comcast.net <jmellorcrummey@comcast.net@b1b131fe-c5db-9c05-3d80-cf3d96147e2e>
2015-07-11 23:14:45 +0000

    Add support to unify callstacks of master and worker threads by adjusting return address obtained inside either parallel begin/end to match the return address seen by the master thread.

--- branches/hpctoolkit-ompt/src/tool/hpcrun/ompt/x86-gnu-omp.h
+++ branches/hpctoolkit-ompt/src/tool/hpcrun/ompt/x86-gnu-omp.h
@@ -48,7 +48,7 @@
 // local includes
 //******************************************************************************
 
-#include <lib/isa-lean/power/instruction-set.h>
+#include <unwind/x86-family/x86-decoder.h>
 
 
 
@@ -70,13 +70,44 @@ length_of_call_instruction()
   return X86_CALL_NBYTES; 
 } 
 
+
 // given an instruction pointer, find the next call instruction. return the 
 // difference in bytes between the end of that call instruction and the 
 // initial instruction pointer
 static inline int 
 offset_to_pc_after_next_call(void *ip)
 {
-  assert(0);
-} 
+  xed_decoded_inst_t xedd;
+  xed_decoded_inst_t *xptr = &xedd;
+  xed_error_enum_t xed_error;
 
+  uint8_t *ins = ip; 
 
+  xed_decoded_inst_zero_set_mode(xptr, &x86_decoder_settings.xed_settings);
+
+  // continue until success or failure
+  for (;;) {
+
+    xed_decoded_inst_zero_keep_mode(xptr);
+    xed_error = xed_decode(xptr, ins, 15);
+
+    if (xed_error != XED_ERROR_NONE) {
+        // on a failure, perform no adjustment.
+        return 0;
+    }
+
+    // advance ins to point at the next instruction
+    ins += xed_decoded_inst_get_length(xptr);
+
+    xed_iclass_enum_t xiclass = xed_decoded_inst_get_iclass(xptr);
+
+    switch(xiclass) {
+    case XED_ICLASS_CALL_FAR:
+    case XED_ICLASS_CALL_NEAR:
+       return ins - (uint8_t *) ip;
+    default:
+       break;
+    }
+  }
+
+}
