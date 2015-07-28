2bf2ee5cc06f37539d6ee7af06b5b7770bb2920f
jmellorcrummey <jmellorcrummey@comcast.net>
2015-07-28 22:22:08 +0000

    clean up properties, comments, and API for MCS locks.

--- src/lib/prof-lean/mcs-lock.c
+++ src/lib/prof-lean/mcs-lock.c
@@ -1,5 +1,60 @@
+// * BeginRiceCopyright *****************************************************
+//
+// $HeadURL$
+// $Id$
+//
+// --------------------------------------------------------------------------
+// Part of HPCToolkit (hpctoolkit.org)
+//
+// Information about sources of support for research and development of
+// HPCToolkit is at 'hpctoolkit.org' and in 'README.Acknowledgments'.
+// --------------------------------------------------------------------------
+//
+// Copyright ((c)) 2002-2015, Rice University
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are
+// met:
+//
+// * Redistributions of source code must retain the above copyright
+//   notice, this list of conditions and the following disclaimer.
+//
+// * Redistributions in binary form must reproduce the above copyright
+//   notice, this list of conditions and the following disclaimer in the
+//   documentation and/or other materials provided with the distribution.
+//
+// * Neither the name of Rice University (RICE) nor the names of its
+//   contributors may be used to endorse or promote products derived from
+//   this software without specific prior written permission.
+//
+// This software is provided by RICE and contributors "as is" and any
+// express or implied warranties, including, but not limited to, the
+// implied warranties of merchantability and fitness for a particular
+// purpose are disclaimed. In no event shall RICE or contributors be
+// liable for any direct, indirect, incidental, special, exemplary, or
+// consequential damages (including, but not limited to, procurement of
+// substitute goods or services; loss of use, data, or profits; or
+// business interruption) however caused and on any theory of liability,
+// whether in contract, strict liability, or tort (including negligence
+// or otherwise) arising in any way out of the use of this software, even
+// if advised of the possibility of such damage.
+//
+// ******************************************************* EndRiceCopyright *
+
 //******************************************************************************
-// MCS lock - a fair based queueing lock
+//
+// File:
+//   $HeadURL$
+//
+// Purpose:
+//   Implement an API for the MCS lock: a fair queue-based lock.
+//
+// Reference:
+//   John M. Mellor-Crummey and Michael L. Scott. 1991. Algorithms for scalable 
+//   synchronization on shared-memory multiprocessors. ACM Transactions on 
+//   Computing Systems 9, 1 (February 1991), 21-65. 
+//   http://doi.acm.org/10.1145/103727.103729
 //******************************************************************************
 
 
@@ -53,7 +108,6 @@ mcs_lock(mcs_lock_t *l, mcs_node_t *me)
   // install my node at the tail of the lock queue. 
   // determine my predecessor, if any.
   //--------------------------------------------------------------------
-  // mcs_node_t *predecessor = (mcs_node_t *) fetch_and_store(l, me);
   mcs_node_t *predecessor = fas(&(l->tail), me);
 
   //--------------------------------------------------------------------
@@ -104,7 +158,7 @@ mcs_unlock(mcs_lock_t *l, mcs_node_t *me)
     enforce_access_to_rmw_order(); 
 
     //--------------------------------------------------------------------
-    // if my node is at the tail of the queue, remove myself
+    // if my node is at the tail of the queue, attempt to remove myself
     //--------------------------------------------------------------------
     if (cas_bool(&(l->tail), me, 0)) {
       //------------------------------------------------------------------
