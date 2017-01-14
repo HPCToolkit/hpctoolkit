98a2a827540637f4c6bf36a6728cd64d51317cc6
Doug Moore <dougm@rice.edu>
2017-01-13 23:54:28 -0600

    Remove cskiplist_ilmstat_btuwi_pair.[ch], a pointless intermediate layer.

--- src/tool/hpcrun/unwind/common/addr_to_recipe_map.c
+++ src/tool/hpcrun/unwind/common/addr_to_recipe_map.c
@@ -16,7 +16,7 @@
 //******************************************************************************
 
 #include "addr_to_recipe_map.h"
-#include "cskiplist_ilmstat_btuwi_pair.h"
+#include <lib/prof-lean/cskiplist.h>
 
 //******************************************************************************
 // macro
@@ -35,7 +35,8 @@ a2r_map_init(mem_alloc m_alloc)
   printf("DXN_DBG a2r_map_init: call cskl_ilmstat_btuwi_init(SKIPLIST_HEIGHT, m_alloc)... \n");
 #endif
 
-  cskl_ilmstat_btuwi_init(SKIPLIST_HEIGHT, m_alloc);
+  cskl_init();
+  ilmstat_btuwi_pair_init();
 }
 
 
@@ -43,8 +44,14 @@ a2r_map_init(mem_alloc m_alloc)
 addr_to_recipe_map_t*
 a2r_map_new(mem_alloc m_alloc)
 {
-  cskl_ilmstat_btuwi_t *map = cskl_ilmstat_btuwi_new(SKIPLIST_HEIGHT, m_alloc);
-  return (addr_to_recipe_map_t *)map;
+  ilmstat_btuwi_pair_t* lsentinel =
+	  ilmstat_btuwi_pair_build(0, 0, NULL, NEVER, NULL, m_alloc );
+  ilmstat_btuwi_pair_t* rsentinel =
+	  ilmstat_btuwi_pair_build(UINTPTR_MAX, UINTPTR_MAX, NULL, NEVER, NULL, m_alloc );
+  cskiplist_t *cskl =
+	  cskl_new( lsentinel, rsentinel, SKIPLIST_HEIGHT,
+		  ilmstat_btuwi_pair_cmp, ilmstat_btuwi_pair_inrange, m_alloc);
+  return (addr_to_recipe_map_t *)cskl;
 }
 
 /*
@@ -54,14 +61,14 @@ a2r_map_new(mem_alloc m_alloc)
 ilmstat_btuwi_pair_t*
 a2r_map_inrange_find(addr_to_recipe_map_t *map, uintptr_t addr)
 {
-  return cskl_ilmstat_btuwi_inrange_find((cskl_ilmstat_btuwi_t*) map, addr);
+  return (ilmstat_btuwi_pair_t*)cskl_inrange_find((cskiplist_t*)map, (void*)addr);
 }
 
 bool
 a2r_map_insert(addr_to_recipe_map_t *map, ilmstat_btuwi_pair_t* itp,
 	mem_alloc m_alloc)
 {
-  return cskl_ilmstat_btuwi_insert((cskl_ilmstat_btuwi_t*) map, itp, m_alloc);
+  return cskl_insert((cskiplist_t*) map, itp, m_alloc);
 }
 
 
@@ -85,13 +92,27 @@ a2r_map_get_recipe(addr_to_recipe_map_t *map, uintptr_t addr )
   return ilmstat_btuwi_pair_recipe(itp, addr);
 }
 
+static void
+cskl_ilmstat_btuwi_free(void *anode)
+{
+#if CSKL_ILS_BTU
+//  printf("DXN_DBG cskl_ilmstat_btuwi_free(%p)...\n", anode);
+#endif
+
+  csklnode_t *node = (csklnode_t*) anode;
+  ilmstat_btuwi_pair_t *ilmstat_btuwi = (ilmstat_btuwi_pair_t*)node->val;
+  ilmstat_btuwi_pair_free(ilmstat_btuwi);
+  node->val = NULL;
+  cskl_free(node);
+}
+
 bool
 a2r_map_cmp_del_bulk_unsynch(
 	addr_to_recipe_map_t *map,
 	ilmstat_btuwi_pair_t* lo,
 	ilmstat_btuwi_pair_t* hi)
 {
-  return cskl_ilmstat_btuwi_cmp_del_bulk_unsynch((cskl_ilmstat_btuwi_t*) map, lo, hi);
+  return cskl_cmp_del_bulk_unsynch((cskiplist_t*) map, lo, hi, cskl_ilmstat_btuwi_free);
 }
 
 bool
@@ -100,22 +121,50 @@ a2r_map_inrange_del_bulk_unsynch(
 	uintptr_t lo,
 	uintptr_t hi)
 {
-  return cskl_ilmstat_btuwi_inrange_del_bulk_unsynch((cskl_ilmstat_btuwi_t*) map, lo, hi);
+  return cskl_inrange_del_bulk_unsynch((cskiplist_t*) map, (void*)lo, (void*)hi, cskl_ilmstat_btuwi_free);
 }
 
 /*
  * Compute a string representation of map and store result in str.
  */
+/*
+ * pre-condition: *nodeval is an ilmstat_btuwi_pair_t.
+ */
+static void
+cskl_ilmstat_btuwi_node_tostr(void* nodeval, int node_height, int max_height,
+	char str[], int max_cskl_str_len)
+{
+  cskl_levels_tostr(node_height, max_height, str, max_cskl_str_len);
+
+  // build needed indentation to print the binary tree inside the skiplist:
+  char cskl_itpair_treeIndent[MAX_CSKIPLIST_STR];
+  cskl_itpair_treeIndent[0] = '\0';
+  int indentlen= strlen(cskl_itpair_treeIndent);
+  strncat(cskl_itpair_treeIndent, str, MAX_CSKIPLIST_STR - indentlen -1);
+  indentlen= strlen(cskl_itpair_treeIndent);
+  strncat(cskl_itpair_treeIndent, ildmod_stat_maxspaces(), MAX_CSKIPLIST_STR - indentlen -1);
+
+  // print the binary tree with the proper indentation:
+  char itpairstr[max_ilmstat_btuwi_pair_len()];
+  ilmstat_btuwi_pair_t* node_val = (ilmstat_btuwi_pair_t*)nodeval;
+  ilmstat_btuwi_pair_tostr_indent(node_val, cskl_itpair_treeIndent, itpairstr);
+
+  // add new line:
+  cskl_append_node_str(itpairstr, str, max_cskl_str_len);
+}
+
 void
 a2r_map_tostring(addr_to_recipe_map_t *map, char str[])
 {
-  cskl_ilmstat_btuwi_tostring((cskl_ilmstat_btuwi_t*) map, str);
+  cskl_tostr((cskiplist_t*) map, cskl_ilmstat_btuwi_node_tostr, str, MAX_CSKIPLIST_STR);
 }
 
 
 void
 a2r_map_print(addr_to_recipe_map_t *map)
 {
-  cskl_ilmstat_btuwi_print((cskl_ilmstat_btuwi_t*)map);
+  char buf[MAX_CSKIPLIST_STR];
+  cskl_tostr((cskiplist_t*) map, cskl_ilmstat_btuwi_node_tostr, buf, MAX_CSKIPLIST_STR);
+  printf("%s", buf);
 }
 
