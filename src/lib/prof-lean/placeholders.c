e7660ef4c8114f4aac9d9744151dff560080e0d2
John Mellor-Crummey <johnmc@rice.edu>
2017-06-28 11:48:31 -0500

    fixing placeholder support for PPC64/BE

--- src/lib/prof-lean/placeholders.c
+++ src/lib/prof-lean/placeholders.c
@@ -1,7 +1,9 @@
+#include <hpctoolkit-config.h>
+
 void *
 canonicalize_placeholder(void *routine)
 {
-#if (HOST_LITTLE_ENDIAN == 0) & HOST_CPU_PPC
+#if defined(HOST_BIG_ENDIAN) && defined(HOST_CPU_PPC)
     // with the PPC 64-bit ABI on big-endian systems, functions are represented by D symbols and require one level of indirection
     void *routine_addr = *(void**) routine;
 #else
