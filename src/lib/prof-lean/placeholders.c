4b8d3f3e1ec61034b3f3a77393be070ee3f6ea79
John Mellor-Crummey <johnmc@rice.edu>
2017-06-28 09:52:46 -0500

    treat placeholder addresses differently on PPC64/BE than PPC64/LE. placeholders are D symbols on PPC64/BE and T symbols on PPC64/LE. D symbols require an extra level of indirection.

--- src/lib/prof-lean/placeholders.c
+++ src/lib/prof-lean/placeholders.c
@@ -1,8 +1,8 @@
 void *
 canonicalize_placeholder(void *routine)
 {
-#ifdef __PPC64__
-    // with the PPC 64-bit ABI, functions are represented by D symbols and require one level of indirection
+#if (HOST_LITTLE_ENDIAN == 0) & HOST_CPU_PPC
+    // with the PPC 64-bit ABI on big-endian systems, functions are represented by D symbols and require one level of indirection
     void *routine_addr = *(void**) routine;
 #else
     void *routine_addr = (void *) routine;
