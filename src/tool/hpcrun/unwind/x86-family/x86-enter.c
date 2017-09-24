HPCToolkit/hpctoolkit

commit 16606a70476db418e30f1d534bf42c2be399f653

Separate x86 registers into their own struct, and pass a register set to new_ui instead of passing individual register values.

src/tool/hpcrun/unwind/x86-family/x86-enter.c
@@ -79,11 +79,14 @@ process_enter(xed_decoded_inst_t *xptr, const xed_inst_t *xi, interval_arg_t *ia
     }
   }
   TMSG(INTV,"new interval from ENTER");
-  next = new_ui(nextInsn(iarg, xptr),
-		RA_STD_FRAME,
-		UWI_RECIPE(iarg->current)->sp_ra_pos + offset, 8, BP_SAVED,
-		UWI_RECIPE(iarg->current)->sp_bp_pos + offset - 8, 0, iarg->current,
-		m_alloc);
+  x86registers_t reg = UWI_RECIPE(iarg->current)->reg;
+  reg.sp_ra_pos += offset;
+  reg.sp_bp_pos = 8;
+  reg.bp_status = BP_SAVED;
+  reg.sp_bp_pos += offset - 8;
+  reg.bp_bp_pos = 0;
+  
+  next = new_ui(nextInsn(iarg, xptr), RA_STD_FRAME, &reg, iarg->current, m_alloc);
   hw_tmp->uwi = next;
   hw_tmp->state = 
     HW_NEW_STATE(hw_tmp->state, HW_BP_SAVED | 

src/tool/hpcrun/unwind/x86-family/x86-move.c
@@ -63,6 +63,8 @@ process_move(xed_decoded_inst_t *xptr, const xed_inst_t *xi, interval_arg_t *iar
 
   xed_operand_enum_t   op0_name = xed_operand_name(op0);
   xed_operand_enum_t   op1_name = xed_operand_name(op1);
+  x86recipe_t *xr = UWI_RECIPE(iarg->current);
+  x86registers_t reg = xr->reg;
     
   if ((op0_name == XED_OPERAND_MEM0) && (op1_name == XED_OPERAND_REG0)) { 
     //------------------------------------------------------------------------
@@ -79,7 +81,7 @@ process_move(xed_decoded_inst_t *xptr, const xed_inst_t *xi, interval_arg_t *iar
 	//--------------------------------------------------------------------
 	// register being stored is BP (or a copy in RAX)
 	//--------------------------------------------------------------------
-	if (UWI_RECIPE(iarg->current)->bp_status == BP_UNCHANGED) {
+	if (reg.bp_status == BP_UNCHANGED) {
 	  //==================================================================
 	  // instruction: save caller's BP into the stack  
 	  // action:      create a new interval with 
@@ -87,13 +89,9 @@ process_move(xed_decoded_inst_t *xptr, const xed_inst_t *xi, interval_arg_t *iar
 	  //                (2) BP position relative to the stack pointer set 
 	  //                    to the offset from SP 
 	  //==================================================================
-	  next = new_ui(nextInsn(iarg, xptr),
-			UWI_RECIPE(iarg->current)->ra_status, UWI_RECIPE(iarg->current)->sp_ra_pos, 
-			UWI_RECIPE(iarg->current)->bp_ra_pos,
-			BP_SAVED,
-			xed_decoded_inst_get_memory_displacement(xptr, 0),
-			UWI_RECIPE(iarg->current)->bp_bp_pos,
-			iarg->current, m_alloc);
+	  reg.bp_status = BP_SAVED;
+	  reg.bp_ra_pos = xed_decoded_inst_get_memory_displacement(xptr, 0);
+	  next = new_ui(nextInsn(iarg, xptr), xr->ra_status, &reg, iarg->current, m_alloc);
 	  hw_tmp->uwi = next;
 	  hw_tmp->state = 
 	    HW_NEW_STATE(hw_tmp->state, HW_BP_SAVED);
@@ -109,19 +107,17 @@ process_move(xed_decoded_inst_t *xptr, const xed_inst_t *xi, interval_arg_t *iar
       //--------------------------------------------------------------------
       // register being loaded is BP
       //--------------------------------------------------------------------
-      if (UWI_RECIPE(iarg->current)->bp_status != BP_UNCHANGED) {
+      if (reg.bp_status != BP_UNCHANGED) {
 	int64_t offset = xed_decoded_inst_get_memory_displacement(xptr, 0);
 	xed_reg_enum_t basereg = xed_decoded_inst_get_base_reg(xptr, 0); 
-	if (x86_isReg_SP(basereg) && (offset == UWI_RECIPE(iarg->current)->sp_bp_pos)) { 
+	if (x86_isReg_SP(basereg) && (offset == reg.sp_bp_pos)) { 
 	  //================================================================
 	  // instruction: restore BP from its saved location in the stack  
 	  // action:      create a new interval with BP status reset to 
 	  //              BP_UNCHANGED
 	  //================================================================
-	  next = new_ui(nextInsn(iarg, xptr),
-			RA_SP_RELATIVE, UWI_RECIPE(iarg->current)->sp_ra_pos, 
-			UWI_RECIPE(iarg->current)->bp_ra_pos, BP_UNCHANGED, UWI_RECIPE(iarg->current)->sp_bp_pos, 
-			UWI_RECIPE(iarg->current)->bp_bp_pos, iarg->current, m_alloc);
+	  reg.bp_status = BP_UNCHANGED;
+	  next = new_ui(nextInsn(iarg, xptr), RA_SP_RELATIVE, &reg, iarg->current, m_alloc);
 	} else {
 	  //================================================================
 	  // instruction: BP is loaded from a memory address DIFFERENT from 
@@ -129,14 +125,12 @@ process_move(xed_decoded_inst_t *xptr, const xed_inst_t *xi, interval_arg_t *iar
 	  // action:      create a new interval with BP status reset to 
 	  //              BP_HOSED
 	  //================================================================
-	  if (UWI_RECIPE(iarg->current)->bp_status != BP_HOSED) {
-	    next = new_ui(nextInsn(iarg, xptr),
-			  RA_SP_RELATIVE, UWI_RECIPE(iarg->current)->sp_ra_pos, 
-			  UWI_RECIPE(iarg->current)->bp_ra_pos, BP_HOSED, UWI_RECIPE(iarg->current)->sp_bp_pos,
-			  UWI_RECIPE(iarg->current)->bp_bp_pos, iarg->current, m_alloc);
+	  if (reg.bp_status != BP_HOSED) {
+	    reg.bp_status = BP_HOSED;
+	    next = new_ui(nextInsn(iarg, xptr), RA_SP_RELATIVE, &reg, iarg->current, m_alloc);
 	    if (HW_TEST_STATE(hw_tmp->state, HW_BP_SAVED, 
 			      HW_BP_OVERWRITTEN) && 
-		(UWI_RECIPE(hw_tmp->uwi)->sp_ra_pos == UWI_RECIPE(next)->sp_ra_pos)) {
+		(UWI_RECIPE(hw_tmp->uwi)->reg.sp_ra_pos == reg.sp_ra_pos)) {
 	      hw_tmp->uwi = next;
 	      hw_tmp->state = 
 		HW_NEW_STATE(hw_tmp->state, HW_BP_OVERWRITTEN);
@@ -155,10 +149,9 @@ process_move(xed_decoded_inst_t *xptr, const xed_inst_t *xi, interval_arg_t *iar
 	// action:      create a new interval with SP status reset to 
 	//              BP_UNCHANGED
 	//================================================================
-	next = new_ui(nextInsn(iarg, xptr),
-	 	      RA_SP_RELATIVE, 0, 0,
-		      UWI_RECIPE(iarg->current)->bp_status, UWI_RECIPE(iarg->current)->sp_bp_pos, 
-		      UWI_RECIPE(iarg->current)->bp_bp_pos, iarg->current, m_alloc);
+	reg.sp_ra_pos = 0;
+	reg.sp_bp_pos = 0;
+	next = new_ui(nextInsn(iarg, xptr), RA_SP_RELATIVE, &reg, iarg->current, m_alloc);
       }
     }
   } else if ((op0_name == XED_OPERAND_REG0) && (op1_name == XED_OPERAND_REG1)){
@@ -172,20 +165,14 @@ process_move(xed_decoded_inst_t *xptr, const xed_inst_t *xi, interval_arg_t *iar
       // instruction: restore SP from BP
       // action:      begin a new SP_RELATIVE interval 
       //====================================================================
-      next = new_ui(nextInsn(iarg, xptr),
-		    RA_SP_RELATIVE, UWI_RECIPE(iarg->current)->bp_ra_pos, UWI_RECIPE(iarg->current)->bp_ra_pos,
-		    UWI_RECIPE(iarg->current)->bp_status, UWI_RECIPE(iarg->current)->bp_bp_pos, UWI_RECIPE(iarg->current)->bp_bp_pos,
-		    iarg->current, m_alloc);
+      next = new_ui(nextInsn(iarg, xptr), RA_SP_RELATIVE, &reg, iarg->current, m_alloc);
     } else if (x86_isReg_BP(reg0) && x86_isReg_SP(reg1)) {
       //====================================================================
       // instruction: initialize BP with value of SP to set up a frame ptr
       // action:      begin a new SP_RELATIVE interval 
       //====================================================================
-      next = new_ui(nextInsn(iarg, xptr), 
-		    RA_STD_FRAME,
-		    UWI_RECIPE(iarg->current)->sp_ra_pos, UWI_RECIPE(iarg->current)->sp_ra_pos, BP_SAVED,
-		    UWI_RECIPE(iarg->current)->sp_bp_pos, UWI_RECIPE(iarg->current)->sp_bp_pos, iarg->current,
-			m_alloc);
+      reg.bp_status = BP_SAVED;
+      next = new_ui(nextInsn(iarg, xptr), RA_STD_FRAME, &reg, iarg->current, m_alloc);
       if (HW_TEST_STATE(hw_tmp->state, HW_BP_SAVED, 
 			HW_BP_OVERWRITTEN)) { 
 	hw_tmp->uwi = next;
@@ -198,20 +185,17 @@ process_move(xed_decoded_inst_t *xptr, const xed_inst_t *xi, interval_arg_t *iar
       //====================================================================
       iarg->rax_rbp_equivalent_at = nextInsn(iarg, xptr);
     } else if (x86_isReg_BP(reg0)) {
-      if (UWI_RECIPE(iarg->current)->bp_status != BP_HOSED){
+      if (reg.bp_status != BP_HOSED){
 	//==================================================================
 	// instruction: move some NON-special register to BP
 	// state:       bp_status is NOT BP_HOSED
 	// action:      begin a new RA_SP_RELATIVE,BP_HOSED interval
 	//==================================================================
-	next = new_ui(nextInsn(iarg, xptr), 
-		      RA_SP_RELATIVE,
-		      UWI_RECIPE(iarg->current)->sp_ra_pos, UWI_RECIPE(iarg->current)->sp_ra_pos, BP_HOSED,
-		      UWI_RECIPE(iarg->current)->sp_bp_pos, UWI_RECIPE(iarg->current)->sp_bp_pos, iarg->current,
-			  m_alloc);
+	reg.bp_status = BP_HOSED;
+	next = new_ui(nextInsn(iarg, xptr), RA_SP_RELATIVE, &reg, iarg->current, m_alloc);
 	if (HW_TEST_STATE(hw_tmp->state, HW_BP_SAVED, 
 			  HW_BP_OVERWRITTEN) && 
-	    (UWI_RECIPE(hw_tmp->uwi)->sp_ra_pos == UWI_RECIPE(next)->sp_ra_pos)) {
+	    (UWI_RECIPE(hw_tmp->uwi)->reg.sp_ra_pos == reg.sp_ra_pos)) {
 	  hw_tmp->uwi = next;
 	  hw_tmp->state = 
 	    HW_NEW_STATE(hw_tmp->state, HW_BP_OVERWRITTEN);

src/tool/hpcrun/unwind/x86-family/x86-addsub.c
@@ -66,6 +66,9 @@ process_addsub(xed_decoded_inst_t *xptr, const xed_inst_t *xi, interval_arg_t *i
   const xed_operand_t* op0 = xed_inst_operand(xi,0);
   const xed_operand_t* op1 = xed_inst_operand(xi,1);
   xed_operand_enum_t   op0_name = xed_operand_name(op0);
+  x86recipe_t *xr = UWI_RECIPE(iarg->current);
+  x86registers_t reg = xr->reg;
+  ra_loc istatus = xr->ra_status;
 
   if (op0_name == XED_OPERAND_REG0) {
     xed_reg_enum_t reg0 = xed_decoded_inst_get_reg(xptr, op0_name);
@@ -77,7 +80,6 @@ process_addsub(xed_decoded_inst_t *xptr, const xed_inst_t *xi, interval_arg_t *i
       if (xed_operand_name(op1) == XED_OPERAND_IMM0) {
 	int sign = (iclass_eq(xptr, XED_ICLASS_ADD)) ? -1 : 1;
 	long immedv = sign * xed_decoded_inst_get_signed_immediate(xptr);
-	ra_loc istatus = UWI_RECIPE(iarg->current)->ra_status;
 	if ((istatus == RA_STD_FRAME) && (immedv > 0) &&
 	    (hw_tmp->state & HW_SP_DECREMENTED)) {
 	  //-------------------------------------------------------------------
@@ -92,10 +94,9 @@ process_addsub(xed_decoded_inst_t *xptr, const xed_inst_t *xi, interval_arg_t *i
 	  // 9 December 2007 -- John Mellor-Crummey
 	  //-------------------------------------------------------------------
 	}
-	next = new_ui(nextInsn(iarg, xptr), 
-		      istatus, UWI_RECIPE(iarg->current)->sp_ra_pos + immedv, UWI_RECIPE(iarg->current)->bp_ra_pos, 
-		      UWI_RECIPE(iarg->current)->bp_status, UWI_RECIPE(iarg->current)->sp_bp_pos + immedv, 
-		      UWI_RECIPE(iarg->current)->bp_bp_pos, iarg->current, m_alloc);
+	reg->sp_ra_pos += immedv;
+	reg->sp_bp_pos += immedv;
+	next = new_ui(nextInsn(iarg, xptr), istatus, &reg, iarg->current, m_alloc);
 
 	if (immedv > 0) {
 	  if (HW_TEST_STATE(hw_tmp->state, 0, HW_SP_DECREMENTED)) {
@@ -113,25 +114,20 @@ process_addsub(xed_decoded_inst_t *xptr, const xed_inst_t *xi, interval_arg_t *i
 	    // 9 December 2007 -- John Mellor-Crummey
 	    //-----------------------------------------------------------------
 	    hw_tmp->uwi = next;
-	    hw_tmp->succ_inst_ptr = 
-	      nextInsn(iarg, xptr);
-	    hw_tmp->state = 
-	      HW_NEW_STATE(hw_tmp->state, HW_SP_DECREMENTED);
+	    hw_tmp->succ_inst_ptr = nextInsn(iarg, xptr);
+	    hw_tmp->state = HW_NEW_STATE(hw_tmp->state, HW_SP_DECREMENTED);
 	    iarg->canonical_interval = next;
 	  }
 	}
       } else {
-	if (UWI_RECIPE(iarg->current)->ra_status != RA_BP_FRAME){
+	if (istatus != RA_BP_FRAME){
 	  //-------------------------------------------------------------------
 	  // no immediate in add/subtract from stack pointer; switch to
 	  // BP_FRAME
 	  //
 	  // 9 December 2007 -- John Mellor-Crummey
 	  //-------------------------------------------------------------------
-	  next = new_ui(nextInsn(iarg, xptr), RA_BP_FRAME, 
-			UWI_RECIPE(iarg->current)->sp_ra_pos, UWI_RECIPE(iarg->current)->bp_ra_pos, 
-			UWI_RECIPE(iarg->current)->bp_status, UWI_RECIPE(iarg->current)->sp_bp_pos, 
-			UWI_RECIPE(iarg->current)->bp_bp_pos, iarg->current, m_alloc);
+	  next = new_ui(nextInsn(iarg, xptr), RA_BP_FRAME, &reg, iarg->current, m_alloc);
 	  iarg->bp_frames_found = true;
 	}
       }

src/tool/hpcrun/unwind/x86-family/x86-and.c
@@ -65,14 +65,12 @@ process_and(xed_decoded_inst_t *xptr, const xed_inst_t *xi, interval_arg_t *iarg
 
   if (op0_name == XED_OPERAND_REG0) {
 	xed_reg_enum_t reg0 = xed_decoded_inst_get_reg(xptr, op0_name);
-	if (x86_isReg_SP(reg0) && UWI_RECIPE(iarg->current)->bp_status != BP_UNCHANGED) {
+	x86recipe_t *xr = UWI_RECIPE(iarg->current);
+	if (x86_isReg_SP(reg0) && xr->reg.bp_status != BP_UNCHANGED) {
 	  //-----------------------------------------------------------------------
 	  // we are adjusting the stack pointer via 'and' instruction
 	  //-----------------------------------------------------------------------
-	  next = new_ui(nextInsn(iarg, xptr),
-		  RA_BP_FRAME, UWI_RECIPE(iarg->current)->sp_ra_pos, UWI_RECIPE(iarg->current)->bp_ra_pos,
-		  UWI_RECIPE(iarg->current)->bp_status, UWI_RECIPE(iarg->current)->sp_bp_pos,
-		  UWI_RECIPE(iarg->current)->bp_bp_pos, iarg->current, m_alloc);
+	  next = new_ui(nextInsn(iarg, xptr), RA_BP_FRAME, &xr->reg, iarg->current, m_alloc);
 	}
   }
   return next;

src/tool/hpcrun/unwind/x86-family/x86-build-intervals.c
@@ -125,7 +125,8 @@ x86_build_intervals(void *ins, unsigned int len, int noisy, mem_alloc m_alloc)
   iarg.end           = end;
   iarg.highwatermark = _h;
   iarg.ins           = ins;
-  iarg.current       = new_ui(ins, RA_SP_RELATIVE, 0, 0, BP_UNCHANGED, 0, 0, NULL, m_alloc);
+  x86registers_t reg = {0, 0, BP_UNCHANGED, 0, 0};
+  iarg.current       = new_ui(ins, RA_SP_RELATIVE, &reg, NULL, m_alloc);
   iarg.first         = iarg.current;
 
   // handle return is different if there are any bp frames

src/tool/hpcrun/unwind/x86-family/x86-call.c
@@ -97,10 +97,11 @@ process_call(xed_decoded_inst_t *xptr, const xed_inst_t *xi, interval_arg_t *iar
   // As if it were a push
   //
   if (call_is_push_next_addr_idiom(xptr, iarg)) {
-    next = new_ui(nextInsn(iarg, xptr), UWI_RECIPE(iarg->current)->ra_status,
-		  UWI_RECIPE(iarg->current)->sp_ra_pos + sizeof(void*), UWI_RECIPE(iarg->current)->bp_ra_pos, 
-                  UWI_RECIPE(iarg->current)->bp_status, UWI_RECIPE(iarg->current)->sp_bp_pos + sizeof(void*), 
-                  UWI_RECIPE(iarg->current)->bp_bp_pos, iarg->current, m_alloc);
+    x86recipe_t *xr = UWI_RECIPE(iarg->current);
+    x86registers reg = xr->reg;
+    reg.sp_ra_pos += sizeof(void*);
+    reg.sp_bp_pos += sizeof(void*);
+    next = new_ui(nextInsn(iarg, xptr), xr->ra_status, &reg, iarg->current, m_alloc);
   }
 #ifdef USE_CALL_LOOKAHEAD
   next = call_lookahead(xptr, iarg->current, iarg->ins);

src/tool/hpcrun/unwind/x86-family/x86-canonical.c
@@ -104,24 +104,25 @@ reset_to_canonical_interval(xed_decoded_inst_t *xptr, unwind_interval **next,
       iarg->canonical_interval = first;
     }
     {
-      ra_loc ra_status = UWI_RECIPE(first)->ra_status;
+      x86recipe_t *xr = UWI_RECIPE(current);
+      x86recipe_t *r1 = UWI_RECIPE(first);
+      x86registers_t reg = r1->reg;
+      ra_loc ra_status = r1->ra_status;
       bp_loc bp_status =
-    	  (UWI_RECIPE(current)->bp_status == BP_HOSED) ? BP_HOSED : UWI_RECIPE(first)->bp_status;
+    	  (xr->reg.bp_status == BP_HOSED) ? BP_HOSED : reg.bp_status;
 #ifndef FIX_INTERVALS_AT_RETURN
-      if ((UWI_RECIPE(current)->ra_status != ra_status) ||
-	  (UWI_RECIPE(current)->bp_status != bp_status) ||
-	  (UWI_RECIPE(current)->sp_ra_pos != UWI_RECIPE(first)->sp_ra_pos) ||
-	  (UWI_RECIPE(current)->bp_ra_pos != UWI_RECIPE(first)->bp_ra_pos) ||
-	  (UWI_RECIPE(current)->bp_bp_pos != UWI_RECIPE(first)->bp_bp_pos) ||
-	  (UWI_RECIPE(current)->sp_bp_pos != UWI_RECIPE(first)->sp_bp_pos))
+      if (xr->ra_status != ra_status) ||
+	  xr->reg.bp_status != bp_status) ||
+	  xr->reg.sp_ra_pos != reg.sp_ra_pos) ||
+	  xr->reg.bp_ra_pos != reg.bp_ra_pos) ||
+	  xr->reg.bp_bp_pos != reg.bp_bp_pos) ||
+	  xr->reg.sp_bp_pos != reg.sp_bp_pos))
 #endif
 	{
-	*next = new_ui(nextInsn(iarg, xptr),
-		       ra_status, UWI_RECIPE(first)->sp_ra_pos, UWI_RECIPE(first)->bp_ra_pos,
-		       bp_status, UWI_RECIPE(first)->sp_bp_pos, UWI_RECIPE(first)->bp_bp_pos,
-		       current, m_alloc);
+	  reg.bp_status = bp_status;
+	*next = new_ui(nextInsn(iarg, xptr), ra_status, &reg, current, m_alloc);
         set_ui_restored_canonical(*next, UWI_RECIPE(iarg->canonical_interval)->prev_canonical);
-        if (UWI_RECIPE(first)->bp_status != BP_HOSED && bp_status == BP_HOSED) {
+        if (r1->reg.bp_status != BP_HOSED && bp_status == BP_HOSED) {
           set_ui_canonical(*next, iarg->canonical_interval);
           iarg->canonical_interval = *next;
         }

src/tool/hpcrun/unwind/x86-family/x86-lea.c
@@ -62,6 +62,7 @@ process_lea(xed_decoded_inst_t *xptr, const xed_inst_t *xi, interval_arg_t *iarg
 
   if ((op0_name == XED_OPERAND_REG0)) { 
     x86recipe_t *xr = UWI_RECIPE(next);
+    x86registers_t reg = xr->reg;
     xed_reg_enum_t regname = xed_decoded_inst_get_reg(xptr, op0_name);
     char *next_ins = nextInsn(iarg, xptr);
     if (x86_isReg_BP(regname)) {
@@ -69,15 +70,8 @@ process_lea(xed_decoded_inst_t *xptr, const xed_inst_t *xi, interval_arg_t *iarg
       // action: clobbering the base pointer; begin a new SP_RELATIVE interval
       // note: we don't check that BP is BP_SAVED; we might have to
       //=======================================================================
-      next = new_ui(next_ins,
-		    RA_SP_RELATIVE, 
-		    xr->sp_ra_pos, 
-		    xr->bp_ra_pos,
-		    BP_HOSED, 
-		    xr->sp_bp_pos, 
-		    xr->bp_bp_pos,
-		    next,
-		    m_alloc);
+      reg.bp_status = BP_HOSED;
+      next = new_ui(next_ins, RA_SP_RELATIVE, &reg, next, m_alloc);
       if (HW_TEST_STATE(hw_tmp->state, HW_BP_SAVED, HW_BP_OVERWRITTEN) &&
 	  (UWI_RECIPE(hw_tmp->uwi)->sp_ra_pos == xr->sp_ra_pos)) {
 	hw_tmp->uwi = next;
@@ -95,15 +89,8 @@ process_lea(xed_decoded_inst_t *xptr, const xed_inst_t *xi, interval_arg_t *iarg
 	    // begin a new interval where sp_ra_pos is adjusted by the displacement.        
 	    //========================================================================
 	    xed_int64_t disp = xed_decoded_inst_get_memory_displacement(xptr, mem_op_index);
-	    next = new_ui(next_ins,
-			  xr->ra_status, 
-			  xr->sp_ra_pos - disp, 
-			  xr->bp_ra_pos,
-			  xr->bp_status,
-			  xr->sp_bp_pos, 
-			  xr->bp_bp_pos,
-			  next,
-			  m_alloc);
+	    reg.ra_pos -= disp;
+	    next = new_ui(next_ins, xr->ra_status, &reg, next, m_alloc);
 
 	    if (disp < 0) {
 	      if (HW_TEST_STATE(hw_tmp->state, 0, HW_SP_DECREMENTED)) {

commit 85e569e7df610fbb652a2ad9b372d9356827ed2d

Fix x86 unwinding bugs introduced in commit 16606a7047...

src/tool/hpcrun/unwind/x86-family/x86-enter.c
@@ -81,7 +81,7 @@ process_enter(xed_decoded_inst_t *xptr, const xed_inst_t *xi, interval_arg_t *ia
   TMSG(INTV,"new interval from ENTER");
   x86registers_t reg = UWI_RECIPE(iarg->current)->reg;
   reg.sp_ra_pos += offset;
-  reg.sp_bp_pos = 8;
+  reg.bp_ra_pos = 8;
   reg.bp_status = BP_SAVED;
   reg.sp_bp_pos += offset - 8;
   reg.bp_bp_pos = 0;

src/tool/hpcrun/unwind/x86-family/x86-move.c
@@ -103,7 +103,7 @@ process_move(xed_decoded_inst_t *xptr, const xed_inst_t *xi, interval_arg_t *iar
 	  //                    to the offset from SP 
 	  //==================================================================
 	  reg.bp_status = BP_SAVED;
-	  reg.bp_ra_pos = xed_decoded_inst_get_memory_displacement(xptr, 0);
+	  reg.sp_bp_pos = xed_decoded_inst_get_memory_displacement(xptr, 0);
 	  next = new_ui(nextInsn(iarg, xptr), xr->ra_status, &reg, m_alloc);
 	  hw_tmp->uwi = next;
 	  hw_tmp->state = 
@@ -143,7 +143,7 @@ process_move(xed_decoded_inst_t *xptr, const xed_inst_t *xi, interval_arg_t *iar
 	    next = new_ui(nextInsn(iarg, xptr), RA_SP_RELATIVE, &reg, m_alloc);
 	    if (HW_TEST_STATE(hw_tmp->state, HW_BP_SAVED, 
 			      HW_BP_OVERWRITTEN) && 
-		(UWI_RECIPE(hw_tmp->uwi)->reg.sp_ra_pos == reg.sp_ra_pos)) {
+		(UWI_RECIPE(hw_tmp->uwi)->reg.sp_ra_pos == UWI_RECIPE(next)->reg.sp_ra_pos)) {
 	      hw_tmp->uwi = next;
 	      hw_tmp->state = 
 		HW_NEW_STATE(hw_tmp->state, HW_BP_OVERWRITTEN);
@@ -163,7 +163,7 @@ process_move(xed_decoded_inst_t *xptr, const xed_inst_t *xi, interval_arg_t *iar
 	//              BP_UNCHANGED
 	//================================================================
 	reg.sp_ra_pos = 0;
-	reg.sp_bp_pos = 0;
+	reg.bp_ra_pos = 0;
 	next = new_ui(nextInsn(iarg, xptr), RA_SP_RELATIVE, &reg, m_alloc);
       }
     }
@@ -178,6 +178,8 @@ process_move(xed_decoded_inst_t *xptr, const xed_inst_t *xi, interval_arg_t *iar
       // instruction: restore SP from BP
       // action:      begin a new SP_RELATIVE interval 
       //====================================================================
+      reg.sp_ra_pos = reg.bp_ra_pos;
+      reg.sp_bp_pos = reg.bp_bp_pos;
       next = new_ui(nextInsn(iarg, xptr), RA_SP_RELATIVE, &reg, m_alloc);
     } else if (x86_isReg_BP(reg0) && x86_isReg_SP(reg1)) {
       //====================================================================
@@ -185,6 +187,8 @@ process_move(xed_decoded_inst_t *xptr, const xed_inst_t *xi, interval_arg_t *iar
       // action:      begin a new interval 
       //====================================================================
       reg.bp_status = BP_SAVED;
+      reg.bp_ra_pos = reg.sp_ra_pos;
+      reg.bp_bp_pos = reg.sp_bp_pos;
       next = new_ui(nextInsn(iarg, xptr), RA_STD_FRAME, &reg, m_alloc);
       if (iarg->sp_realigned) {
         // SP was previously realigned. correct RA offsets based on typical 
@@ -220,10 +224,12 @@ process_move(xed_decoded_inst_t *xptr, const xed_inst_t *xi, interval_arg_t *iar
 	// action:      begin a new RA_SP_RELATIVE,BP_HOSED interval
 	//==================================================================
 	reg.bp_status = BP_HOSED;
+	reg.bp_ra_pos = reg.sp_ra_pos;
+	reg.bp_bp_pos = reg.sp_bp_pos;
 	next = new_ui(nextInsn(iarg, xptr), RA_SP_RELATIVE, &reg, m_alloc);
 	if (HW_TEST_STATE(hw_tmp->state, HW_BP_SAVED, 
 			  HW_BP_OVERWRITTEN) && 
-	    (UWI_RECIPE(hw_tmp->uwi)->reg.sp_ra_pos == reg.sp_ra_pos)) {
+	    (UWI_RECIPE(hw_tmp->uwi)->reg.sp_ra_pos == UWI_RECIPE(next)->reg.sp_ra_pos)) {
 	  hw_tmp->uwi = next;
 	  hw_tmp->state = 
 	    HW_NEW_STATE(hw_tmp->state, HW_BP_OVERWRITTEN);