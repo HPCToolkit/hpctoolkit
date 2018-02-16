edc0248f5a81d77f3d0bd65b0f06600af41519ef
John Mellor-Crummey <johnmc@rice.edu>
2018-02-15 18:07:38 -0600

    adjust line spacing for clarity

--- src/include/linux_info.h
+++ src/include/linux_info.h
@@ -2,11 +2,12 @@
 
 #define LINUX_KERNEL_NAME_REAL          "vmlinux"
 #define LINUX_KERNEL_NAME               "<"  LINUX_KERNEL_NAME_REAL  ">"
+
 #define LINUX_KERNEL_SYMBOL_FILE_SHORT  "kallsyms"
+
 #define LINUX_KERNEL_SYMBOL_FILE        "/proc/" LINUX_KERNEL_SYMBOL_FILE_SHORT
 #define LINUX_PERF_EVENTS_FILE          "/proc/sys/kernel/perf_event_paranoid"
 #define LINUX_PERF_EVENTS_MAX_RATE      "/proc/sys/kernel/perf_event_max_sample_rate"
-
 #define LINUX_KERNEL_KPTR_RESTICT       "/proc/sys/kernel/kptr_restrict"
 
 // measurement subdirectory where kallsyms files from compute nodes will be recorded
