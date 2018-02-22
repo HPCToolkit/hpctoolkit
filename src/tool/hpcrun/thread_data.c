0779e9055d51b9732c0b3b5a725e449a51a66ea5
Keren Zhou <robinho364@gmail.com>
2018-02-22 11:15:22 -0600

    Recover scale_fn field initialization

--- src/tool/hpcrun/thread_data.c
+++ src/tool/hpcrun/thread_data.c
@@ -246,6 +246,11 @@ core_profile_trace_data_init(core_profile_trace_data_t * cptd, int id, cct_ctxt_
   // perf event support
   // ----------------------------------------
   cptd->perf_event_info   = NULL;
+
+  // ----------------------------------------
+  // openmp usage
+  // ----------------------------------------
+  cptd->scale_fn = NULL;
 }
 
 #ifdef ENABLE_CUDA
