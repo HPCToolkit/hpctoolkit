1e00572e8d58af037f56e0e998b83f10b6a114e8
John Mellor-Crummey <johnmc@rice.edu>
2018-03-05 14:11:02 +0000

    set reasonable default max perf frequency
    if linux does not provide a max perf frequency, choose a reasonable value.

--- src/tool/hpcrun/sample-sources/perf/perf-util.c
+++ src/tool/hpcrun/sample-sources/perf/perf-util.c
@@ -324,7 +324,7 @@ int
 perf_util_get_max_sample_rate()
 {
   static int initialized = 0;
-  static int max_sample_rate  = -1;
+  static int max_sample_rate  = 300; // unless otherwise limited
   if (!initialized) {
     FILE *perf_rate_file = fopen(LINUX_PERF_EVENTS_MAX_RATE, "r");
 
