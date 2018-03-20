9a32e57ef9a76bdd0ef78c69892095d800e31a32
laiwei-rice <lai.wei@rice.edu>
2018-03-20 22:23:10 +0000

    Bug fix on trampoline. Writing messages to logs in trampoline can lead to execution errors.

--- src/tool/hpcrun/messages/debug-flag.c
+++ src/tool/hpcrun/messages/debug-flag.c
@@ -109,7 +109,7 @@ static pmsg_category all_list_entries [] = {
  E(_TST_CTL),
  E(UNW),
  // E(UW_RECIPE_MAP),
- E(TRAMP),
+ // E(TRAMP), // Writing messages to logs in trampoline can lead to execution errors.
  E(RETCNT_CTL),
  E(SEGV),
  E(MPI),
