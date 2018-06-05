66ae8b48228fa7f757013159a3d413fedec5b143
Doug Moore <dougm@rice.edu>
2018-06-05 17:06:21 -0500

    Avoid having a single metric accumulator associated with two different values.  Previously, the field identifying the second accumulator for one expression could be the same as the field for the first accumulator for the next expression.

--- src/tool/hpcprof-mpi/main.cpp
+++ src/tool/hpcprof-mpi/main.cpp
@@ -574,7 +574,7 @@ makeSummaryMetrics(Prof::CallPath::Profile& profGbl,
   //    since the 'combine' function will be used during the metric
   //    reduction.  Metric inputs point to accumulators [mXDrvdBeg,
   //    mXDrvdEnd) rather input values.
-  for (uint i = mDrvdBeg, j = mXDrvdBeg; i < mDrvdEnd; ++i, ++j) {
+  for (uint i = mDrvdBeg, j = mXDrvdBeg; i < mDrvdEnd; ++i) {
     Prof::Metric::ADesc* m = mMgrGbl.metric(i);
     Prof::Metric::DerivedIncrDesc* mm =
       dynamic_cast<Prof::Metric::DerivedIncrDesc*>(m);
@@ -582,9 +582,9 @@ makeSummaryMetrics(Prof::CallPath::Profile& profGbl,
 
     Prof::Metric::AExprIncr* expr = mm->expr();
     if (expr) {
-      expr->srcId(j);
+      expr->srcId(j++);
       if (expr->hasAccum2()) {
-	expr->src2Id(j + 1); // cf. Metric::Mgr::makeSummaryMetricIncr()
+	expr->src2Id(j++); // cf. Metric::Mgr::makeSummaryMetricIncr()
       }
     }
   }
