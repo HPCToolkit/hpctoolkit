280e8e004658e48d7d934068805ccbef87d48524
Aaron Thomas Cherian <atc8@gpu.cs.rice.edu>
2020-04-03 02:26:26 -0500

    correlation id is fetched and incremented atomically. Calls to produce and consume channels are made by background thread inside callbacks

--- src/tool/hpcrun/gpu/intel/opencl-api.c
+++ src/tool/hpcrun/gpu/intel/opencl-api.c
@@ -17,21 +17,16 @@
 #include <lib/prof-lean/usec_time.h> //usec_time
 #include <hpcrun/gpu/gpu-monitoring-thread-api.h>
 #include <hpcrun/gpu/gpu-activity-channel.h> //gpu_activity_channel_consume
-
+#include <inttypes.h> //PRIu64
 #define CPU_NANOTIME() (usec_time() * 1000)
 
 void opencl_buffer_completion_notify();
 void opencl_activity_process(cl_event, void *);
 
-uint64_t correlation_id = 0;
-
-void opencl_subscriber_callback(opencl_call type, uint64_t * correlation_id_arg)
+void opencl_function_submitted(opencl_call type, uint64_t correlation_id)
 {
-  uint64_t local_c_id;
-  __atomic_load(&correlation_id, &local_c_id, __ATOMIC_SEQ_CST); //TODO
-  gpu_correlation_id_map_insert(local_c_id, local_c_id);
-  *correlation_id_arg = local_c_id;
-  cct_node_t *api_node = gpu_application_thread_correlation_callback(local_c_id);
+  gpu_correlation_id_map_insert(correlation_id, correlation_id);
+  cct_node_t *api_node = gpu_application_thread_correlation_callback(correlation_id);
   gpu_op_placeholder_flags_t gpu_op_placeholder_flags = 0;
   gpu_op_ccts_t gpu_op_ccts;
 
@@ -56,13 +51,38 @@ void opencl_subscriber_callback(opencl_call type, uint64_t * correlation_id_arg)
   gpu_activity_channel_consume(gpu_metrics_attribute);	
   uint64_t cpu_submit_time = CPU_NANOTIME();
   gpu_correlation_channel_produce(correlation_id, &gpu_op_ccts, cpu_submit_time);
-  __atomic_fetch_add(&correlation_id, 1, __ATOMIC_SEQ_CST);
+}
+
+void opencl_subscriber_callback(cl_event event, cl_int event_command_exec_status, void *user_data)
+{
+  cl_int submit_flag = CL_SUBMITTED;
+  if (event_command_exec_status == submit_flag)
+  {
+	cl_generic_callback* cb_data = (cl_generic_callback*)user_data;
+	printf("submitting type: %d, correlation: %"PRIu64 "\n", cb_data->type, cb_data->correlation_id);
+	opencl_function_submitted(cb_data->type, cb_data->correlation_id);
+  }
 }
 
 void opencl_buffer_completion_callback(cl_event event, cl_int event_command_exec_status, void *user_data)
 {
-  opencl_buffer_completion_notify();
-  opencl_activity_process(event, user_data);
+  cl_int complete_flag = CL_COMPLETE;
+  cl_generic_callback* cb_data = (cl_generic_callback*)user_data;
+  uint64_t correlation_id = cb_data->correlation_id;
+  opencl_call type = cb_data->type;
+
+  if (event_command_exec_status == complete_flag)
+  {
+	gpu_correlation_id_map_entry_t *cid_map_entry = gpu_correlation_id_map_lookup(correlation_id);
+	if (cid_map_entry == NULL)
+	{
+		printf("completion callback was called before registration callback\n");
+		opencl_function_submitted(type, correlation_id);
+	}
+	printf("completion type: %d, correlation: %"PRIu64 "\n", type, correlation_id);
+ 	opencl_buffer_completion_notify();
+	opencl_activity_process(event, user_data);
+  }
 }
 
 void opencl_buffer_completion_notify()
