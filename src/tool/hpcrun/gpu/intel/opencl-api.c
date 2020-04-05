c457a91b2a3ee1fd9e5101a6437acd19203c6765
Aaron Thomas Cherian <atc8@gpu.cs.rice.edu>
2020-04-05 01:31:37 -0500

    making gpu_activity_channel_consume() and gpu_correlation_channel_produce() calls from main thread. ISSUE#3 fixed. Removing unused headers

--- src/tool/hpcrun/gpu/intel/opencl-api.c
+++ src/tool/hpcrun/gpu/intel/opencl-api.c
@@ -1,29 +1,26 @@
-#include <hpcrun/sample_event.h> // hpcrun_sample_callpath()
-#include <hpcrun/safe-sampling.h> // enter() and exit()
-#include <ucontext.h> //ucontext , but not imported in gpu-application-thread-api.c
-#include <lib/prof-lean/hpcrun-fmt.h> // hpcrun_metricVal_t
-#include <hpcrun/gpu/gpu-activity.h> // gpu_activity_t, GPU_ACTIVITY_KERNEL, gpu_memcpy_t, gpu_kernel_t, gpu_memcpy_type_t
+#include <hpcrun/gpu/gpu-activity.h> // gpu_activity_t 
 #include <hpcrun/gpu/gpu-activity-process.h> //gpu_activity_process
-#include <string.h> //memset
-#include "opencl-api.h"
+#include <hpcrun/gpu/gpu-activity-channel.h> //gpu_activity_channel_consume
 #include <hpcrun/gpu/gpu-correlation-channel.h> //gpu_correlation_channel_produce
 #include <hpcrun/gpu/gpu-correlation-id-map.h> //gpu_correlation_id_map_insert
-#include <hpcrun/gpu/gpu-host-correlation-map.h> //gpu_host_correlation_map_insert
 #include <hpcrun/gpu/gpu-application-thread-api.h> // gpu_application_thread_correlation_callback
-#include <hpcrun/gpu/gpu-op-placeholders.h> // gpu_op_placeholder_flags_set, gpu_placeholder_type_copy, gpu_op_placeholder_ip, gpu_op_ccts_insert
+#include <hpcrun/gpu/gpu-op-placeholders.h> // gpu_op_placeholder_flags_set, gpu_op_ccts_insert
 #include <hpcrun/gpu/gpu-metrics.h> //gpu_metrics_attribute
-#include <hpcrun/gpu/gpu-event-id-map.h> //gpu_event_id_map_insert
-#include "opencl-activity-translate.h"
+#include <hpcrun/gpu/gpu-monitoring-thread-api.h> //gpu_monitoring_thread_activities_ready
+#include <hpcrun/safe-sampling.h> //hpcrun_safe_enter, hpcrun_safe_exit 
 #include <lib/prof-lean/usec_time.h> //usec_time
-#include <hpcrun/gpu/gpu-monitoring-thread-api.h>
-#include <hpcrun/gpu/gpu-activity-channel.h> //gpu_activity_channel_consume
+
 #include <inttypes.h> //PRIu64
+
+#include "opencl-api.h"
+#include "opencl-activity-translate.h"
+
 #define CPU_NANOTIME() (usec_time() * 1000)
 
 void opencl_buffer_completion_notify();
 void opencl_activity_process(cl_event, void *);
 
-void opencl_function_submitted(opencl_call type, uint64_t correlation_id)
+void opencl_subscriber_callback(opencl_call type, uint64_t correlation_id)
 {
   gpu_correlation_id_map_insert(correlation_id, correlation_id);
   cct_node_t *api_node = gpu_application_thread_correlation_callback(correlation_id);
@@ -53,17 +50,6 @@ void opencl_function_submitted(opencl_call type, uint64_t correlation_id)
   gpu_correlation_channel_produce(correlation_id, &gpu_op_ccts, cpu_submit_time);
 }
 
-void opencl_subscriber_callback(cl_event event, cl_int event_command_exec_status, void *user_data)
-{
-  cl_int submit_flag = CL_SUBMITTED;
-  if (event_command_exec_status == submit_flag)
-  {
-	cl_generic_callback* cb_data = (cl_generic_callback*)user_data;
-	printf("submitting type: %d, correlation: %"PRIu64 "\n", cb_data->type, cb_data->correlation_id);
-	opencl_function_submitted(cb_data->type, cb_data->correlation_id);
-  }
-}
-
 void opencl_buffer_completion_callback(cl_event event, cl_int event_command_exec_status, void *user_data)
 {
   cl_int complete_flag = CL_COMPLETE;
@@ -77,7 +63,6 @@ void opencl_buffer_completion_callback(cl_event event, cl_int event_command_exec
 	if (cid_map_entry == NULL)
 	{
 		printf("completion callback was called before registration callback. type: %d, correlation: %"PRIu64 "\n", type, correlation_id);
-		//opencl_function_submitted(type, correlation_id);
 	}
 	printf("completion type: %d, correlation: %"PRIu64 "\n", type, correlation_id);
  	opencl_buffer_completion_notify();
@@ -95,5 +80,4 @@ void opencl_activity_process(cl_event event, void * user_data)
   gpu_activity_t gpu_activity;
   opencl_activity_translate(&gpu_activity, event, user_data);
   gpu_activity_process(&gpu_activity);
-  //gpu_metrics_attribute(&gpu_activity);
 }
