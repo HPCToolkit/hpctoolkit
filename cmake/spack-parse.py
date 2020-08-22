5cb379ea21d7d5c771872effb1eebbfb499e26d6
Jonathon Anderson <anderson.jonathonm@gmail.com>
2020-08-21 23:40:31 -0500

    Use the native/cross file for most Spack bits.
    The option is really just to accelerate bootstrapping (you really should always use native files, they're nice)

--- cmake/spack-parse.py
+++ cmake/spack-parse.py
@@ -9,20 +9,43 @@ import sys
 spack = subprocess.run([sys.argv[1], 'find', '--format', '{prefix}\n'] + sys.argv[2:],
                        capture_output=True, text=True)
 if spack.returncode != 0:
-  # Spack exits if there are no matching specs. So if we have errors, just blank.
-  sys.exit(0)
+  # It might be a Spack error, or it might be that there are no matching specs.
+  # So try with just the package name and see if the explicit bits are off.
+  spack = subprocess.run([sys.argv[1], 'find', '--format', '{prefix}\n', sys.argv[2]],
+                         capture_output=True, text=True)
+  if spack.returncode == 0:
+    # The package is installed, but the spec is wrong. Report this.
+    print('''
+No package matching `{0}{1}\' but {0} is installed. Suggestions:
+  1. Adjust {0}_spec to match an installed spec (see `{2} find {0}`)
+  2. Install the specific spec (`{2} install {0}{1}`)
+'''.format(sys.argv[2], sys.argv[3], sys.argv[1]), file=sys.stderr)
+  else:
+    # The package isn't even installed. Report this.
+    print('''
+Package {0} is not installed. Suggestions:
+  1. Install the requested spec (`{2} install {0}{1}`)
+  2. Set {0}_spec to \'system\' to search the system and current environment
+  3. Set the relevant *_root property to an installation for {0}.
+'''.format(sys.argv[2], sys.argv[3], sys.argv[1]), file=sys.stderr)
+
+  # No matter the error, its an error. Stop here.
+  sys.exit(1)
 
 # Rejigger the lines into something usable. Strip whitespace and remove blanks.
 roots = [x.strip() for x in spack.stdout.splitlines()]
 roots = [x for x in roots if len(x) > 0]
 
-# If there are multiple matching installations, error. If there are none, exit.
 if len(roots) > 1:
-  print("Multiple matching installations, please be more specific!",
-        file=sys.stderr)
+  # There are multiple matching packages. Report this.
+  print('''
+Multiple packages match `{0}{1}\'. Suggestions:
+  1. Adjust {0}_spec to be more specific (usually `/<hash>`, see `{2} find {0}{1}`)
+  2. Remove the conficting packages (`{2} uninstall {0}`)
+'''.format(sys.argv[2], sys.argv[3], sys.argv[1]), file=sys.stderr)
   sys.exit(1)
-if len(roots) == 0:
-  sys.exit(0)
+
+assert len(roots) > 0
 root = roots[0]
 
 # Otherwise, just print out the root we found
