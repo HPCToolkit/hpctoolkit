3368106122915b22e7a9cb286f41f53f439ebdd4
Jonathon Anderson <anderson.jonathonm@gmail.com>
2020-08-22 00:42:46 -0500

    Meson has a nice summary output

--- cmake/spack-parse.py
+++ cmake/spack-parse.py
@@ -6,12 +6,13 @@ import sys
 # TODO: Once Spack outputs `prefix` in the JSON, use that instead
 
 # Run the Spack internal command and slurp up its output
-spack = subprocess.run([sys.argv[1], 'find', '--format', '{prefix}\n'] + sys.argv[2:],
+spack = subprocess.run([sys.argv[1], 'find', '--format',
+                        '{prefix}///{name}{@version} /{hash:7}\n'] + sys.argv[2:],
                        capture_output=True, text=True)
 if spack.returncode != 0:
   # It might be a Spack error, or it might be that there are no matching specs.
   # So try with just the package name and see if the explicit bits are off.
-  spack = subprocess.run([sys.argv[1], 'find', '--format', '{prefix}\n', sys.argv[2]],
+  spack = subprocess.run([sys.argv[1], 'find', sys.argv[2]],
                          capture_output=True, text=True)
   if spack.returncode == 0:
     # The package is installed, but the spec is wrong. Report this.
@@ -32,10 +33,10 @@ Package {0} is not installed. Suggestions:
     sys.exit(0)  # "Soft" error, warn the user but don't force the issue.
 
 # Rejigger the lines into something usable. Strip whitespace and remove blanks.
-roots = [x.strip() for x in spack.stdout.splitlines()]
-roots = [x for x in roots if len(x) > 0]
+lines = [x.strip() for x in spack.stdout.splitlines()]
+lines = [x for x in lines if len(x) > 0]
 
-if len(roots) > 1:
+if len(lines) > 1:
   # There are multiple matching packages. Report this.
   print('''
 Multiple packages match `{0}{1}\'. Suggestions:
@@ -43,9 +44,9 @@ Multiple packages match `{0}{1}\'. Suggestions:
   2. Remove the conficting packages (`{2} uninstall {0}`)'''
     .format(sys.argv[2], sys.argv[3], sys.argv[1]), file=sys.stderr)
   sys.exit(1)
+assert len(lines) > 0
 
-assert len(roots) > 0
-root = roots[0]
-
-# Otherwise, just print out the root we found
-print(root)
+# Print out the data for the root that remains
+prefix, spec = lines[0].split('///')
+print(prefix)
+print(spec)
