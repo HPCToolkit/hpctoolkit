7051d379f1d15efa67a54e10b80ecce62ad2ce81
Xiaozhu Meng <mxz297@gmail.com>
2020-08-25 21:18:50 -0500

    Prototype implementation for associating GPU metrics to actual kernels

--- src/tool/hpcrun/gpu/amd/roctracer-api.c
+++ src/tool/hpcrun/gpu/amd/roctracer-api.c
@@ -42,6 +42,19 @@
 // ******************************************************* EndRiceCopyright *
 
 //******************************************************************************
+// system includes
+//******************************************************************************
+
+#include <gelf.h>
+
+#include <errno.h>     // errno
+#include <fcntl.h>     // open
+#include <sys/stat.h>  // mkdir
+#include <sys/types.h>
+#include <unistd.h>
+#include <linux/limits.h>  // PATH_MAX
+
+//******************************************************************************
 // local includes
 //******************************************************************************
 
@@ -63,7 +76,8 @@
 #include <hpcrun/sample-sources/libdl.h>
 
 #include <hpcrun/utilities/hpcrun-nanotime.h>
-
+#include <hpcrun/files.h>
+#include "lib/prof-lean/elf-helper.h"
 
 
 //******************************************************************************
@@ -95,11 +109,49 @@
   }						\
 }
 
+typedef const char* (*hip_kernel_name_fnt)(const hipFunction_t f);
+static hip_kernel_name_fnt hip_kernel_name_fn;
+
+typedef int (*hiprtc_fnt)(uint64_t, void*);
+static hiprtc_fnt hiprtc_get_code;
+static hiprtc_fnt hiprtc_get_code_size;
+
+#define DEBUG 0
+
+#include "hpcrun/gpu/gpu-print.h"
+
+//******************************************************************************
+// type declaration
+//******************************************************************************
+
+typedef struct amd_kernel_internal {
+  uint64_t opaque[3];
+  uint64_t amd_program_internal;
+} amd_kernel_internal_t;
+
+typedef struct amd_hip_function {
+  amd_kernel_internal_t * amd_kernel;
+} amd_hip_function_t;
+
+typedef struct amd_gpu_binary {
+  size_t size;
+  char* buf;
+} amd_gpu_binary_t;
+
+typedef struct amd_function_table {
+  size_t size;
+  char** names;
+  uint64_t* addrs;
+} amd_function_table_t;
 
 
 //******************************************************************************
 // local variables
 //******************************************************************************
+
+amd_gpu_binary_t binary;
+amd_function_table_t function_table;
+static uint32_t amd_gpu_module_id;
 
 //----------------------------------------------------------
 // roctracer function pointers for late binding
@@ -256,7 +308,227 @@ roctracer_kernel_data_set
 }
 #endif
 
+static void
+ensure_kernel_ip_present
+(
+ cct_node_t *kernel_ph,
+ ip_normalized_t kernel_ip
+)
+{
+  // if the phaceholder was previously inserted, it will have a child
+  // we only want to insert a child if there isn't one already. if the
+  // node contains a child already, then the gpu monitoring thread
+  // may be adding children to the splay tree of children. in that case
+  // trying to add a child here (which will turn into a lookup of the
+  // previously added child, would race with any insertions by the
+  // GPU monitoring thread.
+  //
+  // INVARIANT: avoid a race modifying the splay tree of children by
+  // not attempting to insert a child in a worker thread when a child
+  // is already present
+  if (hpcrun_cct_children(kernel_ph) == NULL) {
+    cct_node_t *kernel =
+      hpcrun_cct_insert_ip_norm(kernel_ph, kernel_ip);
+    hpcrun_cct_retain(kernel);
+  }
+}
 
+static void
+construct_amd_gpu_symbols
+(
+ Elf *elf
+)
+{
+  // Initialize elf_help_t to handle extended numbering
+  elf_helper_t eh;
+  elf_helper_initialize(elf, &eh);
+
+  // Get section name section index to find ".strtab"
+  size_t shstrndx;
+  if (elf_getshdrstrndx(elf, &shstrndx) != 0) return;
+
+  // Find .symtab and .strtab sections
+  Elf_Scn *scn = NULL;
+  Elf_Scn *symtab_scn = NULL;
+  Elf_Scn *strtab_scn = NULL;
+  while ((scn = elf_nextscn(elf, scn)) != NULL) {
+    GElf_Shdr shdr;
+    if (!gelf_getshdr(scn, &shdr)) continue;
+    if (shdr.sh_type == SHT_SYMTAB) {
+      symtab_scn = scn;
+      continue;
+    }
+    char *name = elf_strptr(elf, shstrndx , shdr.sh_name);
+    if (name == NULL) continue;
+    if (strcmp(name, ".strtab") == 0) {
+      strtab_scn = scn;
+    }
+	}
+
+  // Get total number of symbols in .symtab
+  GElf_Shdr symtab;
+  gelf_getshdr(symtab_scn, &symtab);
+
+  int nsymbols = 0;
+  if (symtab.sh_entsize > 0) { // avoid divide by 0
+    nsymbols = symtab.sh_size / symtab.sh_entsize;
+    if (nsymbols <= 0) return;
+  } else {
+    return;
+  }
+
+  Elf_Data *symtab_data = elf_getdata(symtab_scn, NULL);
+  if (symtab_data == NULL) return;
+
+  // Get total number of function symbols in .symtab
+  size_t nfuncs = 0;
+  for (int i = 0; i < nsymbols; i++) {
+    GElf_Sym sym;
+    GElf_Sym *symp = NULL;
+    int section_index;
+    symp = elf_helper_get_symbol(&eh, i, &sym, &section_index);
+    if (symp) { // symbol properly read
+      int symtype = GELF_ST_TYPE(sym.st_info);
+      if (sym.st_shndx == SHN_UNDEF) continue;
+      if (symtype != STT_FUNC) continue;
+      nfuncs++;
+    }
+  }
+
+  // Get symbol name string table
+  Elf_Data *strtab_data = elf_getdata(strtab_scn, NULL);
+  if (strtab_data == NULL) return;
+  char* symbol_name_buf = (char*)(strtab_data->d_buf);
+
+  // Initialize our function table
+  function_table.size = nfuncs;
+  function_table.names = (char**)hpcrun_malloc(sizeof(char*) * function_table.size);
+  function_table.addrs = (uint64_t*)hpcrun_malloc(sizeof(uint64_t) * function_table.size);
+  int index = 0;
+
+  // Put each function symbol into our function table
+  for (int i = 0; i < nsymbols; i++) {
+    GElf_Sym sym;
+    GElf_Sym *symp = NULL;
+    int section_index;
+    symp = elf_helper_get_symbol(&eh, i, &sym, &section_index);
+    if (symp) { // symbol properly read
+      int symtype = GELF_ST_TYPE(sym.st_info);
+      if (sym.st_shndx == SHN_UNDEF) continue;
+      if (symtype != STT_FUNC) continue;
+      function_table.names[index] = symbol_name_buf + sym.st_name;
+      function_table.addrs[index] = sym.st_value;
+      ++index;
+    }
+  }
+#if DEBUG != 0
+  printf("Dump AMD GPU functions\n");
+  for (size_t i = 0; i < function_table.size; ++i) {
+    printf("Function %s, at address %lx\n", function_table.names[i], function_table.addrs[i]);
+  }
+#endif
+}
+
+static bool
+cupti_write_cubin
+(
+ const char *file_name,
+ const void *cubin,
+ size_t cubin_size
+)
+{
+  int fd;
+  errno = 0;
+  fd = open(file_name, O_WRONLY | O_CREAT | O_EXCL, 0644);
+  if (errno == EEXIST) {
+    close(fd);
+    return true;
+  }
+  if (fd >= 0) {
+    // Success
+    if (write(fd, cubin, cubin_size) != cubin_size) {
+      close(fd);
+      return false;
+    } else {
+      close(fd);
+      return true;
+    }
+  } else {
+    // Failure to open is a fatal error.
+    hpcrun_abort("hpctoolkit: unable to open file: '%s'", file_name);
+    return false;
+  }
+}
+
+static void
+parse_amd_gpu_binary
+(
+  hipFunction_t f
+)
+{
+  amd_hip_function_t* amd_hip_function = (amd_hip_function_t*)f;
+  amd_kernel_internal_t* amd_internal = amd_hip_function->amd_kernel;
+  uint64_t amd_program = amd_internal->amd_program_internal;
+  amd_program += 0x10;
+
+  hiprtc_get_code_size(amd_program, (void*)(&(binary.size)));
+  binary.buf = hpcrun_malloc(binary.size);
+  hiprtc_get_code(amd_program, (void*)(binary.buf));
+
+  elf_version(EV_CURRENT);
+  Elf *elf = elf_memory(binary.buf, binary.size);
+  if (elf != 0) {
+	  construct_amd_gpu_symbols(elf);
+	  elf_end(elf);
+  }
+
+  // Create file name
+  char file_name[PATH_MAX];  
+  size_t used = 0;
+  used += sprintf(&file_name[used], "%s", hpcrun_files_output_directory());
+  used += sprintf(&file_name[used], "%s", "/amd/");
+  mkdir(file_name, S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
+  used += sprintf(&file_name[used], "%s", "amd_gpu_binary");
+
+  cupti_write_cubin(file_name, binary.buf, binary.size);
+
+  amd_gpu_module_id = hpcrun_loadModule_add(file_name);
+}
+
+static uintptr_t
+lookup_amd_function
+(
+  const char *kernel_name
+)
+{  
+  for (size_t i = 0; i < function_table.size; ++i) {
+    if (strcmp(kernel_name, function_table.names[i]) == 0) {
+      return (uintptr_t)(function_table.addrs[i]);
+    }
+  }
+  return 0;
+}
+
+static ip_normalized_t
+hip_function_lookup
+(
+  hipFunction_t f
+)
+{
+  // TODO: Handle multi-threaded case
+  if (binary.size == 0) {
+    parse_amd_gpu_binary(f);
+  }
+
+  const char * kernel_name = hip_kernel_name_fn(f);
+
+  ip_normalized_t nip;
+  nip.lm_id = amd_gpu_module_id;
+  nip.lm_ip = lookup_amd_function(kernel_name);
+
+  PRINT("HIP launch kernel %s, lm_ip %lx\n", kernel_name, nip.lm_ip);
+  return nip;
+}
 
 static void
 roctracer_subscriber_callback
@@ -269,6 +541,7 @@ roctracer_subscriber_callback
 {
   gpu_op_placeholder_flags_t gpu_op_placeholder_flags = 0;
   bool is_valid_op = false;
+  bool is_kernel_op = false;
   const hip_api_data_t* data = (const hip_api_data_t*)(callback_data);
 
   switch (callback_id) {
@@ -335,14 +608,14 @@ roctracer_subscriber_callback
 
   case HIP_API_ID_hipModuleLaunchKernel:
   case HIP_API_ID_hipLaunchCooperativeKernel:
-  case HIP_API_ID_hipHccModuleLaunchKernel:
+  case HIP_API_ID_hipHccModuleLaunchKernel: {
     //case HIP_API_ID_hipExtModuleLaunchKernel:
-
     gpu_op_placeholder_flags_set(&gpu_op_placeholder_flags,
 				 gpu_placeholder_type_kernel);
     is_valid_op = true;
+    is_kernel_op = true;
     break;
-
+  }
   case HIP_API_ID_hipCtxSynchronize:
   case HIP_API_ID_hipStreamSynchronize:
   case HIP_API_ID_hipDeviceSynchronize:
@@ -351,8 +624,14 @@ roctracer_subscriber_callback
 				 gpu_placeholder_type_sync);
     is_valid_op = true;
     break;
-
+  case HIP_API_ID_hipModuleGetFunction:
+    PRINT("hipModuleGetFunction %s\n", data->args.hipModuleGetFunction.kname);
+    break;
+  case HIP_API_ID_hipModuleLoad:
+    PRINT("hipMOoduleLoad %p\n", data->args.hipModuleLoad.module);
+    break;
   default:
+    PRINT("unhandled %u\n", callback_id);
     break;
   }
 
@@ -367,7 +646,15 @@ roctracer_subscriber_callback
     gpu_op_ccts_t gpu_op_ccts;
     hpcrun_safe_enter();
     gpu_op_ccts_insert(api_node, &gpu_op_ccts, gpu_op_placeholder_flags);
+    if (is_kernel_op) {
+      cct_node_t *kernel_ph = gpu_op_ccts_get(&gpu_op_ccts, gpu_placeholder_type_kernel);
+      hipFunction_t f = data->args.hipModuleLaunchKernel.f;
+      ip_normalized_t kernel_ip = hip_function_lookup(f);
+      ensure_kernel_ip_present(kernel_ph, kernel_ip);
+    }
+
     hpcrun_safe_exit();
+
 
     gpu_activity_channel_consume(gpu_metrics_attribute);
 
@@ -456,6 +743,8 @@ roctracer_bind
   // Somehow roctracter needs libkfdwrapper64.so, but does not really load it.
   // So, we load it before using any function in roctracter.
   CHK_DLOPEN(kfd, "libkfdwrapper64.so", RTLD_NOW | RTLD_GLOBAL);
+
+  CHK_DLOPEN(hip, "libamdhip64.so", RTLD_NOW | RTLD_GLOBAL);
   hpcrun_force_dlopen(false);
 
 #define ROCTRACER_BIND(fn) \
@@ -464,6 +753,24 @@ roctracer_bind
   FORALL_ROCTRACER_ROUTINES(ROCTRACER_BIND)
 
 #undef ROCTRACER_BIND
+
+  dlerror();
+  hip_kernel_name_fn = (hip_kernel_name_fnt) dlsym(hip, "hipKernelNameRef");
+  if (hip_kernel_name_fn == 0) {
+    return -1;
+  }
+
+  dlerror();
+  hiprtc_get_code = (hiprtc_fnt) dlsym(hip, "hiprtcGetCode");
+  if (hiprtc_get_code == 0) {
+    return -1;
+  }
+
+  dlerror();
+  hiprtc_get_code_size = (hiprtc_fnt) dlsym(hip, "hiprtcGetCodeSize");
+  if (hiprtc_get_code_size == 0) {
+    return -1;
+  }
 
   return 0;
 #else
