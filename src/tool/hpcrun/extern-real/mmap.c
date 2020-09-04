0384272b579a7ca354c1956b6c968557447f32bd
Mark W. Krentel <krentel@rice.edu>
2020-09-04 15:59:30 -0500

    Step one of hiding hpcrun's functions and libraries.  This much works around the deadlock with openmpi 4.x and UCX on stria at SNL.
      1. configure.ac tests if libunwind and lzma have a static (.a)
      archive compiled with -fPIC.  If yes, then proceed, else issue a
      warning and build libhpcrun.so as we previously do.
    
      2. Copy libunwind.a and lzma.a into src/extern and use objcopy to
      --redefine-sym mmap() and munmap() into hpcrun_real_mmap and
      hpcrun_real_munmap.  Using a separate directory is an easy way to
      make sure that these are built before hpcrun.
    
      3. In hpcrun/extern-real, write hpcrun_real_* functions to call the
      real versions via syscall().
    
      4. Edit hpcrun/Makefile.am, hpcrun, hpclink to allow both old and
      new versions of linking.
    
    Remove the --enable-libunwind-primary option and clean up the related
    hash-defines.  Libunwind is now too intertwined in the rest of the
    code to be optional.
    
    Not yet done: localize the symbols in hpcrun, and stripping the line
    map without stripping the EH frames.

---
+++ src/tool/hpcrun/extern-real/mmap.c
@@ -0,0 +1,73 @@
+// -*-Mode: C++;-*-
+
+// * BeginRiceCopyright *****************************************************
+//
+// $HeadURL$
+// $Id$
+//
+// --------------------------------------------------------------------------
+// Part of HPCToolkit (hpctoolkit.org)
+//
+// Information about sources of support for research and development of
+// HPCToolkit is at 'hpctoolkit.org' and in 'README.Acknowledgments'.
+// --------------------------------------------------------------------------
+//
+// Copyright ((c)) 2002-2020, Rice University
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are
+// met:
+//
+// * Redistributions of source code must retain the above copyright
+//   notice, this list of conditions and the following disclaimer.
+//
+// * Redistributions in binary form must reproduce the above copyright
+//   notice, this list of conditions and the following disclaimer in the
+//   documentation and/or other materials provided with the distribution.
+//
+// * Neither the name of Rice University (RICE) nor the names of its
+//   contributors may be used to endorse or promote products derived from
+//   this software without specific prior written permission.
+//
+// This software is provided by RICE and contributors "as is" and any
+// express or implied warranties, including, but not limited to, the
+// implied warranties of merchantability and fitness for a particular
+// purpose are disclaimed. In no event shall RICE or contributors be
+// liable for any direct, indirect, incidental, special, exemplary, or
+// consequential damages (including, but not limited to, procurement of
+// substitute goods or services; loss of use, data, or profits; or
+// business interruption) however caused and on any theory of liability,
+// whether in contract, strict liability, or tort (including negligence
+// or otherwise) arising in any way out of the use of this software, even
+// if advised of the possibility of such damage.
+//
+// ******************************************************* EndRiceCopyright *
+
+// Redefine function calls from external libraries (libunwind) to
+// better hide them from UCX.
+//
+// Call the real functions via syscall().
+
+//----------------------------------------------------------------------
+
+#define _GNU_SOURCE  1
+
+#include <sys/types.h>
+#include <sys/syscall.h>
+#include <errno.h>
+#include <unistd.h>
+
+void *
+hpcrun_real_mmap(void *addr, size_t len, int prot, int flags, int fd, off_t offset)
+{
+  return (void *)
+    syscall((long) SYS_mmap, addr, len, prot, flags, fd, offset);
+}
+
+int
+hpcrun_real_munmap(void *addr, size_t len)
+{
+  return (int)
+    syscall((long) SYS_munmap, addr, len);
+}
