5d41e1d5ce357d0f07cdea5bfbb87f372ed245d4
Jonathon Anderson <anderson.jonathonm@gmail.com>
2021-01-06 15:39:30 -0600

    Give an error message when the script errors

--- src/extern/redefine.sh
+++ src/extern/redefine.sh
@@ -11,8 +11,8 @@ oc_args="$oc_args --redefine-sym munmap=hpcrun_real_munmap"
 oc_args="$oc_args --redefine-sym dl_iterate_phdr=hpcrun_real_dl_iterate_phdr"
 
 for lib in "$(PKG_CONFIG_PATH="$pc_dir" "$pc" --libs "$pc_name")"; do
-  test -n "$1" || exit 1
-  test -f "$lib" || exit 1
+  if [ -n "$1" ]; then echo "missing output filename: $lib" >&2; exit 1; fi
+  if [ -f "$lib" ]; then echo "file does not exist: $lib" >&2; exit 1; fi
   "$objcopy" $oc_args "$lib" "$1" || exit $?
   shift
 done
