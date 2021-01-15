15492fd63514d2c67a0ccfba46ee973a2d415c86
Jonathon Anderson <anderson.jonathonm@gmail.com>
2021-01-15 09:39:43 -0600

    Fixup some bits to work inside Spack... mostly

--- src/extern/redefine.sh
+++ src/extern/redefine.sh
@@ -4,15 +4,15 @@ objcopy="$1"
 pc="$2"
 pc_dir="$3"
 pc_name="$4"
-shift ; shift ; shift ; shift
+shift 4
 
 oc_args="--redefine-sym mmap=hpcrun_real_mmap"
 oc_args="$oc_args --redefine-sym munmap=hpcrun_real_munmap"
 oc_args="$oc_args --redefine-sym dl_iterate_phdr=hpcrun_real_dl_iterate_phdr"
 
 for lib in "$(PKG_CONFIG_PATH="$pc_dir" "$pc" --libs "$pc_name")"; do
-  if [ -n "$1" ]; then echo "missing output filename: $lib" >&2; exit 1; fi
-  if [ -f "$lib" ]; then echo "file does not exist: $lib" >&2; exit 1; fi
+  if [ -z "$1" ]; then echo "missing output filename: $lib" >&2; exit 1; fi
+  if [ ! -f "$lib" ]; then echo "file does not exist: $lib" >&2; exit 1; fi
   "$objcopy" $oc_args "$lib" "$1" || exit $?
   shift
 done
