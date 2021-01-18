2a5399467952f3e0e79105fea0602fd7a7914ce5
Jonathon Anderson <anderson.jonathonm@gmail.com>
2021-01-17 23:45:24 -0600

    Force the shell to properly parse the pkg-config output as shellcode.

--- src/extern/redefine.sh
+++ src/extern/redefine.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 objcopy="$1"
 pc="$2"
@@ -10,9 +10,11 @@ oc_args="--redefine-sym mmap=hpcrun_real_mmap"
 oc_args="$oc_args --redefine-sym munmap=hpcrun_real_munmap"
 oc_args="$oc_args --redefine-sym dl_iterate_phdr=hpcrun_real_dl_iterate_phdr"
 
-for lib in "$(PKG_CONFIG_PATH="$pc_dir" "$pc" --libs "$pc_name")"; do
-  if [ -z "$1" ]; then echo "missing output filename: $lib" >&2; exit 1; fi
-  if [ ! -f "$lib" ]; then echo "file does not exist: $lib" >&2; exit 1; fi
+parseLibs() { pc_libs=("$@"); }
+parseLibs $(PKG_CONFIG_PATH="$pc_dir" "$pc" --libs "$pc_name")
+
+for lib in "${pc_libs[@]}"; do
+  if [ -z "$1" ]; then echo "missing output filename for: '$lib'" >&2; exit 1; fi
   "$objcopy" $oc_args "$lib" "$1" || exit $?
   shift
 done
