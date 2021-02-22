2614b072094fd921f8da54b490651f08ecbbcb86
Xiaozhu Meng <mxz297@gmail.com>
2021-02-22 09:29:51 -0600

    Code cleanup

--- src/tool/hpcrun/gpu/amd/roctracer-activity-translate.c
+++ src/tool/hpcrun/gpu/amd/roctracer-activity-translate.c
@@ -47,7 +47,7 @@
 
 #include "roctracer-activity-translate.h"
 
-#define DEBUG 1
+#define DEBUG 0
 
 #include <hpcrun/gpu/gpu-print.h>
 
@@ -133,12 +133,13 @@ roctracer_activity_translate
  roctracer_record_t *record
 )
 {
-//#if DEBUG
   const char * name = roctracer_op_string(record->domain, record->op, record->kind);
-//#endif
   memset(ga, 0, sizeof(gpu_activity_t));
 
   if (record->domain == ACTIVITY_DOMAIN_HIP_OPS) {
+    // TODO: I cannot find the corresponding enum in AMD toolchain
+    // to identify different types of activities.
+    // At this point, match the string name
     if (strcmp(name, "KernelExecution") == 0) {
       convert_kernel_launch(ga, record);
     } else if (strcmp(name, "CopyDeviceToDevice") == 0) {
@@ -155,82 +156,7 @@ roctracer_activity_translate
     }
   } else if (record->domain == ACTIVITY_DOMAIN_HIP_API) {
     // Ignore API tracing records
-    /*
-    switch(record->kind){
-    case HIP_API_ID_hipMemcpyDtoD:
-    case HIP_API_ID_hipMemcpyDtoDAsync:
-      convert_memcpy(ga, record, GPU_MEMCPY_D2D);
-      break;
-    case HIP_API_ID_hipMemcpy2DToArray:
-      convert_memcpy(ga, record, GPU_MEMCPY_D2D);
-      break;
-    case HIP_API_ID_hipMemcpyAtoH:
-      convert_memcpy(ga, record, GPU_MEMCPY_A2H);
-      break;
-    case HIP_API_ID_hipMemcpyHtoD:
-    case HIP_API_ID_hipMemcpyHtoDAsync:
-      convert_memcpy(ga, record, GPU_MEMCPY_H2D);
-      break;
-    case HIP_API_ID_hipMemcpyHtoA:
-      convert_memcpy(ga, record, GPU_MEMCPY_H2A);
-      break;
-    case HIP_API_ID_hipMemcpyDtoH:
-    case HIP_API_ID_hipMemcpyDtoHAsync:
-      convert_memcpy(ga, record, GPU_MEMCPY_D2H);
-      break;
-    case HIP_API_ID_hipMemcpyAsync:
-    case HIP_API_ID_hipMemcpyFromSymbol:
-    case HIP_API_ID_hipMemcpy3D:
-    case HIP_API_ID_hipMemcpy2D:
-    case HIP_API_ID_hipMemcpyPeerAsync:
-    case HIP_API_ID_hipMemcpyFromArray:
-    case HIP_API_ID_hipMemcpy2DAsync:
-    case HIP_API_ID_hipMemcpyToArray:
-    case HIP_API_ID_hipMemcpyToSymbol:
-    case HIP_API_ID_hipMemcpyPeer:
-    case HIP_API_ID_hipMemcpyParam2D:
-    case HIP_API_ID_hipMemcpy:
-    case HIP_API_ID_hipMemcpyToSymbolAsync:
-    case HIP_API_ID_hipMemcpyFromSymbolAsync:
-      convert_memcpy(ga, record, GPU_MEMCPY_UNK);
-      break;
-    case HIP_API_ID_hipModuleLaunchKernel:
-    case HIP_API_ID_hipLaunchCooperativeKernel:
-    case HIP_API_ID_hipHccModuleLaunchKernel:
-    case HIP_API_ID_hipExtModuleLaunchKernel:
-    case HIP_API_ID_hipLaunchKernel:
-      convert_kernel_launch(ga, record);
-      break;
-    case HIP_API_ID_hipCtxSynchronize:
-      convert_sync(ga, record, GPU_SYNC_UNKNOWN);
-      break;
-    case HIP_API_ID_hipStreamSynchronize:
-      convert_sync(ga, record, GPU_SYNC_STREAM);
-      break;
-    case HIP_API_ID_hipStreamWaitEvent:
-      convert_sync(ga, record, GPU_SYNC_STREAM_EVENT_WAIT);
-      break;
-    case HIP_API_ID_hipDeviceSynchronize:
-      convert_sync(ga, record, GPU_SYNC_UNKNOWN);
-      break;
-    case HIP_API_ID_hipEventSynchronize:
-      convert_sync(ga, record, GPU_SYNC_EVENT);
-      break;
-    case HIP_API_ID_hipMemset3DAsync:
-    case HIP_API_ID_hipMemset2D:
-    case HIP_API_ID_hipMemset2DAsync:
-    case HIP_API_ID_hipMemset:
-    case HIP_API_ID_hipMemsetD8:
-    case HIP_API_ID_hipMemset3D:
-    case HIP_API_ID_hipMemsetAsync:
-    case HIP_API_ID_hipMemsetD32Async:
-      convert_memset(ga, record);
-      break;
-    default:
-      convert_unknown(ga);
-      PRINT("roctracer buffer event: Unhandled HIP OPS activity %s, duration %d\n", name, record->end_ns - record->begin_ns);
-    }
-    */    
+    convert_unknown(ga);
   } else {
     convert_unknown(ga);
     PRINT("roctracer buffer enent: Unhandled activity %s, domain %u, op %u, kind %u\n", 
--- src/tool/hpcrun/gpu/gpu-trace.c
+++ src/tool/hpcrun/gpu/gpu-trace.c
@@ -223,22 +223,6 @@ gpu_trace_stream_append
                            td->prev_dLCA, time);
 }
 
-
-static void
-gpu_trace_first
-(
- thread_data_t* td,
- cct_node_t *no_activity,
- uint64_t start
-)
-{
-  if (td->gpu_trace_first_time == 0) {
-    td->gpu_trace_first_time = start - 1;
-    gpu_trace_stream_append(td, no_activity, start - 1);
-  }
-}
-
-
 static uint64_t
 gpu_trace_start_adjust
 (
