6d11a2171dea7b61ce862f7139586855a0901d4e
Jokeren <robinho364@gmail.com>
2021-03-12 17:31:58 -0600

    Dyninst 10.2.0 does not have cuda_op_general, fallback to default path that don't use entry_id

--- src/lib/banal/gpu/GPUBlock.cpp
+++ src/lib/banal/gpu/GPUBlock.cpp
@@ -20,32 +20,36 @@ Address GPUBlock::last() const {
 
 
 void GPUBlock::getInsns(Insns &insns) const {
+#ifdef DYNINST_SUPPORTS_INTEL_GPU
   entryID entry_id = _entry_ids_max_;
+  unsigned char dummy_inst[MAX_INST_SIZE];
 
   if (_arch == Arch_cuda) {
     entry_id = cuda_op_general;
-  } else {
-#ifdef DYNINST_SUPPORTS_INTEL_GPU
-    if (_arch == Arch_intelGen9) {
-      entry_id = intel_gpu_op_general;
-    }
-#endif  // DYNINST_SUPPORTS_INTEL_GPU
+  } else if (_arch == Arch_intelGen9) {
+    entry_id = intel_gpu_op_general;
   }
 
   // Don't construct CFG if Dyninst does not support this GPU arch
   if (entry_id == _entry_ids_max_) {
     return;
   }
-
-  unsigned char dummy_inst[MAX_INST_SIZE];
+#endif  // DYNINST_SUPPORTS_INTEL_GPU
 
   for (auto &inst_offset : _inst_offsets) {
+#ifdef DYNINST_SUPPORTS_INTEL_GPU
+    entryID entry_id = _entry_ids_max_;
     InstructionAPI::Operation op(entry_id, "", _arch);
 
     auto offset = inst_offset.first;
     auto size = inst_offset.second;
 
     InstructionAPI::Instruction inst(op, size, dummy_inst, _arch);
+#else
+    // Dyninst 10.2.0 does not have cuda_op_general
+    // Just use the default constructor
+    InstructionAPI::Instruction inst;    
+#endif  // DYNINST_SUPPORTS_INTEL_GPU  
     insns.emplace(offset, inst);
   }
 }
