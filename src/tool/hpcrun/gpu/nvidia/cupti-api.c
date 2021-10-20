a078f5d0aaf662e580330e3ea201f6d4ffbf92bb
blue42u <anderson.jonathonm@gmail.com>
2021-10-20 13:15:14 -0500

    Mark the entire CUPTI subscriber callback as "safe"
    When monitoring both CPU and GPU metrics, its possible that Run will non-deterministically
    hang trying (and failing) to gather CPU metrics for the CUPTI subscriber callback.
    The log file in this case contains a number of failed unwinds coming from this function.
    
    This patch wraps the entire callback in a hpcrun_safe_enter/exit pair, preventing
    samples in the region and (it seems) solving the hang.

--- src/tool/hpcrun/gpu/nvidia/cupti-api.c
+++ src/tool/hpcrun/gpu/nvidia/cupti-api.c
@@ -492,6 +492,8 @@ static ip_normalized_t cupti_func_ip_resolve(CUfunction function) {
 
 static void cupti_subscriber_callback(
     void* userdata, CUpti_CallbackDomain domain, CUpti_CallbackId cb_id, const void* cb_info) {
+  int oursafe = hpcrun_safe_enter();
+
   if (domain == CUPTI_CB_DOMAIN_RESOURCE) {
     const CUpti_ResourceData* rd = (const CUpti_ResourceData*)cb_info;
     if (cb_id == CUPTI_CBID_RESOURCE_MODULE_LOADED) {
@@ -880,6 +882,9 @@ static void cupti_subscriber_callback(
           is_kernel_op, is_valid_op, cupti_runtime_api_flag);
     }
   }
+
+  if (oursafe)
+    hpcrun_safe_exit();
 }
 
 void cupti_device_timestamp_get(CUcontext context, uint64_t* time) {
