279e50c22b60d6df0310c16ae7ff472f37236217
aarontcopal2 <aarontcopal2@gmail.com>
2021-11-21 23:26:54 -0600

    changing the papi metric collection strategy: a helper thread will be responsible for polling papi counters. ISSUE: the papi metrics are not visible

---
+++ src/tool/hpcrun/gpu/opencl/intel/papi/papi_metric_collector.h
@@ -0,0 +1,62 @@
+#ifndef PAPI_METRIC_COLLECTOR_H_
+#define PAPI_METRIC_COLLECTOR_H_
+
+//******************************************************************************
+// local includes
+//******************************************************************************
+
+#include <hpcrun/cct/cct.h>                                             // cct_node_t
+#include <hpcrun/gpu/blame-shifting/blame-kernel-map.h>                 // kernel_node_t
+
+
+
+//******************************************************************************
+// type declarations
+//******************************************************************************
+
+typedef struct cct_node_linkedlist_t {
+  _Atomic (struct cct_node_linkedlist_t*) next;
+  cct_node_t *node;
+} cct_node_linkedlist_t;
+
+
+
+//******************************************************************************
+// interface operations
+//******************************************************************************
+
+void
+hpcrun_completed
+(
+ void
+);
+
+
+void
+add_kernel_to_incomplete_list
+(
+ kernel_node_t *kernel_node
+);
+
+
+void
+remove_kernel_from_incomplete_list
+(
+ kernel_node_t *kernel_node
+);
+
+
+void
+cct_list_node_free_helper
+(
+ cct_node_linkedlist_t *node
+);
+
+
+void*
+papi_metric_callback
+(
+ void *arg
+);
+
+#endif      // PAPI_METRIC_COLLECTOR_H_
