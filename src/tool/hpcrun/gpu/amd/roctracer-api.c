2bf2f92d679b840fa28e22c9dfb8c4494712f6ef
John Mellor-Crummey <johnmc@rice.edu>
2022-01-14 12:38:12 -0600

    roctracer support for fixing finalization (#504)
    * at thread termination: ensure that rocm activities are flushed and processed
    * at process termination: perform above thread termination, also shut down rocm monitoring, repeat activity flush and processing to be safe

--- src/tool/hpcrun/gpu/amd/roctracer-api.c
+++ src/tool/hpcrun/gpu/amd/roctracer-api.c
@@ -578,6 +578,19 @@ roctracer_init
 }
 
 void
+roctracer_flush
+(
+ void* args,
+ int how
+)
+{
+  HPCRUN_ROCTRACER_CALL(roctracer_flush_activity_expl, (NULL));
+
+  gpu_application_thread_process_activities();
+}
+
+
+void
 roctracer_fini
 (
  void* args,
@@ -590,8 +603,7 @@ roctracer_fini
   HPCRUN_ROCTRACER_CALL(roctracer_disable_domain_activity, (ACTIVITY_DOMAIN_HSA_OPS));
   HPCRUN_ROCTRACER_CALL(roctracer_disable_domain_callback, (ACTIVITY_DOMAIN_KFD_API));
   HPCRUN_ROCTRACER_CALL(roctracer_disable_domain_callback, (ACTIVITY_DOMAIN_ROCTX));
-  HPCRUN_ROCTRACER_CALL(roctracer_flush_activity_expl, (NULL));
 
-  gpu_application_thread_process_activities();
+  roctracer_flush(args, how);
 }
 
