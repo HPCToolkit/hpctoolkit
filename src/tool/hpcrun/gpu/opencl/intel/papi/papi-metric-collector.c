bbdd878444c158620ed2549bfcf7e103748cc227
aarontcopal2 <aarontcopal2@gmail.com>
2022-01-29 04:37:39 -0600

    adding copyright for papi-metric-collector.* files. Removing unused variable and import from the same files

--- src/tool/hpcrun/gpu/opencl/intel/papi/papi-metric-collector.c
+++ src/tool/hpcrun/gpu/opencl/intel/papi/papi-metric-collector.c
@@ -1,3 +1,46 @@
+// -*-Mode: C++;-*- // technically C99
+
+// * BeginRiceCopyright *****************************************************
+//
+// --------------------------------------------------------------------------
+// Part of HPCToolkit (hpctoolkit.org)
+//
+// Information about sources of support for research and development of
+// HPCToolkit is at 'hpctoolkit.org' and in 'README.Acknowledgments'.
+// --------------------------------------------------------------------------
+//
+// Copyright ((c)) 2002-2022, Rice University
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are
+// met:
+//
+// * Redistributions of source code must retain the above copyright
+//   notice, this list of conditions and the following disclaimer.
+//
+// * Redistributions in binary form must reproduce the above copyright
+//   notice, this list of conditions and the following disclaimer in the
+//   documentation and/or other materials provided with the distribution.
+//
+// * Neither the name of Rice University (RICE) nor the names of its
+//   contributors may be used to endorse or promote products derived from
+//   this software without specific prior written permission.
+//
+// This software is provided by RICE and contributors "as is" and any
+// express or implied warranties, including, but not limited to, the
+// implied warranties of merchantability and fitness for a particular
+// purpose are disclaimed. In no event shall RICE or contributors be
+// liable for any direct, indirect, incidental, special, exemplary, or
+// consequential damages (including, but not limited to, procurement of
+// substitute goods or services; loss of use, data, or profits; or
+// business interruption) however caused and on any theory of liability,
+// whether in contract, strict liability, or tort (including negligence
+// or otherwise) arising in any way out of the use of this software, even
+// if advised of the possibility of such damage.
+//
+// ******************************************************* EndRiceCopyright *
+
 //******************************************************************************
 // system includes
 //******************************************************************************
@@ -15,7 +58,6 @@
 #include <hpcrun/gpu-monitors.h>                                        // gpu_monitors_apply, gpu_monitor_type_enter
 #include <hpcrun/memory/hpcrun-malloc.h>                                // hpcrun_malloc_safe
 #include <hpcrun/gpu/opencl/opencl-api.h>                               // opencl_api_thread_finalize
-#include <hpcrun/device-finalizers.h>                                   // device_finalizer_register
 
 
 
@@ -28,7 +70,6 @@ static bool hpcrun_complete = false;
 static kernel_node_t* incomplete_kernel_list_head = NULL;
 static kernel_node_t* incomplete_kernel_list_tail = NULL;
 static kernel_node_t *kernel_node_free_list = NULL;
-static device_finalizer_fn_entry_t device_finalizer_flush;
 static _Atomic(uint32_t) g_unfinished_kernels = { 0 };
 
 
