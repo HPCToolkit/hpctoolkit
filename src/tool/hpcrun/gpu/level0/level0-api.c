65bddce694be9e2cf6de57814429b110fde03e5e
Xiaozhu Meng <mxz297@gmail.com>
2022-03-23 17:48:39 +0000

    Handle fence and command queue synchronization for Level 0

--- src/tool/hpcrun/gpu/level0/level0-api.c
+++ src/tool/hpcrun/gpu/level0/level0-api.c
@@ -61,6 +61,8 @@
 #include "level0-data-node.h"
 #include "level0-binary.h"
 #include "level0-kernel-module-map.h"
+#include "level0-fence-map.h"
+#include "level0-command-queue-map.h"
 
 #include <hpcrun/main.h>
 #include <hpcrun/memory/hpcrun-malloc.h>
@@ -102,7 +104,10 @@
   macro(zeModuleCreate) \
   macro(zeModuleDestroy) \
   macro(zeKernelCreate) \
-  macro(zeKernelDestroy)
+  macro(zeKernelDestroy) \
+  macro(zeFenceDestroy) \
+  macro(zeFenceReset) \
+  macro(zeCommandQueueSynchronize)
 
 #define LEVEL0_FN_NAME(f) DYN_FN_NAME(f)
 
@@ -351,6 +356,31 @@ LEVEL0_FN
   zeKernelDestroy,
   (
     ze_kernel_handle_t hKernel      // [in][release] handle of the kernel object
+  )
+);
+
+LEVEL0_FN
+(
+  zeFenceDestroy,
+  (
+    ze_fence_handle_t hFence        // [in][release] handle of fence object to destroy
+  )
+);
+
+LEVEL0_FN
+(
+  zeFenceReset,
+  (
+    ze_fence_handle_t hFence        //  [in] handle of the fence
+  )
+);
+
+LEVEL0_FN
+(
+  zeCommandQueueSynchronize,
+  (
+    ze_command_queue_handle_t hCommandQueue,   // [in] handle of the command queue
+    uint64_t timeout                           // [in] if non-zero, then indicates the maximum time (in nanoseconds) to yield before returning
   )
 );
 
@@ -743,6 +773,43 @@ level0_process_immediate_command_list
   }
 }
 
+static void
+level0_attribute_fence
+(
+  ze_fence_handle_t hFence
+)
+{
+  if (hFence == NULL) return;
+
+  level0_fence_data_t * data = level0_fence_map_lookup(hFence);
+  if (data == NULL) return;
+
+  for (int i = 0; i < data->num; ++i) {
+    level0_command_list_reset_entry(data->list[i]);
+  }
+
+  level0_fence_map_delete(hFence);
+}
+
+static void
+level0_attribute_command_queue
+(
+  ze_command_queue_handle_t command_queue
+)
+{
+  if (command_queue == NULL) return;
+
+  level0_command_queue_data_t * data = level0_command_queue_map_lookup(command_queue);
+  if (data == NULL) return;
+
+  for (int i = 0; i < data->num; ++i) {
+    level0_command_list_reset_entry(data->list[i]);
+  }
+
+  level0_command_queue_map_delete(command_queue);
+}
+
+
 //******************************************************************************
 // interface operations
 //******************************************************************************
@@ -775,7 +842,7 @@ hpcrun_zeCommandListAppendLaunchKernel
                                                   ///< on before launching
 )
 {
-  PRINT("Enter zeCommandListAppendLaunchKernel wrapper\n");
+  PRINT("Enter zeCommandListAppendLaunchKernel wrapper: command list %p\n", hCommandList);
   // Entry action:
   // We need to create a new event for querying time stamps
   // if the user appends the kernel with an empty event parameter
@@ -806,7 +873,7 @@ hpcrun_zeCommandListAppendMemoryCopy
                                                   ///< on before launching
 )
 {
-  PRINT("Enter zeCommandListAppendMemoryCopy wrapper\n");
+  PRINT("Enter zeCommandListAppendMemoryCopy wrapper: command list %p\n", hCommandList);
   // Entry action:
   // We need to create a new event for querying time stamps
   // if the user appends the kernel with an empty event parameter
@@ -867,6 +934,7 @@ hpcrun_zeCommandListDestroy
   ze_command_list_handle_t hCommandList           ///< [in][release] handle of command list object to destroy
 )
 {
+  PRINT("Enter zeCommandListDestroy wrapper: command list %p\n", hCommandList);
   // Entry action
   level0_command_list_destroy_entry(hCommandList);
   // Execute the real level0 API
@@ -882,6 +950,7 @@ hpcrun_zeCommandListReset
   ze_command_list_handle_t hCommandList           ///< [in] handle of command list object to reset
 )
 {
+  PRINT("Enter zeCommandListReset wrapper: command list %p\n", hCommandList);
   // Entry action
   level0_command_list_reset_entry(hCommandList);
   // Execute the real level0 API
@@ -900,9 +969,12 @@ hpcrun_zeCommandQueueExecuteCommandLists
   ze_fence_handle_t hFence                        ///< [in][optional] handle of the fence to signal on completion
 )
 {
-  PRINT("Enter zeCommandQueueExecuteCommandLists wrapper\n");
+  PRINT("Enter zeCommandQueueExecuteCommandLists wrapper: command queue %p, fence %p\n",
+    hCommandQueue, hFence);
   // Entry action
   level0_command_queue_execute_command_list_entry(numCommandLists, phCommandLists);
+  level0_fence_map_insert(hFence, numCommandLists, phCommandLists);
+  level0_command_queue_map_insert(hCommandQueue, numCommandLists, phCommandLists);
   // Execute the real level0 API
   ze_result_t ret = HPCRUN_LEVEL0_CALL(zeCommandQueueExecuteCommandLists,
     (hCommandQueue, numCommandLists, phCommandLists, hFence));
@@ -1040,6 +1112,43 @@ hpcrun_zeKernelDestroy
 
   ze_result_t ret = HPCRUN_LEVEL0_CALL(zeKernelDestroy, (hKernel));
 
+  return ret;
+}
+
+ze_result_t
+hpcrun_zeFenceDestroy
+(
+  ze_fence_handle_t hFence        // [in][release] handle of fence object to destroy
+)
+{
+  PRINT("Enter zeFenceDestroy wrapper: fence %p\n", hFence);
+  level0_attribute_fence(hFence);
+  ze_result_t ret = HPCRUN_LEVEL0_CALL(zeFenceDestroy, (hFence));
+  return ret;
+}
+
+ze_result_t
+hpcrun_zeFenceReset
+(
+  ze_fence_handle_t hFence       //  [in] handle of the fence
+)
+{
+  PRINT("Enter zeFenceReset wrapper: fence %p\n", hFence);
+  level0_attribute_fence(hFence);
+  ze_result_t ret = HPCRUN_LEVEL0_CALL(zeFenceReset, (hFence));
+  return ret;
+}
+
+ze_result_t
+hpcrun_zeCommandQueueSynchronize
+(
+  ze_command_queue_handle_t hCommandQueue,   // [in] handle of the command queue
+  uint64_t timeout                           // [in] if non-zero, then indicates the maximum time (in nanoseconds) to yield before returning
+)
+{
+  PRINT("Enter zeCommandQueueSynchronize wrapper: commmand queue %p\n", hCommandQueue);
+  level0_attribute_command_queue(hCommandQueue);
+  ze_result_t ret = HPCRUN_LEVEL0_CALL(zeCommandQueueSynchronize, (hCommandQueue, timeout));
   return ret;
 }
 
