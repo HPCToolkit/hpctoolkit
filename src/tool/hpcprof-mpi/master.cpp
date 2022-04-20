d46a598215c3847e212a2423614192ea607e6178
blue42u <anderson.jonathonm@gmail.com>
2022-04-20 09:10:03 -0500

    Initial implementation of metrics.yaml support (Sink)
    Missing a bunch of pieces and probably way too complicated, but it works for now.
    Tested with `-e gpu=nvidia,pc -e REALTIME` and `-e CPUTIME`.

--- src/tool/hpcprof-mpi/master.cpp
+++ src/tool/hpcprof-mpi/master.cpp
@@ -55,6 +55,7 @@
 #include "lib/profile/sources/packed.hpp"
 #include "lib/profile/sinks/experimentxml4.hpp"
 #include "lib/profile/sinks/hpctracedb2.hpp"
+#include "lib/profile/sinks/metricsyaml.hpp"
 #include "lib/profile/sinks/sparsedb.hpp"
 #include "lib/profile/finalizers/denseids.hpp"
 #include "lib/profile/finalizers/directclassification.hpp"
@@ -172,6 +173,7 @@ int rank0(ProfArgs&& args) {
                                                       tdb.get());
     pipelineB << std::move(tdb);
     pipelineB << make_unique_x<sinks::SparseDB>(args.output);
+    pipelineB << make_unique_x<sinks::MetricsYAML>(args.output);
     break;
   }
   }
--- src/tool/hpcprof/main.cpp
+++ src/tool/hpcprof/main.cpp
@@ -52,6 +52,7 @@
 #include "lib/profile/source.hpp"
 #include "lib/profile/sinks/experimentxml4.hpp"
 #include "lib/profile/sinks/hpctracedb2.hpp"
+#include "lib/profile/sinks/metricsyaml.hpp"
 #include "lib/profile/sinks/sparsedb.hpp"
 #include "lib/profile/finalizers/denseids.hpp"
 #include "lib/profile/finalizers/directclassification.hpp"
@@ -102,6 +103,7 @@ int main(int argc, char* const argv[]) {
                                                       tdb.get());
     pipelineB << std::move(tdb);
     pipelineB << make_unique_x<sinks::SparseDB>(args.output);
+    pipelineB << make_unique_x<sinks::MetricsYAML>(args.output);
     break;
   }
   }
