567ad1fb444a5f9fdca6eaa3257f0a6f9209e241
John Mellor-Crummey <johnmc@rice.edu>
2022-06-09 00:53:08 +0000

    refine messages from hpcprof and hpcstruct
    hpcprof's default output directory is now an INFO message
    instead of a warning.
    
    hpcstruct's NOTE messages are now INFO messages

--- src/tool/hpcprof/args.cpp
+++ src/tool/hpcprof/args.cpp
@@ -418,7 +418,7 @@ ProfArgs::ProfArgs(int argc, char* const argv[])
           }
           output = input.parent_path() / fn;
         }
-        util::log::warning{} << "Output database argument not given, defaulting"
+        util::log::info{} << "Output database argument not given, defaulting"
           " to `" << output.string() << "'";
       }
       if(stdshim::filesystem::exists(output)) {
@@ -438,7 +438,7 @@ ProfArgs::ProfArgs(int argc, char* const argv[])
             ss << fbase << "-" << std::hex << std::setfill('0') << std::setw(8)
                << rand(gen);
           } while(stdshim::filesystem::exists(output / ss.str()));
-          util::log::warning{} << "Output database `" << (output/fbase).string()
+          util::log::info{} << "Output database `" << (output/fbase).string()
             << "' exists, outputting to `" << (output/ss.str()).string() << "'";
           output /= ss.str();
         } else {
