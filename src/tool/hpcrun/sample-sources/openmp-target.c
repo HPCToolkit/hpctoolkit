f4a5cf3d7a575ca8fa226d8af336b821982c8a09
John M Mellor-Crummey <johnmc@rice.edu>
2022-07-21 15:40:51 -0500

    fix enumeration and display of AMD GPU metrics
    AMD moved the location of rocprofiler's metrics.xml file.
    use configure to find it. specify the file path in a new
    autoconf substitution variable ROCM_PROFILER_METRICS.
    
    fix the display of rocprofiler metric descriptions by
    using display_event_info to wrap description text as
    needed.
    
    remove display of gpu=openmp metric option. it is enabled
    by default.

--- src/tool/hpcrun/sample-sources/openmp-target.c
+++ src/tool/hpcrun/sample-sources/openmp-target.c
@@ -168,16 +168,19 @@ METHOD_FN(gen_event_set,int lush_metrics)
 static void
 METHOD_FN(display_events)
 {
+#if 0
+  // this gets turned on automatically if there is target OMPT support
   printf("===========================================================================\n");
   printf("Available AMD GPU events\n");
   printf("===========================================================================\n");
   printf("Name\t\tDescription\n");
   printf("---------------------------------------------------------------------------\n");
-  printf("%s\t\tOperation-level monitoring of OpenMP offloading.\n"
+  printf("%s\tOperation-level monitoring of OpenMP offloading.\n"
 	 "\t\tCollect timing information on GPU kernel invocations,\n"
 	 "\t\tmemory copies, etc.\n",
 	 OPENMP_TARGET);
   printf("\n");
+#endif
 }
 
 
