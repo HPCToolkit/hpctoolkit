8253cb7f6c1f2f35327ad1aacd672556be65f5d8
Jonathon Anderson <anderson.jonathonm@gmail.com>
2023-02-24 09:27:26 -0600

    Add a test for parsing deep Context trees
    Currently the hpctoolkit.formats doesn't support deep Context trees,
    even though we need them for some tests. This commit adds a test that
    the parsing of a Context tree is not recursive and thus will work for
    very deep trees.
    
    This test fails for now.

--- tests2/lib/python/hpctoolkit/formats/v4/metadb_test.py
+++ tests2/lib/python/hpctoolkit/formats/v4/metadb_test.py
@@ -1,12 +1,26 @@
+import contextlib
 import dataclasses
+import io
+import struct
+import sys
 from pathlib import Path
 
 from hpctoolkit.test.tarball import extracted
 
 from .._test_util import assert_good_traversal, dump_to_string, testdatadir, yaml
-from .metadb import MetaDB
+from .metadb import Context, MetaDB
 
 _ = yaml
+
+
+@contextlib.contextmanager
+def recursionlimit(limit: int):
+    old_limit = sys.getrecursionlimit()
+    sys.setrecursionlimit(limit)
+    try:
+        yield
+    finally:
+        sys.setrecursionlimit(old_limit)
 
 
 def test_small_v4_0(yaml):
@@ -19,6 +33,40 @@ def test_small_v4_0(yaml):
         got = MetaDB.from_file(f)
 
     assert dataclasses.asdict(got) == dataclasses.asdict(expected)
+
+
+def test_context_deep_recursion():
+    """Test that deep Context trees will not run out of frames during the parsing."""
+    # {Ctx} structure for v4.0
+    ctx_s = struct.Struct("<QQ LBBBB H6x")
+    assert ctx_s.size == 0x20
+
+    def mkctx(*, pchild: int | None, ctx_id: int) -> bytes:
+        return ctx_s.pack(
+            ctx_s.size if pchild is not None else 0, pchild or 0, ctx_id, 0, 1, 0, 0, 0
+        )
+
+    with recursionlimit(100):
+        nctxs = sys.getrecursionlimit() + 100
+        with io.BytesIO() as bf:
+            bf.write(mkctx(pchild=None, ctx_id=0))
+            for i in range(1, nctxs):
+                bf.write(mkctx(pchild=(i - 1) * ctx_s.size, ctx_id=i))
+
+            c = Context.from_file(
+                0,
+                bf,
+                i * ctx_s.size,
+                modules_by_offset={},
+                files_by_offset={},
+                functions_by_offset={},
+            )[0]
+
+    got_ctxs = 1
+    while c.children:
+        c = c.children[0]
+        got_ctxs += 1
+    assert got_ctxs == nctxs
 
 
 def test_yaml_rt(yaml):
