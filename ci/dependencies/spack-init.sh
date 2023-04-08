66709bc51dbd84c1dcd657feb416a9b0f2759a82
Jonathon Anderson <anderson.jonathonm@gmail.com>
2023-04-08 15:35:36 -0500

    ci: Minor cleanup and simplification
    - Remove network-stats.sh. It's numbers have not been useful of late.
    - Simplify spack-init.sh. Now it only handles Spack develop and does not
      react to SPACK_CHECKOUT_VERSION.

--- ci/dependencies/spack-init.sh
+++ ci/dependencies/spack-init.sh
@@ -8,10 +8,6 @@ fi
 # By default, we use a fork of Spack that has a few PRs fused together
 spack_url=https://github.com/spack/spack.git
 spack_ref=develop
-if test -n "$1"; then
-  spack_url="$1"
-  spack_ref="$2"
-fi
 
 echo -e "\e[0Ksection_start:$(date +%s):spack_clone_init[collapsed=true]\r\e[0KSpack update and initialization"
 
@@ -22,25 +18,9 @@ if ! [ -d .spack.git ]; then
 fi
 git -C .spack.git remote set-url origin "$spack_url"
 
-if test -n "$SPACK_CHECKOUT_VERSION"; then
-  # Hotfix SPACK_CHECKOUT_VERSION, because it tends to be borked
-  echo "$SPACK_CHECKOUT_VERSION"
-  if (echo "$SPACK_CHECKOUT_VERSION" | grep -oE '[0-9a-f]{32}' > sha); then
-    SPACK_CHECKOUT_VERSION=$(cat sha)
-  fi
-  rm sha
-
-  # Update the Spack if needed, and then clone
-  if ! git -C .spack.git cat-file -e "$SPACK_CHECKOUT_VERSION"'^{commit}'; then
-    git -C .spack.git fetch --verbose origin +"$spack_ref":"$spack_ref"  || exit $?
-  fi
-  git clone --shared --branch="$spack_ref" .spack.git "$SPACK_ROOT" || exit $?
-  git -C "$SPACK_ROOT" checkout --detach "$SPACK_CHECKOUT_VERSION" || exit $?
-else
-  # Pull the latest, and then clone
-  git -C .spack.git fetch --verbose origin +"$spack_ref":"$spack_ref" || exit $?
-  git clone --shared --branch="$spack_ref" .spack.git "$SPACK_ROOT" || exit $?
-fi
+# Pull the latest, and then clone
+git -C .spack.git fetch --verbose origin +"$spack_ref":"$spack_ref" || exit $?
+git clone --shared --branch="$spack_ref" .spack.git "$SPACK_ROOT" || exit $?
 
 # Set some standard configuration values for Spack in CI
 #   Locking can be slow with fuse-overlayfs, give it a little more time than usual to sort out
