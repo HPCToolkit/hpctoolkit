23fa46a656bfd3360a343e233b5019eda3f149a2
Jonathon Anderson <anderson.jonathonm@gmail.com>
2023-09-14 02:13:17 -0500

    tests: Refactor Python out into "hpctesttool"
    Previously, all test scripts are written in Python and use a series of
    Python utilities in a side directory. This side directory depends on
    packages from the PyPI and uses Python packaging tools (Poetry). The
    "proper" way to check for dependencies and such is to install it in
    a Python virtual environment, but managing that gets overly complicated.
    
    Most tests boil down to "run `hpc* ...` and then check the output," so
    Python is only really needed for the second half of that statement.
    This commit makes the tests clearer and simpler by rewriting them into
    (short!) shell scripts that call `hpctesttool`, a new (uninstalled)
    utility that is effectively the Python halves of the tests.
    
    Tearing this apart significantly simplifies the process of managing and
    running tests. The Python scripts do not need to be pre-processed to
    rewrite the shebangs, `PYTHONPATH` does not need to be fiddled with,
    and we don't have nonstandard `HPCTOOLKIT_APP_*` variables running
    around. This also means Meson does not need to reconfigure every time
    a test script is edited, which is a significant DevX boon.
    
    I'm not a fan of using shell scripts here, but I have gone this route
    because the better alternative (Python's `subprocess`) requires more
    lines of code and is overall less familiar. The shell scripts are short
    and simple in an effort to avoid the usual issues with using shell.

---
+++ lib/python/hpctesttool/__main__.py
@@ -0,0 +1,249 @@
+import collections.abc
+import difflib
+import sys
+import tarfile
+import typing
+import xml.etree.ElementTree as XmlET
+from pathlib import Path, PurePosixPath
+
+import click
+import pyparsing as pp
+import ruamel.yaml
+
+from . import struct_canonicalize, struct_match
+from .formats import from_path, vcurrent
+from .formats.diff.strict import StrictAccuracy, StrictDiff
+from .match.context import MatchCtx, MatchEntryPoint, MatchFunction, chainmatch
+from .test.execution import Database, Measurements
+
+
+@click.group()
+def main() -> None:
+    """Front-end to all sorts of testing utilities."""
+
+
+@main.group()
+def test() -> None:
+    """Specific tests."""
+
+
+@test.command
+@click.option("-p", "--procs", type=int, default=1, help="Expected number of processes")
+@click.option(
+    "-t",
+    "--threads-per-proc",
+    type=int,
+    default=[1],
+    multiple=True,
+    help="Expected number of threads per process",
+)
+@click.option("-T", "--trace/--no-trace", help="Expect to have traces")
+@click.argument(
+    "measurements", type=click.Path(exists=True, readable=True, file_okay=False, path_type=Path)
+)
+def produces_profiles(
+    *, measurements: Path, procs: int, threads_per_proc: collections.abc.Sequence[int], trace: bool
+) -> None:
+    """Test that the given MEASUREMENTS contain a sufficient number of profiles."""
+    meas = Measurements(measurements)
+    meas.check_standard(procs=procs, threads_per_proc=threads_per_proc, traces=trace)
+
+
+@test.command
+@click.option("-T", "--trace/--no-trace", help="Expect a trace.db")
+@click.argument(
+    "database", type=click.Path(exists=True, readable=True, file_okay=False, path_type=Path)
+)
+def check_db(*, trace: bool, database: Path) -> None:
+    """Test that the given database has the required data."""
+    db = Database(database)
+    db.check_standard(tracedb=trace)
+
+
+@test.command
+@click.argument(
+    "script",
+    type=click.Path(exists=True, readable=True, dir_okay=False, path_type=Path, resolve_path=True),
+)
+@click.argument(
+    "database", type=click.Path(exists=True, readable=True, file_okay=False, path_type=Path)
+)
+def unwind_py_simple(*, script: Path, database: Path) -> None:
+    """Test that the given DATABASE contains a func_hi -> func_mid -> func_lo callstack.
+
+    Specifically, searches for a callpath of the form:
+
+    \b
+    (main_thread)
+    -call> [line] SCRIPT
+    -call> [function] func_hi
+    -lexical> [line] SCRIPT
+    -call> [function] func_mid
+    -lexical> [line] SCRIPT
+    -call> [function] func_lo
+    """  # noqa: D301
+    db = from_path(database)
+    assert isinstance(db, vcurrent.Database)
+
+    matches = list(
+        chainmatch(
+            db.meta.context,
+            MatchEntryPoint(entry_point="main_thread"),
+            MatchCtx(relation="call", lexical_type="line", file=str(script)),
+            MatchCtx(
+                relation="call",
+                lexical_type="function",
+                function=MatchFunction(name="func_hi", module="<logical python>"),
+            ),
+            MatchCtx(relation="lexical", lexical_type="line", file=str(script)),
+            MatchCtx(
+                relation="call",
+                lexical_type="function",
+                function=MatchFunction(name="func_mid", module="<logical python>"),
+            ),
+            MatchCtx(relation="lexical", lexical_type="line", file=str(script)),
+            MatchCtx(
+                relation="call",
+                lexical_type="function",
+                function=MatchFunction(name="func_lo", module="<logical python>"),
+            ),
+        )
+    )
+    if not matches:
+        raise ValueError("Unable to find match!")
+    if len(matches) > 1:
+        raise ValueError(f"Found {len(matches)} matches, expected 1!")
+
+
+@test.command
+@click.argument(
+    "database", type=click.Path(exists=True, readable=True, file_okay=False, path_type=Path)
+)
+@click.argument(
+    "canonical", type=click.Path(exists=True, readable=True, file_okay=False, path_type=Path)
+)
+def db_compare(*, database: Path, canonical: Path) -> None:
+    """Compare a DATABASE against a CANONICAL database and report any differences."""
+    diff = StrictDiff(from_path(database), from_path(canonical))
+    acc = StrictAccuracy(diff)
+    if len(diff.hunks) > 0 or acc.inaccuracy:
+        diff.render(sys.stdout)
+        acc.render(sys.stdout)
+        raise click.ClickException("Comparison failed!")
+
+
+@test.command
+@click.argument(
+    "structfile", type=click.Path(exists=True, readable=True, dir_okay=False, path_type=Path)
+)
+@click.argument(
+    "binary", type=click.Path(exists=True, dir_okay=False, path_type=Path, resolve_path=True)
+)
+@click.argument(
+    "sources", type=click.Path(exists=True, readable=True, dir_okay=False, path_type=Path), nargs=-1
+)
+@click.option(
+    "--debug/--no-debug",
+    default=True,
+    help="Disable any matching that would rely on the presence of debug info",
+)
+def match_struct(
+    *, structfile: Path, binary: Path, sources: collections.abc.Iterable[Path], debug: bool
+) -> None:
+    """Compare a STRUCTFILE against the lexical structure expressed in SOURCES."""
+    try:
+        tag = struct_match.parse_sources(sources, debug=debug, binary=str(binary))
+    except pp.ParseException as e:
+        raise click.ClickException("Pattern syntax error:\n" + e.explain()) from e
+    except pp.ParseFatalException as e:
+        raise click.ClickException("Pattern syntax error:\n" + e.explain()) from e
+
+    data = XmlET.parse(structfile)
+    found = False
+    for lm in data.findall("LM"):
+        found = True
+        msg = tag.match(lm)
+        if msg is not None:
+            raise click.ClickException(msg)
+    if not found:
+        raise click.ClickException("Missing <LM> tag?")
+
+
+@test.command
+@click.argument("structfile", type=click.File("rb"))
+@click.argument("canonical", type=click.File("rb"))
+def struct_compare(*, structfile: typing.BinaryIO, canonical: typing.BinaryIO) -> None:
+    """Compare a STRUCTFILE against a CANONICAL Structfile and report any differences."""
+    expected = struct_canonicalize.canonical_form(canonical)
+    got = struct_canonicalize.canonical_form(structfile)
+    if got != expected:
+        for line in difflib.unified_diff(expected, got):
+            click.echo(line, nl=False)
+        raise click.ClickException("Differences found between obtained and expected structure!")
+
+
+@test.command
+@click.argument(
+    "contents", type=click.Path(exists=True, file_okay=False, readable=True, path_type=Path)
+)
+@click.argument("output", type=click.File("wb"))
+def tarball(*, contents: Path, output: typing.BinaryIO) -> None:
+    """Generate a (sanitized) tarball from a directory."""
+
+    def sanitize(ti):
+        ti.uid, ti.gid = 0, 0
+        ti.uname, ti.gname = "root", "root"
+        return ti
+
+    with tarfile.open(fileobj=output, mode="w:xz") as out:
+        out.add(contents, arcname=".", recursive=True, filter=sanitize)
+
+
+@test.command
+@click.option(
+    "--add",
+    type=(click.Path(exists=True, dir_okay=False), PurePosixPath),
+    metavar="<FILE DSTPATH>",
+    multiple=True,
+    help="Add a FILE to the tarball at the DSTPATH",
+)
+@click.argument("output", type=click.File("wb"))
+def tarball_files(
+    *, add: collections.abc.Iterable[tuple[Path, PurePosixPath]], output: typing.BinaryIO
+) -> None:
+    """Create a tarball from individual files and mappings."""
+
+    def sanitize(ti):
+        ti.uid, ti.gid = 0, 0
+        ti.uname, ti.gname = "root", "root"
+        return ti
+
+    with tarfile.open(fileobj=output, mode="w:xz") as out:
+        for src, dst in add:
+            out.add(src, arcname=dst, recursive=False, filter=sanitize)
+
+
+@test.command
+@click.argument("tarball", type=click.File("rb"))
+@click.argument(
+    "output", type=click.Path(exists=False, file_okay=False, writable=True, path_type=Path)
+)
+def untarball(*, tarball: typing.BinaryIO, output: Path) -> None:
+    """Unpack a tarball with tarfile.extractall."""
+    output.mkdir(parents=True, exist_ok=True)
+    with tarfile.open(fileobj=tarball, mode="r:*") as tf:
+        tf.extractall(output, numeric_owner=True)
+
+
+@test.command
+@click.argument(
+    "database", type=click.Path(exists=True, file_okay=False, readable=True, path_type=Path)
+)
+@click.argument("output", type=click.File("w", encoding="utf-8"))
+def yaml(*, database: Path, output: typing.BinaryIO) -> None:
+    """Transcode the given database into a YAML file."""
+    ruamel.yaml.YAML(typ="rt").dump(from_path(database), output)
+
+
+if __name__ == "__main__":
+    main()
---
+++ lib/python/hpctesttool/match/context.py
@@ -0,0 +1,311 @@
+import abc
+import collections.abc
+import functools
+import types
+import typing
+
+from hpctesttool.formats.v4 import metadb
+
+
+class _MatchAny:
+    """Matches any value passed to it."""
+
+    def matches(self, _got: typing.Any) -> typing.Literal[True]:
+        return True
+
+    def __str__(self) -> str:
+        return "ANY"
+
+
+_MatchAnySingleton = _MatchAny()
+
+
+def _coercion(func):
+    @functools.wraps(func)
+    def wrapper(cls, val):
+        if isinstance(val, _MatchAny | cls):
+            return val
+        return func(cls, val)
+
+    return wrapper
+
+
+class _MatchExact:
+    """Matches an exact value."""
+
+    def __init__(self, value: typing.Any):
+        self._value = value
+
+    def matches(self, got: typing.Any) -> bool:
+        return got == self._value
+
+    def __str__(self) -> str:
+        return repr(self._value)
+
+    @classmethod
+    @_coercion
+    def coerce(cls, val: typing.Any) -> "_MatchExact":
+        return _MatchExact(val)
+
+
+def _coercion_to_exact(exact: type | types.UnionType):
+    def apply(func):
+        @functools.wraps(func)
+        def wrapper(cls, val):
+            if isinstance(val, _MatchAny | cls):
+                return val
+            if isinstance(val, exact):
+                return _MatchExact(val)
+            return func(cls, val)
+
+        return wrapper
+
+    return apply
+
+
+class _MatchStrBase(abc.ABC):
+    """Match a single string."""
+
+    @abc.abstractmethod
+    def __init__(self, expected: str):
+        pass
+
+    @abc.abstractmethod
+    def matches(self, got: str) -> bool:
+        """Test if the given string satisfies the requirements."""
+
+    @classmethod
+    @_coercion
+    def coerce(cls, expected: str) -> "_MatchStrBase":
+        return cls(expected)
+
+
+class MatchStr(_MatchStrBase):
+    """Match a single string completely."""
+
+    def __init__(self, expected: str):
+        self.expected = expected
+
+    def matches(self, got: str) -> bool:
+        return got == self.expected
+
+    def __str__(self) -> str:
+        return repr(self.expected)
+
+
+class MatchStrEndsWith(_MatchStrBase):
+    """Match if a string ends with the given string."""
+
+    def __init__(self, expectedend: str):
+        self.expectedend = expectedend
+
+    def matches(self, got: str) -> bool:
+        return got.endswith(self.expectedend)
+
+    def __str__(self) -> str:
+        return "* + " + repr(self.expectedend)
+
+
+class MatchFile:
+    """Match a single File with the specified attributes set."""
+
+    def __init__(self, *, path=_MatchAnySingleton):
+        self._submatch = {
+            "path": MatchStrEndsWith.coerce(path),
+        }
+
+    def matches(self, f: metadb.File) -> bool:
+        """Test if the given File satisfies the requirements."""
+        return all(v.matches(getattr(f, k)) for k, v in self._submatch.items())
+
+    def __str__(self) -> str:
+        return (
+            "File("
+            + ", ".join(
+                [f"{k}={m}" for k, m in self._submatch.items() if not isinstance(m, _MatchAny)]
+            )
+            + ")"
+        )
+
+    def itermatch(self, fs: metadb.SourceFiles) -> collections.abc.Iterator[metadb.File]:
+        """Iterate over all matching Files."""
+        return filter(self.matches, fs.files)
+
+    @classmethod
+    @_coercion_to_exact(metadb.File | None)
+    def coerce(cls, val) -> "MatchFile":
+        return cls(path=val)
+
+
+class MatchModule:
+    """Match a single Module with the specified attributes set."""
+
+    def __init__(self, *, path=_MatchAnySingleton):
+        self._submatch = {
+            "path": MatchStrEndsWith.coerce(path),
+        }
+
+    def matches(self, m: metadb.Module) -> bool:
+        """Test if the given Module satisfies the requirements."""
+        return all(v.matches(getattr(m, k)) for k, v in self._submatch.items())
+
+    def __str__(self) -> str:
+        return (
+            "Module("
+            + ", ".join(
+                [f"{k}={m}" for k, m in self._submatch.items() if not isinstance(m, _MatchAny)]
+            )
+            + ")"
+        )
+
+    def itermatch(self, lm: metadb.LoadModules) -> collections.abc.Iterator[metadb.Module]:
+        """Iterate over all matching Modules."""
+        return filter(self.matches, lm.modules)
+
+    @classmethod
+    @_coercion_to_exact(metadb.Module | None)
+    def coerce(cls, val) -> "MatchModule":
+        return cls(path=val)
+
+
+class MatchFunction:
+    """Match a single Function with the specified attributes set."""
+
+    def __init__(
+        self,
+        *,
+        name=_MatchAnySingleton,
+        module=_MatchAnySingleton,
+        offset: int | _MatchAny = _MatchAnySingleton,
+        file=_MatchAnySingleton,
+        line: int | _MatchAny = _MatchAnySingleton,
+    ):
+        self._submatch = {
+            "name": MatchStr.coerce(name),
+            "module": MatchModule.coerce(module),
+            "offset": _MatchExact.coerce(offset),
+            "file": MatchFile.coerce(file),
+            "line": _MatchExact.coerce(line),
+        }
+
+    def matches(self, f: metadb.Function) -> bool:
+        """Test if the given Function satisfies the requirements."""
+        return all(v.matches(getattr(f, k)) for k, v in self._submatch.items())
+
+    def __str__(self) -> str:
+        return (
+            "Function("
+            + ", ".join(
+                [f"{k}={m}" for k, m in self._submatch.items() if not isinstance(m, _MatchAny)]
+            )
+            + ")"
+        )
+
+    def itermatch(self, fs: metadb.Functions) -> collections.abc.Iterator[metadb.Function]:
+        """Iterate over all matching Functions."""
+        return filter(self.matches, fs.functions)
+
+    @classmethod
+    @_coercion_to_exact(metadb.Function | None)
+    def coerce(cls, val) -> "MatchFunction":
+        return cls(name=val)
+
+
+class MatchEntryPoint:
+    """Match any EntryPoint with the specified attributes set."""
+
+    @classmethod
+    @_coercion_to_exact(metadb.EntryPoint.EntryPoint)
+    def _coerce_entry_point(cls, val) -> _MatchExact:
+        return _MatchExact(metadb.EntryPoint.EntryPoint[val])
+
+    def __init__(self, *, entry_point=_MatchAnySingleton, pretty_name=_MatchAnySingleton):
+        self._submatch = {
+            "entry_point": self._coerce_entry_point(entry_point),
+            "pretty_name": MatchStr.coerce(pretty_name),
+        }
+
+    def matches(self, ep: metadb.EntryPoint) -> bool:
+        """Test if the given EntryPoint satisfies this matcher's requirements."""
+        return all(v.matches(getattr(ep, k)) for k, v in self._submatch.items())
+
+    def __str__(self) -> str:
+        return (
+            "EntryPoint("
+            + ", ".join(
+                [f"{k}={m}" for k, m in self._submatch.items() if not isinstance(m, _MatchAny)]
+            )
+            + ")"
+        )
+
+    def itermatch(self, ct: metadb.ContextTree) -> collections.abc.Iterator[metadb.EntryPoint]:
+        """Iterate over all EntryPoints that satisfy this matcher's requirements."""
+        return filter(self.matches, ct.entry_points)
+
+    @classmethod
+    @_coercion_to_exact(metadb.EntryPoint)
+    def coerce(cls, val) -> "MatchEntryPoint":
+        return cls(entry_point=val)
+
+
+class MatchCtx:
+    """Match a single Context with the specified attributes set."""
+
+    @classmethod
+    @_coercion_to_exact(metadb.Context.Relation)
+    def _coerce_relation(cls, val) -> _MatchExact:
+        return _MatchExact(metadb.Context.Relation[val])
+
+    @classmethod
+    @_coercion_to_exact(metadb.Context.LexicalType)
+    def _coerce_lexical_type(cls, val) -> _MatchExact:
+        return _MatchExact(metadb.Context.LexicalType[val])
+
+    def __init__(
+        self,
+        *,
+        relation=_MatchAnySingleton,
+        lexical_type=_MatchAnySingleton,
+        function=_MatchAnySingleton,
+        file=_MatchAnySingleton,
+        line: int | _MatchAny = _MatchAnySingleton,
+        module=_MatchAnySingleton,
+        offset: int | _MatchAny = _MatchAnySingleton,
+    ):
+        self._submatch = {
+            "relation": self._coerce_relation(relation),
+            "lexical_type": self._coerce_lexical_type(lexical_type),
+            "function": MatchFunction.coerce(function),
+            "file": MatchFile.coerce(file),
+            "line": _MatchExact.coerce(line),
+            "module": MatchModule.coerce(module),
+            "offset": _MatchExact.coerce(offset),
+        }
+
+    def matches(self, c: metadb.Context) -> bool:
+        """Test if the given Context satisfies the requirements."""
+        return all(v.matches(getattr(c, k)) for k, v in self._submatch.items())
+
+    def __str__(self) -> str:
+        return (
+            "Context("
+            + ", ".join(
+                [f"{k}={m}" for k, m in self._submatch.items() if not isinstance(m, _MatchAny)]
+            )
+            + ")"
+        )
+
+    def itermatch(
+        self, c: metadb.Context | metadb.EntryPoint
+    ) -> collections.abc.Iterator[metadb.Context]:
+        """Iterate over all child Contexts that satisfy the requirements."""
+        return filter(self.matches, c.children)
+
+
+def chainmatch(val, matcher, *later_matches) -> collections.abc.Iterator:
+    """Chain together a number of matchers."""
+    for got in matcher.itermatch(val):
+        if len(later_matches) > 0:
+            yield from chainmatch(got, *later_matches)
+        else:
+            yield got
