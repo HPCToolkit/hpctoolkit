d924ba97ae0f8b3d3b1fe2fe21e8626a3f9bb931
Jonathon Anderson <anderson.jonathonm@gmail.com>
2023-10-30 04:02:31 -0500

    hpcrun: Hide symbols using visibility
    The current hpcrun link uses `objcopy` to try and limit the symbols that
    `libhpcrun.so` exposes. This is nonstandard and just plain inconvenient,
    so reimplement using GCC-style function visibility attributes instead.
    
    All code is now compiled with `-fvisiblity=hidden`, which means
    function symbols will not be exposed after the link unless they are
    explicitly marked with `__attribute__((visibility("default")))`. This
    puts the documentation for what symbols are exposed *on the functions
    themselves* instead of in a confusing nonstandard fragment of Autogoo.
    This also means the `libhpcrun.so` in the build directory is the
    "completed" version that gets installed, there is no final link during
    the install.
    
    As it happens the only symbols that are exposed now are foils, or
    internal symbols used for the private namespace. This is entirely
    intentional and comes out rather convenient.

--- src/tool/hpcrun/unwind/common/libunwind-pvt.h
+++ src/tool/hpcrun/unwind/common/libunwind-pvt.h
@@ -51,22 +51,31 @@
 
 void libunwind_bind();
 
+__attribute__((visibility("default")))
 int libunwind_getcontext(unw_context_t*);
 
+__attribute__((visibility("default")))
 int libunwind_init_local(unw_cursor_t*, unw_context_t*);
 
+__attribute__((visibility("default")))
 int libunwind_init_local2(unw_cursor_t*, unw_context_t*, int);
 
+__attribute__((visibility("default")))
 int libunwind_step(unw_cursor_t*);
 
+__attribute__((visibility("default")))
 int libunwind_reg_states_iterate(unw_cursor_t*, unw_reg_states_callback, void*);
 
+__attribute__((visibility("default")))
 int libunwind_apply_reg_state(unw_cursor_t*, void*);
 
+__attribute__((visibility("default")))
 int libunwind_get_reg(unw_cursor_t*, int, unw_word_t*);
 
+__attribute__((visibility("default")))
 int libunwind_set_reg(unw_cursor_t*, int, unw_word_t);
 
+__attribute__((visibility("default")))
 int libunwind_get_save_loc(unw_cursor_t*, int, unw_save_loc_t*);
 
 #endif  // LIBUNWIND_PVT_H
