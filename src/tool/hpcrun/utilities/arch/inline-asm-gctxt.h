2c0a2ddcdd575c4c8df08f12a4c7c4dfd2936fa0
Jonathon Anderson <anderson.jonathonm@gmail.com>
2023-11-10 13:20:25 -0600

    Use the preprocessor to select arch-specific headers
    Previously any arch-specific headers were "selected" by adding include
    directories to the build. Now that is no longer being done, so this
    commit replaces the few headers that dispatch-#include like this with
    an #ifdef block to select the specific header to #include.

--- src/tool/hpcrun/utilities/arch/inline-asm-gctxt.h
+++ src/tool/hpcrun/utilities/arch/inline-asm-gctxt.h
@@ -1,7 +1,18 @@
 #ifndef INLINE_ASM_GCTXT_H
 #define INLINE_ASM_GCTXT_H
 
-#include "specific-inline-asm-gctxt.h" // DO NOT chg to <> include syntax:
-                                       // uses -I cpp arg to select architecture
+#include "hpctoolkit-config.h"
+
+#if defined(HOST_CPU_x86) || defined(HOST_CPU_x86_64)
+#include "x86-family/specific-inline-asm-gctxt.h"
+#elif defined(HOST_CPU_PPC)
+#include "ppc64/specific-inline-asm-gctxt.h"
+#elif defined(HOST_CPU_IA64)
+#include "ia64/specific-inline-asm-gctxt.h"
+#elif defined(HOST_CPU_ARM64)
+#include "aarch64/specific-inline-asm-gctxt.h"
+#else
+#error No valid HOST_CPU_* defined
+#endif
 
 #endif // INLINE_ASM_GCTXT_H
