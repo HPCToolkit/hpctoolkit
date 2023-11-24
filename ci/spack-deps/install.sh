44b86a1b2e9f710a05b229f62d1449d603aaf1e1
Jonathon Anderson <anderson.jonathonm@gmail.com>
2023-11-24 14:06:58 -0600

    Use OS-packaged build tools in CI
    Most of the build tools we use to build HPCToolkit don't *need* to be
    installed via Spack, and there isn't much difference depending on which
    version is used. For example, the Autotools and Python.
    
    Instead of installing these tools via Spack, install them using the
    packages distributed by the OS.

--- ci/spack-deps/install.sh
+++ ci/spack-deps/install.sh
@@ -13,9 +13,8 @@ make -j "$(nproc)" -C "$here" --output-sync=recurse SPACK_INSTALL_FLAGS=--no-che
 spack -D "$here" gc -y
 
 # Install Jinja2 in a temporary venv
-py="$(spack -D "$here" find --format='{prefix}' python)"/bin/python
-"$py" -m venv /tmp/venv
-/tmp/venv/bin/python -m pip install 'Jinja2>=3.1,<4'
+"$(spack python --path)" -m venv /tmp/venv
+/tmp/venv/bin/python -m pip install 'Jinja2>=3.0,<4'
 
 # Generate a spack_env.ini Meson native file for the installed packages
 /tmp/venv/bin/python "$here"/j2-spack.py "$here"/spack_env.ini.j2 "$here"/spack_env.ini
