04e20444afd9553d6f32bec7ff8abb645661b24a
Jonathon Anderson <anderson.jonathonm@gmail.com>
2024-01-18 01:12:30 -0600

    hpcrun: Improve foil dlsym routine
    Instead of just calling `dlsym(RTLD_NEXT)`, also search all objects in
    the loadmap if that fails. Completely ripped off from
    https://github.com/HPCToolkit/libmonitor/pull/3, see there for details.
    
    This does not fix any known issues, but brings the foil interceptions
    to the same interception power as libmonitor interceptions.

--- src/tool/hpcrun/foil/foil.h
+++ src/tool/hpcrun/foil/foil.h
@@ -47,10 +47,18 @@
 #ifndef FOIL_FOIL_H
 #define FOIL_FOIL_H
 
+#ifndef _GNU_SOURCE
+#define _GNU_SOURCE
+#endif
+
 #include <assert.h>
 #include <stdatomic.h>
 #include <stdint.h>
 #include <dlfcn.h>
+#include <link.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
 
 /// Macro marker for (foil) functions that need to be exposed symbols.
 #define HPCRUN_EXPOSED __attribute__((visibility("default"))) extern
@@ -81,12 +89,75 @@ static inline void* hpcrun_foil_base_lookup_cached(hpcrun_pfn_cache_t* cache, co
   static hpcrun_pfn_cache_t _cache_foilbase_ ## VAR; \
   typeof(foilbase_ ## NAME)* VAR = hpcrun_foil_base_lookup_cached(&_cache_foilbase_ ## VAR, #NAME);
 
+struct callback_data {
+    const char* symbol;
+    bool skip;
+    void *skip_until_base;
+    void *result;
+};
+static int phdr_callback(struct dl_phdr_info *info, size_t sz, void *data_v) {
+  struct callback_data *data = data_v;
+
+  // Decide whether to skip this object as being before libmonitor
+  if (data->skip) {
+    if (data->skip_until_base == (void*)info->dlpi_addr) {
+      data->skip = false;
+    }
+    return 0;
+  }
+
+  // Call dlopen to stabilize a handle for our use
+  void *this = dlopen(info->dlpi_name, RTLD_LAZY | RTLD_NOLOAD | RTLD_LOCAL);
+  if (this == NULL) {
+    return 0;
+  }
+
+  // Poke it and see if we can find the symbol we want. Stop if we found it,
+  // otherwise continue the scan.
+  data->result = dlsym(this, data->symbol);
+  dlclose(this);
+  return data->result == NULL ? 0 : 1;
+}
+
 /// Helper function to fetch the "real" function pointer for a symbol.
 /// Usable only for LD_PRELOAD foils.
 static inline void* hpcrun_foil_dlsym_cached(hpcrun_pfn_cache_t* cache, const char* name) {
   intptr_t result = atomic_load_explicit(cache, memory_order_acquire);
   if (result == 0 /* Not NULL, which (technically) may not be 0 */) {
+    dlerror();
     result = (intptr_t)dlsym(RTLD_NEXT, name);
+    const char* err = dlerror();
+    if (err != NULL) {
+      // Fallback: try to find the symbol by inspecting every object loaded
+      // after us. This is similar to but not identical to RTLD_NEXT, since we
+      // also inspect objects that were loaded with dlopen(RTLD_LOCAL).
+
+      struct callback_data cb_data = {
+          .symbol = name,
+          .skip = true,
+          .skip_until_base = NULL,
+          .result = NULL,
+      };
+
+      // Identify ourselves, using our base address
+      Dl_info dli;
+      if (dladdr(&hpcrun_foil_dlsym_cached, &dli) == 0) {
+          // This should never happen, but if it does error
+          assert(false && "dladdr1 failed to find libmonitor\n");
+          abort();
+      }
+      cb_data.skip_until_base = dli.dli_fbase;
+
+      // Use dl_iterate_phdr to scan through all the objects
+      int found = dl_iterate_phdr(phdr_callback, &cb_data);
+      if (found == 0 || cb_data.result == NULL) {
+          // We didn't find the required symbol, this is a hard error.
+          fprintf(stderr, "dlsym(%s) failed: %s\n", name, err);
+          abort();
+      }
+      result = (intptr_t)cb_data.result;
+    }
+
     intptr_t expected = 0;
     if (!atomic_compare_exchange_strong(cache, &expected, result))
       assert(expected == result);
