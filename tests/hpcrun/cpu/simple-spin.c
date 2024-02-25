bfb12d44aa989e4938d89768568d5b754347414a
Jonathon Anderson <anderson.jonathonm@gmail.com>
2024-02-24 21:18:31 -0600

    tests/hpcrun: Reorganize and lower system load
    In an effort to make the hpcrun tests less straining on the system, they
    have been split by feature and tuned for lower system load. Take 2.
    
    `tests/hpcrun/cpu` now tests CPU events, such as itimer and perf events,
    and is organized as such. These tests run on a simple single-thread
    hot loop. Just enough to make the events fire.
    
    `tests/hpcrun/threads` is where all the tests for thread models are.
    Only the default events are used here, and each threading model has
    3 threads: the main thread (idle), a worker thread (hot loop) and
    an idle thread. This makes these more direct tests for the thread
    tracking subsystem.
    
    All tests changed here are tuned so that only one thread (max) is
    running a hot loop. This keeps the load on the system low when running
    these tests in parallel. The hot loops are also now truly hot (i.e. no
    more yields) since excessive syscalls can also stress the system.

---
+++ tests/hpcrun/cpu/simple-spin.c
@@ -0,0 +1,23 @@
+#include <error.h>
+#include <math.h>
+#include <stdlib.h>
+
+enum {
+  N = 1ULL<<20,
+};
+
+int main() {
+  double* d_p1 = malloc(N * sizeof d_p1[0]);
+  double* d_l1 = malloc(N * sizeof d_l1[0]);
+  double* d_r1 = malloc(N * sizeof d_r1[0]);
+  for (unsigned int i = 0; i < N; i++) {
+    // use transcendental function in the kernel
+    d_p1[i] = d_p1[i] + 1.
+            + (sqrt(exp(log(d_l1[i] * d_l1[i])) + exp(log(d_r1[i] * d_r1[i]))))
+                  / (sqrt(exp(log(d_l1[i] * d_r1[i])) + exp(log((d_r1[i] * d_l1[i])))));
+  }
+  free(d_p1);
+  free(d_l1);
+  free(d_r1);
+  return 0;
+}
