64ee4ef728162fbde227a276a27d837f1831719b
Jonathon Anderson <anderson.jonathonm@gmail.com>
2024-03-19 19:35:01 -0500

    Remove unused header files
    We have a number of headers in the repository that are no longer
     #include'd by any other files. This commit cleans them out.

--- src/tool/hpcrun/constructors.h
+++
@@ -1,9 +0,0 @@
-#ifndef __constructors__h
-#define __constructors__h
-
-#define concat_names(x,y ) x ## y
-#define HPCRUN_CONSTRUCTOR(x) \
-  __attribute__((constructor)) \
-  void concat_names(hpcrun_constructor_, x)
-
-#endif
